32d102ddb81e0323a81520cc5a0b4e7f512f0e1c
unknown <lb@silverliningresearch.com>
2023-08-04 09:03:10 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-03-01",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 13,
-        "download_time": "04-08-2023 10:47:58"
+        "download_time": "04-08-2023 11:03:05"
     },
     {
         "InterviewDate": "2023-03-01",
@@ -6703,7 +6703,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-08-04",
         "quota_id": "ATH - Ryanair",
-        "Number of interviews": 2
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-08-04",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "04-08-2023 10:47:50"
+        "download_time": "04-08-2023 11:02:55"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -21113,7 +21113,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-08-04",
         "quota_id": "TK1938-IST",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-08-04",
