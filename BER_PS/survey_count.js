fe91d273d2b3b665114d8940d1ff4e9c1e3120fb
unknown <lb@silverliningresearch.com>
2023-08-09 10:08:18 +0000

    update new data

--- BER_PS/survey_count.js
+++ BER_PS/survey_count.js
@@ -5,7 +5,7 @@ let survey_count = `[
         "Dest": "Mailand (LIN)",
         "Start": "06:49:24",
         "Finish": "07:14:11",
-        "download_time": "09-08-2023 11:52:53"
+        "download_time": "09-08-2023 12:08:12"
     },
     {
         "InterviewDate": "2023-03-01",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "09-08-2023 11:52:44"
+        "download_time": "09-08-2023 12:07:58"
     },
     {
         "InterviewDate": "2023-04-01",
