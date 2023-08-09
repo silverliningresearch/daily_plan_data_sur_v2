580059059f3377683976498927bddc760700055c
unknown <lb@silverliningresearch.com>
2023-08-09 11:23:04 +0000

    update new data

--- BER_PS/survey_count.js
+++ BER_PS/survey_count.js
@@ -5,7 +5,7 @@ let survey_count = `[
         "Dest": "Mailand (LIN)",
         "Start": "06:49:24",
         "Finish": "07:14:11",
-        "download_time": "09-08-2023 13:08:01"
+        "download_time": "09-08-2023 13:23:01"
     },
     {
         "InterviewDate": "2023-03-01",
