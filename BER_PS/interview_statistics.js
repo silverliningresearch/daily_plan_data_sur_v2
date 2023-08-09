fd8d6c77b49870312b1603d89bb4f4ec4c113a32
unknown <lb@silverliningresearch.com>
2023-08-09 21:23:03 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-03-01",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 13,
-        "download_time": "09-08-2023 23:08:03"
+        "download_time": "09-08-2023 23:22:59"
     },
     {
         "InterviewDate": "2023-03-01",
--- BER_PS/survey_count.js
+++ BER_PS/survey_count.js
@@ -5,7 +5,7 @@ let survey_count = `[
         "Dest": "Mailand (LIN)",
         "Start": "06:49:24",
         "Finish": "07:14:11",
-        "download_time": "09-08-2023 23:08:03"
+        "download_time": "09-08-2023 23:22:59"
     },
     {
         "InterviewDate": "2023-03-01",
