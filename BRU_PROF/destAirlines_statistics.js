6ca772c157f7e15a27e2d4724b0ee3aad3f35a3a
unknown <lb@silverliningresearch.com>
2023-08-11 12:16:16 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "11-08-2023 13:59:10"
+        "download_time": "11-08-2023 14:15:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -20387,6 +20387,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2023-08-11",
+        "quota_id": "AGA-TB",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-11",
         "quota_id": "AMS-KL",
         "Number of interviews": 10
     },
@@ -20414,6 +20419,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-08-11",
         "quota_id": "CFU-TB",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2023-08-11",
+        "quota_id": "CMN-TB",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-08-11",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "11-08-2023 13:59:10"
+        "download_time": "11-08-2023 14:15:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -22489,6 +22489,16 @@ let interview_statistics = `[
         "InterviewDate": "2023-08-11",
         "quota_id": "TB2421-ZTH",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-08-11",
+        "quota_id": "TB2501-AGA",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-11",
+        "quota_id": "TB2533-CMN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-08-11",
