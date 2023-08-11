233b76adfa18b6f6fa29e15e43e834e861802774
unknown <lb@silverliningresearch.com>
2023-08-11 14:13:25 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "11-08-2023 15:58:15"
+        "download_time": "11-08-2023 16:13:09"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -20437,6 +20437,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2023-08-11",
+        "quota_id": "DUB-EI",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-08-11",
         "quota_id": "FCO-AZ",
         "Number of interviews": 10
     },
@@ -20473,12 +20478,12 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2023-08-11",
         "quota_id": "MIR-SN",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-08-11",
         "quota_id": "MUC-LH",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-08-11",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "11-08-2023 15:58:15"
+        "download_time": "11-08-2023 16:13:09"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -22407,6 +22407,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2023-08-11",
+        "quota_id": "EI635-DUB",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-08-11",
         "quota_id": "GQ801-ATH",
         "Number of interviews": 1
     },
@@ -22443,7 +22448,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-08-11",
         "quota_id": "LH2289-MUC",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-08-11",
@@ -22458,7 +22463,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-08-11",
         "quota_id": "SN1115-MIR",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-08-11",
