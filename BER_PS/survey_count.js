5a7336e0e539767be8bdfcaeaa25115c083b1a8d
unknown <lb@silverliningresearch.com>
2023-08-12 01:35:22 +0000

    update new data

--- BER_PS/survey_count.js
+++ BER_PS/survey_count.js
@@ -5,7 +5,7 @@ let survey_count = `[
         "Dest": "Mailand (LIN)",
         "Start": "06:49:24",
         "Finish": "07:14:11",
-        "download_time": "12-08-2023 03:20:21"
+        "download_time": "12-08-2023 03:35:18"
     },
     {
         "InterviewDate": "2023-03-01",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "12-08-2023 03:20:09"
+        "download_time": "12-08-2023 03:35:06"
     },
     {
         "InterviewDate": "2023-04-01",
