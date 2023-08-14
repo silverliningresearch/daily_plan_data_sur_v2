18990ff794529b9061d5bc7af61d92696ad6ad3f
unknown <lb@silverliningresearch.com>
2023-08-14 10:29:55 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "14-08-2023 12:26:14"
+        "download_time": "14-08-2023 12:29:41"
     },
     {
         "InterviewerID": 1,
---
+++ CAG_OSQS/interviews_count.js
@@ -0,0 +1,15 @@
+let interview_statistics  = `[
+    {
+        "InterviewDate": "2023-08-14",
+        "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 3\u2666Airside\u266601-28\u2666PC\u2666Min20",
+        "Number of interviews": 1,
+        "download_time": "14-08-2023 18:29:55"
+    },
+    {
+        "InterviewDate": "2023-08-14",
+        "quota_id": "WH Smith - Premises D (Air)\u2666TERMINAL 2\u2666Airside\u2666026-055-02A\u2666OW\u2666Min6",
+        "Number of interviews": 1
+    }
+]    
+
+ `;
