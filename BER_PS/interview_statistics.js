6b232bd087bbf5147ab6ceeb47139fd174bea07b
unknown <lb@silverliningresearch.com>
2023-08-19 05:06:13 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "19-08-2023 06:51:09"
+        "download_time": "19-08-2023 07:06:09"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -10269,6 +10269,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-08-18",
         "quota_id": "ZRH - Swiss",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2023-08-19",
+        "quota_id": "FRA - Lufthansa",
+        "Number of interviews": 5
     }
 ]    
 
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "19-08-2023 06:50:55"
+        "download_time": "19-08-2023 07:05:55"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -23809,6 +23809,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-08-19",
         "quota_id": "SN2607-FRA",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2023-08-19",
+        "quota_id": "SN3175-FCO",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-08-19",
