a3e97bc5bee78cde3ca55d25462445742a716cbc
unknown <lb@silverliningresearch.com>
2023-08-24 04:36:21 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "24-08-2023 12:20:56"
+        "download_time": "24-08-2023 12:35:56"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -138,6 +138,16 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-08-17",
         "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 1\u2666Landside\u266602-83\u2666PC\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-24",
+        "quota_id": "Oakley\u2666TERMINAL 4\u2666Airside\u2666#02-47\u2666MP\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-24",
+        "quota_id": "The Coffee Bean and Tea Leaf\u2666TERMINAL 4\u2666Airside\u266602-36N\u2666FBA\u2666Min6",
         "Number of interviews": 1
     }
 ]    
