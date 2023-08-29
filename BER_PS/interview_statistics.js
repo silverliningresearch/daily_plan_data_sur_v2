274a414f16706c4d5367ee0801ccf0fd2a908964
unknown <lb@silverliningresearch.com>
2023-08-29 05:51:17 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "29-08-2023 07:35:55"
+        "download_time": "29-08-2023 07:50:54"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -10718,7 +10718,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-08-29",
         "quota_id": "FRA - Lufthansa",
-        "Number of interviews": 1
+        "Number of interviews": 7
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "29-08-2023 13:36:00"
+        "download_time": "29-08-2023 13:50:59"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -163,6 +163,16 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-08-29",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-29",
+        "quota_id": "Hudsons\u2666TERMINAL 2\u2666Airside\u266602-178\u2666FBA\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-29",
+        "quota_id": "WH Smith - Premises C (Land)\u2666TERMINAL 1\u2666Landside\u266602-84\u2666OW\u2666Min6",
         "Number of interviews": 1
     }
 ]    
