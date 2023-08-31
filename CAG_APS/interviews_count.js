f2252ce3370d9a6030753bc51ee8a0de98e90988
unknown <lb@silverliningresearch.com>
2023-08-31 23:38:07 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-08-2023",
         "quota_id": "T1_TR_BKK",
         "Number of interviews": 5,
-        "download_time": "01-09-2023 07:21:19"
+        "download_time": "01-09-2023 07:38:06"
     },
     {
         "InterviewDate": "01-08-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 07:21:02"
+        "download_time": "01-09-2023 07:35:57"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -314,6 +314,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-31",
         "quota_id": "UOB T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-K10\u2666MC\u2666Min9",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-09-01",
+        "quota_id": "Paul le Cafe\u2666TERMINAL 1\u2666Airside\u266602-26\u2666FBA\u2666Min6",
+        "Number of interviews": 2
     }
 ]    
 
