5a4ded5bfacfaab9ec5420617c6bae14811150b8
unknown <lb@silverliningresearch.com>
2023-09-01 11:06:16 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 12:50:58"
+        "download_time": "01-09-2023 13:05:54"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -10838,7 +10838,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "STN - Ryanair",
-        "Number of interviews": 12
+        "Number of interviews": 14
     },
     {
         "InterviewDate": "2023-09-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 18:51:03"
+        "download_time": "01-09-2023 19:05:59"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -322,6 +322,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-01",
+        "quota_id": "CHANGI RECOMMENDS (T1 L2 Landside)\u2666TERMINAL 1\u2666Landside\u2666#02-K39\u2666CR\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-01",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K4\u2666CR\u2666Min9",
         "Number of interviews": 9
     },
@@ -333,7 +338,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "CHEERS\u2666TERMINAL 1\u2666Landside\u2666#02-05A\u2666LS\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-09-01",
