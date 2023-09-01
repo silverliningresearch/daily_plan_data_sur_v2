232e316ee0c8b59a38e11528b817bb39cd0e4410
unknown <lb@silverliningresearch.com>
2023-09-01 10:51:15 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-09-2023 12:35:48"
+        "download_time": "01-09-2023 12:50:49"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -25453,7 +25453,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "HU492-PEK",
-        "Number of interviews": 3
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-09-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 18:36:03"
+        "download_time": "01-09-2023 18:51:03"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -329,6 +329,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-01",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-09-01",
+        "quota_id": "CHEERS\u2666TERMINAL 1\u2666Landside\u2666#02-05A\u2666LS\u2666Min9",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-09-01",
