43efd8277c981d69b3f42f210dc337ddc0c36da2
unknown <lb@silverliningresearch.com>
2023-09-01 06:06:18 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "01-09-2023 07:50:49"
+        "download_time": "01-09-2023 08:05:49"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -23277,6 +23277,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-01",
+        "quota_id": "BCN-FR",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-01",
         "quota_id": "CDG-SN",
         "Number of interviews": 1
     },
@@ -23284,6 +23289,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-09-01",
         "quota_id": "FRA-SN",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2023-09-01",
+        "quota_id": "MAD-FR",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-09-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 13:51:04"
+        "download_time": "01-09-2023 14:06:03"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -328,7 +328,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "Mango Tree Kitchen / Archipelago\u2666TERMINAL 1\u2666Airside\u2666#02-39N\u2666FBA\u2666Min9",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-09-01",
@@ -343,7 +343,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "Pontian Wanton Noodle\u2666TERMINAL 1\u2666Airside\u2666#02-K10N\u2666FBA\u2666Min9",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-09-01",
