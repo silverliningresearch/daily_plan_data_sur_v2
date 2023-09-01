e5c8ba6b530b62e95e6194ce8f59a83e93b57999
unknown <lb@silverliningresearch.com>
2023-09-01 09:21:12 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "01-09-2023 11:05:47"
+        "download_time": "01-09-2023 11:20:47"
     },
     {
         "InterviewerID": 1,
@@ -1038,7 +1038,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 4,
         "Interview_Date": "2023-09-01",
-        "completed_interviews": 57
+        "completed_interviews": 59
     },
     {
         "InterviewerID": 5,
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-09-2023 17:06:00"
+        "download_time": "01-09-2023 17:20:59"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -328,7 +328,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-01",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-09-01",
