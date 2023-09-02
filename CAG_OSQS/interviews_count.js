056438cd375ba0974934a5d742213cf3abe85b6c
unknown <lb@silverliningresearch.com>
2023-09-02 04:21:11 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "02-09-2023 12:06:03"
+        "download_time": "02-09-2023 12:20:57"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -413,7 +413,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-02",
         "quota_id": "Bottega Veneta\u2666TERMINAL 2\u2666Airside\u2666#02-224\u2666LX\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-09-02",
@@ -423,7 +423,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-02",
         "quota_id": "Ferragamo\u2666TERMINAL 2\u2666Airside\u2666#02-222 (#026-078E)\u2666LX\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-09-02",
@@ -444,6 +444,16 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-02",
         "quota_id": "Rimowa\u2666TERMINAL 2\u2666Airside\u2666#02-213\u2666MP\u2666Min9",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2023-09-02",
+        "quota_id": "SUPERFOOD KITCHEN\u2666TERMINAL 3\u2666Landside\u2666#B2-25\u2666LS\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-02",
+        "quota_id": "SUSHI DELI\u2666TERMINAL 3\u2666Landside\u2666#B2-25A\u2666LS\u2666Min9",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-09-02",
