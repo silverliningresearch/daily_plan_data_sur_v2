b2bf9e7e374172e69392666903c2bec737a8919d
unknown <lb@silverliningresearch.com>
2023-09-03 02:06:14 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "03-09-2023 09:51:02"
+        "download_time": "03-09-2023 10:06:02"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -502,8 +502,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-03",
+        "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 3\u2666Landside\u2666#01-K20\u2666CR\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-03",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#01-K21\u2666CR\u2666Min9",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-03",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min9",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-09-03",
@@ -518,7 +528,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "STARBUCKS COFFEE\u2666TERMINAL 3\u2666Landside\u2666#02-90\u2666FBL\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
