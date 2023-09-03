3a7b8c8f5477843255523285a5169a22b71c4052
unknown <lb@silverliningresearch.com>
2023-09-03 16:36:26 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "03-09-2023 18:21:00"
+        "download_time": "03-09-2023 18:36:05"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -10904,6 +10904,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-09-03",
         "quota_id": "FRA - Lufthansa",
         "Number of interviews": 26
+    },
+    {
+        "InterviewDate": "2023-09-03",
+        "quota_id": "HEL - Finnair",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-09-03",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "03-09-2023 18:20:51"
+        "download_time": "03-09-2023 18:35:54"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -23664,6 +23664,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-09-03",
         "quota_id": "MAH-TB",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-09-03",
+        "quota_id": "MXP-SN",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-09-03",
