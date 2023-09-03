a94d99651c4e342ebdc21a1c3a186c6940e583ad
unknown <lb@silverliningresearch.com>
2023-09-03 08:21:25 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-08-2023",
         "quota_id": "T1_TR_BKK",
         "Number of interviews": 5,
-        "download_time": "03-09-2023 16:06:18"
+        "download_time": "03-09-2023 16:21:24"
     },
     {
         "InterviewDate": "01-08-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "03-09-2023 16:06:02"
+        "download_time": "03-09-2023 16:21:13"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -503,12 +503,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "BABA NYONYA\u2666TERMINAL 3\u2666Landside\u2666#03-33\u2666FBL\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "CENTRAL THAI\u2666TERMINAL 3\u2666Landside\u2666#03-30/31\u2666FBL\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-09-03",
@@ -543,12 +543,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "Crown Prince Kitchenette\u2666TERMINAL 3\u2666Landside\u2666#03-42/43\u2666FBL\u2666Min9",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "FAIRPRICE FINEST\u2666TERMINAL 3\u2666Landside\u2666#B2-10\u2666LS\u2666Min9",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-09-03",
@@ -583,7 +583,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-03",
         "quota_id": "ZONE X\u2666TERMINAL 3\u2666Landside\u2666#B2-23\u2666LS\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 6
     }
 ]    
 
