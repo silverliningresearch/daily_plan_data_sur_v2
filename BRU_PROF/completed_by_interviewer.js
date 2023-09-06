66fb338a7b5e22e739df5244482b6552442bd476
unknown <lb@silverliningresearch.com>
2023-09-06 15:51:57 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "06-09-2023 17:35:51"
+        "download_time": "06-09-2023 17:50:59"
     },
     {
         "InterviewerID": 1,
@@ -193,7 +193,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 1,
         "Interview_Date": "2023-09-06",
-        "completed_interviews": 11
+        "completed_interviews": 13
     },
     {
         "InterviewerID": 2,
@@ -1788,7 +1788,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 10,
         "Interview_Date": "2023-09-06",
-        "completed_interviews": 21
+        "completed_interviews": 24
     },
     {
         "InterviewerID": 11,
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "06-09-2023 17:35:51"
+        "download_time": "06-09-2023 17:50:58"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -26239,6 +26239,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-09-06",
         "quota_id": "HV9061-TFS",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2023-09-06",
+        "quota_id": "LX783-ZRH",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-09-06",
