19843674f54c8ec602e1aeb208599a1a4b37bf71
unknown <lb@silverliningresearch.com>
2023-09-07 10:51:53 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-09-2023 18:36:14"
+        "download_time": "07-09-2023 18:51:13"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -853,12 +853,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-07",
         "quota_id": "Bacha Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-15A\u2666CCD\u2666Min9",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-09-07",
         "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min9",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-09-07",
@@ -868,7 +868,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-07",
         "quota_id": "Giordano\u2666TERMINAL 1\u2666Airside\u2666#02-19N\u2666MP\u2666Min9",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-09-07",
@@ -883,7 +883,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-07",
         "quota_id": "Kaboom\u2666TERMINAL 1\u2666Airside\u2666#02-18N\u2666CT\u2666Min9",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-09-07",
@@ -908,7 +908,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-07",
         "quota_id": "T3 iSC Arrival counter\u2666TERMINAL 3\u2666Landside\u2666NA\u2666PS\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-09-07",
