f5f75aa72430cbc0f7d632a97e112de59665db87
unknown <lb@silverliningresearch.com>
2023-09-11 03:37:17 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "11-09-2023 11:22:00"
+        "download_time": "11-09-2023 11:36:37"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -1294,6 +1294,16 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-10",
         "quota_id": "Taste Singapore (Premises A3)\u2666TERMINAL 1\u2666Airside\u2666#02-K23\u2666CCD\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-09-11",
+        "quota_id": "LT T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-65\u2666LT\u2666Min25",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-11",
+        "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min25",
+        "Number of interviews": 1
     }
 ]    
 
