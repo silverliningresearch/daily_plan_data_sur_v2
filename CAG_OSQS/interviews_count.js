86a48ec68a2afc88af02cf2e544ecbf2d91feb51
unknown <lb@silverliningresearch.com>
2023-09-12 10:52:03 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-09-2023 18:36:26"
+        "download_time": "12-09-2023 18:51:23"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -1368,12 +1368,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-12",
         "quota_id": "Changi Recommends (T1 Licensed Area A)\u2666TERMINAL 1\u2666Airside\u2666#02-COUNTER (near PAUL)\u2666CR\u2666Min9",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-09-12",
+        "quota_id": "Charles & Keith\u2666TERMINAL 1\u2666Airside\u2666#02-51\u2666MP\u2666Min9",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-09-12",
         "quota_id": "Christian Dior\u2666TERMINAL 1\u2666Airside\u2666#02-44 & #03-44-MZ\u2666LX\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-09-12",
@@ -1383,7 +1388,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-12",
         "quota_id": "Louis Vuitton\u2666TERMINAL 1\u2666Airside\u2666#02-50\u2666LX\u2666Min9",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-09-12",
