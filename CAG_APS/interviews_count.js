868422c2f58863d1ec2d092f458bcbc238e7fec1
unknown <lb@silverliningresearch.com>
2023-09-15 03:21:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-08-2023",
         "quota_id": "T1_TR_BKK",
         "Number of interviews": 5,
-        "download_time": "15-09-2023 11:06:53"
+        "download_time": "15-09-2023 11:21:56"
     },
     {
         "InterviewDate": "01-08-2023",
@@ -1988,7 +1988,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "15-09-2023",
         "quota_id": "T1_KR_PNH",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "15-09-2023",
@@ -2009,6 +2009,11 @@ let interview_statistics  = `[
         "InterviewDate": "15-09-2023",
         "quota_id": "T1_TR_HAN",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "15-09-2023",
+        "quota_id": "T4_HU_HAK",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "16-08-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "15-09-2023 11:06:14"
+        "download_time": "15-09-2023 11:21:17"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -1658,7 +1658,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-15",
         "quota_id": "PARIS BAGUETTE\u2666TERMINAL 3\u2666Landside\u2666#01-22\u2666FBL\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-09-15",
