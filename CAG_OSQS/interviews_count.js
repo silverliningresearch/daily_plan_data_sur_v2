5a3b580c66b679b9dfe203962ba6ac1e5c12b3c1
unknown <lb@silverliningresearch.com>
2023-09-15 04:21:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "15-09-2023 12:06:19"
+        "download_time": "15-09-2023 12:21:13"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -1672,13 +1672,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-15",
-        "quota_id": "STARHUB\u2666TERMINAL 3\u2666Landside\u2666#01-K11\u2666CR\u2666Min9",
+        "quota_id": "Ray-Ban\u2666TERMINAL 2\u2666Airside\u2666#02-152 (#02-094)\u2666MP\u2666Min9",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-09-15",
-        "quota_id": "TRAVELLERS\u2666TERMINAL 3\u2666Landside\u2666#01-K17\u2666CR\u2666Min9",
+        "quota_id": "STARHUB\u2666TERMINAL 3\u2666Landside\u2666#01-K11\u2666CR\u2666Min9",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-09-15",
+        "quota_id": "TRAVELLERS\u2666TERMINAL 3\u2666Landside\u2666#01-K17\u2666CR\u2666Min9",
+        "Number of interviews": 4
     }
 ]    
 
