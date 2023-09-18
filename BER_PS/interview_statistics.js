d12c883cb2f8530465a238f789dca0f3818fad87
unknown <lb@silverliningresearch.com>
2023-09-18 02:37:01 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "18-09-2023 04:21:10"
+        "download_time": "18-09-2023 04:36:10"
     },
     {
         "InterviewDate": "2023-01-10",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "18-09-2023 10:21:24"
+        "download_time": "18-09-2023 10:36:23"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -2037,13 +2037,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-18",
+        "quota_id": "HUGO\u2666TERMINAL 2\u2666Airside\u266602-232\u2666LX\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-09-18",
         "quota_id": "The Cocoa Trees (Premises B3) (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-403\u2666CCD\u2666Min9",
         "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-09-18",
         "quota_id": "iStudio\u2666TERMINAL 2\u2666Airside\u2666#02-238\u2666EL\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
