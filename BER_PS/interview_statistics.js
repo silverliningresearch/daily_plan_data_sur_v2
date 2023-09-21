a2c2ea75e535ac913dc5a90a8e4913755a115f04
unknown <lb@silverliningresearch.com>
2023-09-21 13:07:20 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "21-09-2023 14:51:22"
+        "download_time": "21-09-2023 15:06:22"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -12333,7 +12333,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-09-21",
         "quota_id": "CAI - Egypt Air",
-        "Number of interviews": 7
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2023-09-21",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "21-09-2023 20:51:39"
+        "download_time": "21-09-2023 21:06:39"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -2393,7 +2393,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-21",
         "quota_id": "LT T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-22\u2666LT\u2666Min20",
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2023-09-21",
@@ -2418,7 +2418,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-21",
         "quota_id": "Perfumes & Cosmetics (T1 ABW)\u2666TERMINAL 1\u2666Airside\u2666#01-22F\u2666PC\u2666Min25",
-        "Number of interviews": 12
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2023-09-21",
