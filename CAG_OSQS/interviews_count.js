57255fd928ffb677fc7a411b38b76e9ab3b05bb3
unknown <lb@silverliningresearch.com>
2023-09-22 01:37:04 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "22-09-2023 09:21:40"
+        "download_time": "22-09-2023 09:36:23"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -2459,6 +2459,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-21",
         "quota_id": "The Cocoa Trees (Premises C1) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-27\u2666CCD\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-09-22",
+        "quota_id": "Bee Cheng Hiang (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#026-135 (#02-199)\u2666CCD\u2666Min9",
+        "Number of interviews": 1
     }
 ]    
 
