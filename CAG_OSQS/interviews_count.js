1fd8b05d374fa48a33412049b83ceeda034af73c
unknown <lb@silverliningresearch.com>
2023-09-23 23:56:19 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "24-09-2023 07:40:29"
+        "download_time": "24-09-2023 07:55:40"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -2674,6 +2674,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-24",
         "quota_id": "Hudsons (DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-209 (beside Travelex)\u2666FBA\u2666Min9",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2023-09-24",
+        "quota_id": "T2 iSC Departure counter (beside GST Refund Counter)\u2666TERMINAL 2\u2666Airside\u2666NA\u2666PS\u2666Min9",
+        "Number of interviews": 1
     }
 ]    
 
