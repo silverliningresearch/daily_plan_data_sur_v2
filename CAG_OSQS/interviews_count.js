00dcf5fcef58c4053c81596e0b677fde4c30c5e2
unknown <lb@silverliningresearch.com>
2023-09-26 03:25:29 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-09-2023 11:08:55"
+        "download_time": "26-09-2023 11:24:51"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -2982,6 +2982,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-26",
+        "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP\u2666Min9",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-09-26",
         "quota_id": "LT T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#02-401\u2666LT\u2666Min25",
         "Number of interviews": 4
     },
@@ -3003,7 +3008,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-26",
         "quota_id": "Prada\u2666TERMINAL 2\u2666Airside\u2666#026-072\u2666LX\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-09-26",
@@ -3038,7 +3043,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-26",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 4
     }
 ]    
 
