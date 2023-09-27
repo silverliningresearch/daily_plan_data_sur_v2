222330c24b8d4366e4f0acc41821aca0abdbd2a1
unknown <lb@silverliningresearch.com>
2023-09-27 02:41:31 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-09-2023 10:25:40"
+        "download_time": "27-09-2023 10:40:52"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -3119,6 +3119,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-27",
         "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 3\u2666Airside\u2666#03-15\u2666HL\u2666Min20",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-27",
+        "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min25",
+        "Number of interviews": 3
     }
 ]    
 
