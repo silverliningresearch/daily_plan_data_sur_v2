4e4e5c2ab11c82c36bf61186ebe87bf53ec96979
unknown <lb@silverliningresearch.com>
2023-09-28 23:52:14 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "05-09-2023",
         "quota_id": "T1_BA_SYD",
         "Number of interviews": 3,
-        "download_time": "29-09-2023 07:37:11"
+        "download_time": "29-09-2023 07:52:13"
     },
     {
         "InterviewDate": "05-09-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "29-09-2023 07:36:35"
+        "download_time": "29-09-2023 07:51:35"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -3353,7 +3353,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-29",
         "quota_id": "Bacha Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-15A\u2666CCD\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-09-29",
@@ -3363,7 +3363,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-29",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 1\u2666Airside\u2666#02-14\u2666CCD\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
