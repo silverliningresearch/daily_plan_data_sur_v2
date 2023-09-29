65b3a7db3859e38d01da9c7b2fb8f0b62120942c
unknown <lb@silverliningresearch.com>
2023-09-29 03:22:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "29-09-2023 11:06:40"
+        "download_time": "29-09-2023 11:21:59"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -3387,6 +3387,16 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-09-29",
+        "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-09-29",
+        "quota_id": "Jamie's Deli\u2666TERMINAL 1\u2666Airside\u2666#02-40N\u2666FBA\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-29",
         "quota_id": "Kaboom\u2666TERMINAL 1\u2666Airside\u2666#02-18N\u2666CT\u2666Min9",
         "Number of interviews": 1
     },
@@ -3398,7 +3408,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-29",
         "quota_id": "Starbucks Coffee (Premises A)\u2666TERMINAL 1\u2666Airside\u2666#02-66\u2666FBA\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-09-29",
@@ -3409,6 +3419,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-09-29",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 1\u2666Airside\u2666#02-14\u2666CCD\u2666Min9",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-09-29",
+        "quota_id": "The Cocoa Trees (Premises A2) (T1 DTE)\u2666TERMINAL 1\u2666Airside\u2666#02-62\u2666CCD\u2666Min9",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-09-29",
