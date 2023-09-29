a139adb9da6560bc2a0982715ec2524cd0422592
unknown <lb@silverliningresearch.com>
2023-09-29 03:37:13 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "05-09-2023",
         "quota_id": "T1_BA_SYD",
         "Number of interviews": 3,
-        "download_time": "29-09-2023 11:22:39"
+        "download_time": "29-09-2023 11:37:12"
     },
     {
         "InterviewDate": "05-09-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "29-09-2023 11:21:59"
+        "download_time": "29-09-2023 11:36:34"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -3388,7 +3388,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-29",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-09-29",
@@ -3403,6 +3403,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-09-29",
         "quota_id": "Mango Tree Kitchen / Archipelago\u2666TERMINAL 1\u2666Airside\u2666#02-39N\u2666FBA\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-09-29",
+        "quota_id": "Pezzo\u2666TERMINAL 1\u2666Airside\u2666#02-K09N\u2666FBA\u2666Min9",
         "Number of interviews": 1
     },
     {
