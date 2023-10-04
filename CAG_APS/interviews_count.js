20c1f9522d39931a2b6557b76d79d0abc65f3182
unknown <lb@silverliningresearch.com>
2023-10-04 03:37:33 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "04-10-2023 11:22:39"
+        "download_time": "04-10-2023 11:37:32"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -139,6 +139,11 @@ let interview_statistics  = `[
         "InterviewDate": "04-10-2023",
         "quota_id": "T3_SQ_ICN",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "04-10-2023",
+        "quota_id": "T3_SQ_LHR",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "04-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "04-10-2023 11:22:05"
+        "download_time": "04-10-2023 11:36:58"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -4137,13 +4137,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-04",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K11\u2666CR\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-10-04",
         "quota_id": "Paris Baguette\u2666TERMINAL 4\u2666Landside\u2666#01-05\u2666FBL\u2666Min9",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-04",
         "quota_id": "Premises B - Baggage Storage by Smarte Carte (Landside)\u2666TERMINAL 4\u2666Landside\u2666#01-02\u2666BS\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 6
     }
 ]    
 
