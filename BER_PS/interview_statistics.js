a9d49f0e049f2587d79027b9e3c4339dd3766923
unknown <lb@silverliningresearch.com>
2023-10-05 05:37:33 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "05-10-2023 07:21:43"
+        "download_time": "05-10-2023 07:36:33"
     },
     {
         "InterviewDate": "2023-01-10",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "05-10-2023 13:22:06"
+        "download_time": "05-10-2023 13:36:57"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -4418,7 +4418,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-05",
         "quota_id": "Ambassador Transit Lounge\u2666TERMINAL 3\u2666Airside\u2666#03-14\u2666HL\u2666Min20",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-10-05",
