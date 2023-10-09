dcfc9c85c1293cd264d05cab05368a8f832bd224
unknown <lb@silverliningresearch.com>
2023-10-09 05:37:21 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "09-10-2023 13:22:34"
+        "download_time": "09-10-2023 13:37:21"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -308,7 +308,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "09-10-2023",
         "quota_id": "T3_ID_CGK",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "09-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "09-10-2023 13:21:59"
+        "download_time": "09-10-2023 13:36:45"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -5053,7 +5053,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-09",
         "quota_id": "Hermes\u2666TERMINAL 3\u2666Airside\u2666#02-05E\u2666LX\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-09",
@@ -5083,7 +5083,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-09",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-10-09",
@@ -5098,7 +5098,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-09",
         "quota_id": "Saint Laurent\u2666TERMINAL 3\u2666Airside\u2666#02-06(N)\u2666LX\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-10-09",
