e5664057dd61ac166196fc6f809e4014a2fd658d
unknown <lb@silverliningresearch.com>
2023-10-16 04:53:29 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "16-10-2023 12:37:19"
+        "download_time": "16-10-2023 12:52:48"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6117,8 +6117,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-16",
+        "quota_id": "Crystal Jade La Mian Xiao Long Bao\u2666TERMINAL 1\u2666Airside\u2666#03-54\u2666FBA\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-10-16",
         "quota_id": "I Love Taimei\u2666TERMINAL 1\u2666Airside\u2666#03-53\u2666FBA\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-16",
@@ -6153,7 +6158,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-16",
         "quota_id": "Tap & Brew\u2666TERMINAL 1\u2666Airside\u2666#03-52\u2666FBA\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-16",
