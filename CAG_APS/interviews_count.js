1ac4a77781660725ca364a02cfaf89d52ed4e5f5
unknown <lb@silverliningresearch.com>
2023-10-17 23:53:15 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "18-10-2023 07:38:10"
+        "download_time": "18-10-2023 07:53:14"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -992,8 +992,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "18-10-2023",
+        "quota_id": "T1_TR_HKT",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "18-10-2023",
         "quota_id": "T3_SH_PVG",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "18-10-2023",
+        "quota_id": "T3_SQ_DRW",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "18-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "18-10-2023 07:37:34"
+        "download_time": "18-10-2023 07:52:37"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6339,6 +6339,16 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-18",
         "quota_id": "Oakley\u2666TERMINAL 4\u2666Airside\u2666#02-47\u2666MP\u2666Min9",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-18",
+        "quota_id": "T4 iSC Departure counter\u2666TERMINAL 4\u2666Airside\u2666NA\u2666PS\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-18",
+        "quota_id": "The Cocoa Trees (Premises D)\u2666TERMINAL 4\u2666Airside\u2666#02-26\u2666CCD\u2666Min9",
+        "Number of interviews": 2
     }
 ]    
 
