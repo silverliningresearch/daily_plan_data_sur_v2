ba132e2cb8591239d85a7586ac50ba026bd8388d
unknown <lb@silverliningresearch.com>
2023-10-18 01:23:02 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "18-10-2023 09:08:38"
+        "download_time": "18-10-2023 09:23:00"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1023,6 +1023,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "18-10-2023",
         "quota_id": "T2_CA_PEK",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "18-10-2023",
+        "quota_id": "T3_ID_YIA",
         "Number of interviews": 1
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "18-10-2023 09:08:03"
+        "download_time": "18-10-2023 09:22:23"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6334,6 +6334,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-17",
         "quota_id": "iStudio\u2666TERMINAL 4\u2666Airside\u2666#02-52\u2666EL\u2666Min9",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2023-10-18",
+        "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP\u2666Min9",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-10-18",
