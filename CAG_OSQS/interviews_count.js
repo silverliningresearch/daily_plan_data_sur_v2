76e68abec9a385a8d1db8ec0683bd5350e86f42c
unknown <lb@silverliningresearch.com>
2023-10-18 11:37:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "18-10-2023 19:22:14"
+        "download_time": "18-10-2023 19:37:06"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6453,7 +6453,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-18",
         "quota_id": "The Cocoa Trees (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-35N\u2666CCD\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-10-18",
@@ -6468,7 +6468,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-18",
         "quota_id": "WH Smith - Premises B (Air) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-41C\u2666OW\u2666Min9",
-        "Number of interviews": 6
+        "Number of interviews": 7
     }
 ]    
 
