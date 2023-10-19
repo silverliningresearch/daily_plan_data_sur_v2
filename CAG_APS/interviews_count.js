4096ca346375f260e37c3fc79f95e853b777a50f
unknown <lb@silverliningresearch.com>
2023-10-19 05:53:18 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "19-10-2023 13:38:15"
+        "download_time": "19-10-2023 13:53:16"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1128,6 +1128,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "19-10-2023",
         "quota_id": "T1_FJ_NAN",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "19-10-2023",
+        "quota_id": "T1_OQ_CKG",
         "Number of interviews": 2
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "19-10-2023 13:37:37"
+        "download_time": "19-10-2023 13:52:41"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6493,7 +6493,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-19",
         "quota_id": "GO NOODLE HOUSE & PAPPARICH\u2666TERMINAL 2\u2666Landside\u2666#03-24 & 25\u2666FBL\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-10-19",
@@ -6528,7 +6528,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-19",
         "quota_id": "XW Western Grill \u2666TERMINAL 2\u2666Landside\u2666#03-23\u2666FBL\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
