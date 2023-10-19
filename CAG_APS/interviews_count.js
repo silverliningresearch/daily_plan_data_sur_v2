8a9b58c0a296169350652ea7cb10bbd0d8415da3
unknown <lb@silverliningresearch.com>
2023-10-19 02:53:12 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "19-10-2023 10:38:44"
+        "download_time": "19-10-2023 10:53:10"
     },
     {
         "InterviewDate": "01-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "19-10-2023 10:38:06"
+        "download_time": "19-10-2023 10:52:35"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6469,6 +6469,16 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-18",
         "quota_id": "WH Smith - Premises B (Air) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-41C\u2666OW\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-10-19",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-05 (beside iSC counter near lifts)\u2666CR\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-19",
+        "quota_id": "Paris Baguette\u2666TERMINAL 2\u2666Landside\u2666#01-04\u2666FBL\u2666Min9",
+        "Number of interviews": 2
     }
 ]    
 
