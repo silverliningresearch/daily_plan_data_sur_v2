488585263481e1f47b5188092d15fe611bea6011
unknown <lb@silverliningresearch.com>
2023-10-20 03:23:20 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "20-10-2023 11:09:56"
+        "download_time": "20-10-2023 11:23:19"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1228,7 +1228,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "20-10-2023",
         "quota_id": "T4_AA_DPS",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "20-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "20-10-2023 11:09:19"
+        "download_time": "20-10-2023 11:22:43"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6553,6 +6553,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-20",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-20",
+        "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min9",
         "Number of interviews": 1
     },
     {
