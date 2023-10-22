e3d4315ccb75c3cf7af0f2e7413b5d707a467fa7
unknown <lb@silverliningresearch.com>
2023-10-22 00:38:36 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "22-10-2023 02:21:22"
+        "download_time": "22-10-2023 02:36:25"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "22-10-2023 08:22:45"
+        "download_time": "22-10-2023 08:37:58"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6722,13 +6722,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-22",
+        "quota_id": "BIKANERVALA\u2666TERMINAL 3\u2666Landside\u2666#04-01\u2666FBL\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-22",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-22",
         "quota_id": "YAKUN FAMILY CAFE & BEARD PAPA'S\u2666TERMINAL 3\u2666Landside\u2666#B2-07\u2666FBL\u2666Min9",
-        "Number of interviews": 6
+        "Number of interviews": 9
     }
 ]    
 
