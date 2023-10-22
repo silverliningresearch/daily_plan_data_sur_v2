31f7d7a0275de17a86e4375faab45529f20f8a49
unknown <lb@silverliningresearch.com>
2023-10-22 09:38:38 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "22-10-2023 17:24:06"
+        "download_time": "22-10-2023 17:38:37"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1254,6 +1254,11 @@ let interview_statistics  = `[
         "InterviewDate": "21-10-2023",
         "quota_id": "T3_SV_JED",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "22-10-2023",
+        "quota_id": "T4_3K_PEN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "22-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "22-10-2023 17:23:27"
+        "download_time": "22-10-2023 17:38:00"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6798,7 +6798,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-22",
         "quota_id": "Scarves & Glitter, House of Kashkha\u2666TERMINAL 1\u2666Airside\u2666#02-K46\u2666MP\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-10-22",
