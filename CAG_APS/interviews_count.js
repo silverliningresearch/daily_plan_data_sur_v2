599cc279fb0a15c597234446104f0ab559a95caa
unknown <lb@silverliningresearch.com>
2023-10-22 01:53:29 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "22-10-2023 09:38:22"
+        "download_time": "22-10-2023 09:53:28"
     },
     {
         "InterviewDate": "01-10-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "22-10-2023 09:37:46"
+        "download_time": "22-10-2023 09:52:50"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6727,8 +6727,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-22",
+        "quota_id": "JOLLIBEAN\u2666TERMINAL 3\u2666Landside\u2666#B2-13\u2666FBL\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-22",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2023-10-22",
+        "quota_id": "MA MUM TO GO\u2666TERMINAL 1\u2666Landside\u2666#01-K23\u2666FBL\u2666Min9",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-10-22",
