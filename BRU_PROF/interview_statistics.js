80c033dd36c1a9628cd0df6187df5db3ca647fec
unknown <lb@silverliningresearch.com>
2023-10-23 06:38:34 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "23-10-2023 08:21:27"
+        "download_time": "23-10-2023 08:36:27"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -32904,6 +32904,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-10-23",
         "quota_id": "FR2947-MAD",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2023-10-23",
+        "quota_id": "LH2283-MUC",
+        "Number of interviews": 2
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "23-10-2023 14:22:59"
+        "download_time": "23-10-2023 14:37:57"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -6842,6 +6842,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-23",
+        "quota_id": "Liquor & Tobacco (T2 Arrival South)\u2666TERMINAL 2\u2666Airside\u2666#01-176\u2666LT\u2666Min15",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-10-23",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min9",
         "Number of interviews": 5
     },
@@ -6857,8 +6862,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-23",
+        "quota_id": "Perfumes & Cosmetics (T2 ABS)\u2666TERMINAL 2\u2666Airside\u2666#01-177\u2666PC\u2666Min15",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-23",
         "quota_id": "Ray-Ban\u2666TERMINAL 2\u2666Airside\u2666#02-152 (#026-094)\u2666MP\u2666Min9",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2023-10-23",
+        "quota_id": "Starbucks\u2666TERMINAL 2\u2666Airside\u2666#026-095-06\u2666FBA\u2666Min9",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-23",
