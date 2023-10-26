2d6665bd9ad1754e3a2281908f0768def8d9e606
unknown <lb@silverliningresearch.com>
2023-10-26 00:38:28 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "26-10-2023 08:23:22"
+        "download_time": "26-10-2023 08:38:27"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1539,6 +1539,11 @@ let interview_statistics  = `[
         "InterviewDate": "26-10-2023",
         "quota_id": "T1_TR_SYD",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "26-10-2023",
+        "quota_id": "T3_SQ_SEA",
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-10-2023 08:22:46"
+        "download_time": "26-10-2023 08:37:50"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7282,8 +7282,23 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-26",
+        "quota_id": "Adidas\u2666TERMINAL 2\u2666Airside\u2666#02-168 (#026-106/7)\u2666MP\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-10-26",
+        "quota_id": "Kaboom\u2666TERMINAL 2\u2666Airside\u2666#02-162\u2666CT\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-26",
+        "quota_id": "Lacoste\u2666TERMINAL 2\u2666Airside\u2666#02-163\u2666MP\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-26",
         "quota_id": "Liquor & Tobacco (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-201/202/203\u2666LT\u2666Min15",
-        "Number of interviews": 10
+        "Number of interviews": 11
     },
     {
         "InterviewDate": "2023-10-26",
