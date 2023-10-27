b044c210051e1f320e566aba863f48bcb3c2b456
unknown <lb@silverliningresearch.com>
2023-10-27 04:08:45 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-10-2023 11:52:57"
+        "download_time": "27-10-2023 12:08:04"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7522,6 +7522,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-10-27",
+        "quota_id": "Bottega Veneta\u2666TERMINAL 2\u2666Airside\u2666#02-224\u2666LX\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-10-27",
         "quota_id": "Burberry\u2666TERMINAL 2\u2666Airside\u2666#02-214\u2666LX\u2666Min9",
         "Number of interviews": 5
     },
@@ -7539,6 +7544,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-27",
         "quota_id": "Giordano\u2666TERMINAL 2\u2666Airside\u2666#026-096\u2666MP\u2666Min9",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-10-27",
+        "quota_id": "Guardian (Premises B) (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-192\u2666PH\u2666Min9",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-10-27",
