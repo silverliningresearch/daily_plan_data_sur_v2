61eddbf3285a6ec9b183229a46c26ddd9d7ee3fe
unknown <lb@silverliningresearch.com>
2023-10-27 08:23:41 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "27-10-2023 10:06:33"
+        "download_time": "27-10-2023 10:21:29"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-10-2023 16:08:08"
+        "download_time": "27-10-2023 16:23:03"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7559,6 +7559,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-27",
         "quota_id": "Guardian (Premises B) (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-192\u2666PH\u2666Min9",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2023-10-27",
+        "quota_id": "Irvins Salted Egg\u2666TERMINAL 3\u2666Airside\u2666#02-43A\u2666CCD\u2666Min9",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-10-27",
