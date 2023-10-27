fca59b4c9f9da476181c3874453293e1e9ef6f09
unknown <lb@silverliningresearch.com>
2023-10-27 05:52:42 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "27-10-2023 13:37:39"
+        "download_time": "27-10-2023 13:52:41"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1583,7 +1583,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "27-10-2023",
         "quota_id": "T3_MU_XIA",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "27-10-2023",
@@ -1598,7 +1598,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "27-10-2023",
         "quota_id": "T4_QH_SGN",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-10-2023 13:37:03"
+        "download_time": "27-10-2023 13:52:05"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7528,7 +7528,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-27",
         "quota_id": "Bottega Veneta\u2666TERMINAL 2\u2666Airside\u2666#02-224\u2666LX\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-10-27",
@@ -7603,7 +7603,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-27",
         "quota_id": "Saint Laurent\u2666TERMINAL 2\u2666Airside\u2666#026-074\u2666LX\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-10-27",
