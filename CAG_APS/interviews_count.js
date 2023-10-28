e5759b8867b34590947fd66bd82b97670618e703
unknown <lb@silverliningresearch.com>
2023-10-28 01:53:44 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "28-10-2023 09:39:50"
+        "download_time": "28-10-2023 09:53:43"
     },
     {
         "InterviewDate": "01-10-2023",
@@ -1603,6 +1603,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "28-10-2023",
         "quota_id": "T1_TR_SUR",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "28-10-2023",
+        "quota_id": "T1_TR_VTE",
         "Number of interviews": 1
     }
 ]    
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "28-10-2023 09:39:14"
+        "download_time": "28-10-2023 09:53:02"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7698,7 +7698,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-28",
         "quota_id": "The Coffee Bean and Tea Leaf\u2666TERMINAL 4\u2666Airside\u2666#02-36N\u2666FBA\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
