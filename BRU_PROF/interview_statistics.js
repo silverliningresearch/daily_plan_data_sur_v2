5eff1902fb7e06aa71623112fe3c22094d663ad1
unknown <lb@silverliningresearch.com>
2023-10-28 07:53:40 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "28-10-2023 09:36:29"
+        "download_time": "28-10-2023 09:51:30"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -33663,7 +33663,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-10-28",
         "quota_id": "SN255-ABJ",
-        "Number of interviews": 3
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-10-28",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "28-10-2023 15:38:03"
+        "download_time": "28-10-2023 15:53:02"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7753,7 +7753,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-28",
         "quota_id": "LIHO TEA\u2666TERMINAL 3\u2666Landside\u2666#01-23\u2666LS\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-10-28",
@@ -7783,7 +7783,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-28",
         "quota_id": "T3 iSC Arrival counter\u2666TERMINAL 3\u2666Landside\u2666NA\u2666PS\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-10-28",
@@ -7823,7 +7823,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-10-28",
         "quota_id": "Travelex T3 Arr Mtg Hall North\u2666TERMINAL 3\u2666Landside\u2666#01-K15\u2666MC\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-10-28",
