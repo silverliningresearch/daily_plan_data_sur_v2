6f44bc0b426a788118c8287f39d08b702cd62560
unknown <lb@silverliningresearch.com>
2023-10-30 05:23:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "30-10-2023 13:07:32"
+        "download_time": "30-10-2023 13:22:39"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7909,6 +7909,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-10-29",
         "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-10-30",
+        "quota_id": "Encik Tan\u2666TERMINAL 3\u2666Airside\u2666#02-K6\u2666FBA\u2666Min9",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-10-30",
