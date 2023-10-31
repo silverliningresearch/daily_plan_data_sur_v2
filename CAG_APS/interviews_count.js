245542c08e18dca3457c190bf6d553d5e744dbff
unknown <lb@silverliningresearch.com>
2023-10-31 05:24:02 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-10-2023",
         "quota_id": "T1_QR_DOH",
         "Number of interviews": 5,
-        "download_time": "31-10-2023 13:09:23"
+        "download_time": "31-10-2023 13:24:00"
     },
     {
         "InterviewDate": "01-10-2023",
