3eef66642ed47345f015e2cbdd2c947885e2ac25
unknown <lb@silverliningresearch.com>
2023-11-01 02:38:49 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-11-2023 03:21:36"
+        "download_time": "01-11-2023 03:36:38"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-11-2023 10:23:09"
+        "download_time": "01-11-2023 10:38:12"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7949,6 +7949,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-01",
         "quota_id": "MA MUM\u2666TERMINAL 4\u2666Landside\u2666#01-04\u2666FBL\u2666Min9",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-11-01",
+        "quota_id": "Starbucks Coffee\u2666TERMINAL 4\u2666Landside\u2666#02-04\u2666FBL\u2666Min9",
+        "Number of interviews": 3
     }
 ]    
 
