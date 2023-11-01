46a03409d79f4d6042c803af97e3f8c29847f70b
unknown <lb@silverliningresearch.com>
2023-11-01 05:36:36 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,12 +3,32 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 5,
-        "download_time": "01-11-2023 13:21:53"
+        "download_time": "01-11-2023 13:36:35"
     },
     {
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CZ_CAN",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "01-11-2023",
+        "quota_id": "T1_PR_BRI",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "01-11-2023",
+        "quota_id": "T1_PR_MNL",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "01-11-2023",
+        "quota_id": "T1_PR_NAI",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "01-11-2023",
+        "quota_id": "T1_PR_PHI",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-11-2023 13:21:19"
+        "download_time": "01-11-2023 13:36:00"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -7953,7 +7953,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-01",
         "quota_id": "ANDES BY ASTONS\u2666TERMINAL 4\u2666Landside\u2666#02-202\u2666FBL\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-01",
@@ -7963,7 +7963,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-01",
         "quota_id": "Burger King\u2666TERMINAL 4\u2666Landside\u2666#02-204\u2666FBL\u2666Min9",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-01",
