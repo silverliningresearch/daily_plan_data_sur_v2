191df63350ad17672033d96591828fc89f2b5b50
unknown <lb@silverliningresearch.com>
2023-11-03 08:21:39 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "03-11-2023 16:06:01"
+        "download_time": "03-11-2023 16:21:00"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8153,7 +8153,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-03",
         "quota_id": "Hudsons (DTS Gourmet Garden)\u2666TERMINAL 2\u2666Airside\u2666#02-178\u2666FBA\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-03",
@@ -8183,7 +8183,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-03",
         "quota_id": "The Hainan Story Coffee & Toast\u2666TERMINAL 2\u2666Airside\u2666#02-K11\u2666FBA\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-03",
