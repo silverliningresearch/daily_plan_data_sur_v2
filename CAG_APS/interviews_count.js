a1c3876708f5c1fd4d812c5a38bc721abfeeab0e
unknown <lb@silverliningresearch.com>
2023-11-04 02:36:45 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "04-11-2023 10:21:39"
+        "download_time": "04-11-2023 10:36:44"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "04-11-2023 10:21:05"
+        "download_time": "04-11-2023 10:36:10"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8198,6 +8198,16 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-04",
         "quota_id": "Burger King\u2666TERMINAL 1\u2666Airside\u2666#03-02\u2666FBA\u2666Min9",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-11-04",
+        "quota_id": "CTE T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-21\u2666MC\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-11-04",
+        "quota_id": "Premises B - Baggage Storage by Smarte Carte\u2666TERMINAL 1\u2666Airside\u2666#02-17\u2666BS\u2666Min9",
         "Number of interviews": 1
     }
 ]    
