165fffe0ce6cf8ca40efe3c9690ef3f15fa8b7c3
unknown <lb@silverliningresearch.com>
2023-11-06 04:51:43 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "06-11-2023 12:36:08"
+        "download_time": "06-11-2023 12:51:07"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8333,12 +8333,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-06",
         "quota_id": "ANJAPPAR AND KAILASH PARBAT\u2666TERMINAL 1\u2666Landside\u2666#03-20\u2666FBL\u2666Min9",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-11-06",
         "quota_id": "Aerotel\u2666TERMINAL 1\u2666Airside\u2666#03-139&140\u2666HL\u2666Min20",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-06",
@@ -8368,7 +8368,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-06",
         "quota_id": "Penang Culture and Woke Ramen\u2666TERMINAL 1\u2666Landside\u2666#03-19\u2666FBL\u2666Min9",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-11-06",
@@ -8378,7 +8378,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-06",
         "quota_id": "T1 iSC Departure counter\u2666TERMINAL 1\u2666Airside\u2666NA\u2666PS\u2666Min9",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-11-06",
