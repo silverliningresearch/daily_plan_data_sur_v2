2d5b546a2daa4e17bbbcf0c990249598c41e4463
unknown <lb@silverliningresearch.com>
2023-11-07 05:36:56 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-11-2023 13:21:24"
+        "download_time": "07-11-2023 13:36:19"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8488,7 +8488,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-07",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min9",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-07",
@@ -8503,7 +8503,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-07",
         "quota_id": "UOB T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K27\u2666MC\u2666Min9",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-07",
