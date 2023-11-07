224914bf5b71311e258bed480699d8cc38145188
unknown <lb@silverliningresearch.com>
2023-11-07 02:51:46 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "07-11-2023 10:36:45"
+        "download_time": "07-11-2023 10:51:45"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -609,6 +609,11 @@ let interview_statistics  = `[
         "InterviewDate": "07-11-2023",
         "quota_id": "T3_SQ_HKG",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "07-11-2023",
+        "quota_id": "T3_SQ_MEL",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "07-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-11-2023 10:36:09"
+        "download_time": "07-11-2023 10:51:09"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8453,6 +8453,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-07",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min9",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-11-07",
+        "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 1
     }
 ]    
