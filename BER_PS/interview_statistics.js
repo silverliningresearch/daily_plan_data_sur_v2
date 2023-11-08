1f965470cf95097092575e6dfb908db217e4ba98
unknown <lb@silverliningresearch.com>
2023-11-08 06:51:52 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "08-11-2023 07:36:06"
+        "download_time": "08-11-2023 07:51:06"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -14604,6 +14604,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-11-07",
         "quota_id": "VCE - easyJet Europe",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-08",
+        "quota_id": "HEL - Finnair",
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-08",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "08-11-2023 14:36:15"
+        "download_time": "08-11-2023 14:51:14"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8558,7 +8558,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-08",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-11-08",
