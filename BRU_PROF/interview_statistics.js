bf4dcf6b450da8b785164cf9228a3f57f2c5af97
unknown <lb@silverliningresearch.com>
2023-11-08 05:36:59 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "08-11-2023 06:21:02"
+        "download_time": "08-11-2023 06:36:00"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -35448,7 +35448,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-08",
         "quota_id": "SN2729-ZRH",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-08",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "08-11-2023 13:21:33"
+        "download_time": "08-11-2023 13:36:21"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8543,7 +8543,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-08",
         "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 3\u2666Airside\u2666#03-15\u2666HL\u2666Min20",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-11-08",
