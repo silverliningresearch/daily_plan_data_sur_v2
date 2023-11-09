5e484289aa548e63777fc3c432df6e8fd3a6b982
unknown <lb@silverliningresearch.com>
2023-11-09 02:36:53 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "09-11-2023 10:21:51"
+        "download_time": "09-11-2023 10:36:52"
     },
     {
         "InterviewDate": "01-11-2023",
