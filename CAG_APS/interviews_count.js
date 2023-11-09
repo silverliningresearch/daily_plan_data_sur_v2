fea5f901c07e5ff1d7f9b1a9c9dc529df6075698
unknown <lb@silverliningresearch.com>
2023-11-09 23:51:49 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "10-11-2023 07:36:53"
+        "download_time": "10-11-2023 07:51:48"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "10-11-2023 07:36:16"
+        "download_time": "10-11-2023 07:51:09"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -8754,6 +8754,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-09",
         "quota_id": "iStudio\u2666TERMINAL 3\u2666Airside\u2666#02-54\u2666EL\u2666Min9",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-11-10",
+        "quota_id": "Hudsons Cafe\u2666TERMINAL 3\u2666Airside\u2666#02-60\u2666FBA\u2666Min9",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-10",
