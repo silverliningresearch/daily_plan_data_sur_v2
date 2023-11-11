7630d2d774ebec02346690093c94fb4263eb0a88
unknown <lb@silverliningresearch.com>
2023-11-11 23:51:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "12-11-2023 07:36:59"
+        "download_time": "12-11-2023 07:51:57"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-11-2023 07:36:22"
+        "download_time": "12-11-2023 07:51:21"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9088,7 +9088,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-12",
         "quota_id": "Straits Food Village - Premises B (Pho Street, Subway, Kaveri)\u2666TERMINAL 2\u2666Airside\u2666#036-169\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     }
 ]    
 
