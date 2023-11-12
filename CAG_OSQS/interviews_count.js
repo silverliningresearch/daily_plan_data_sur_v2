6d7f80b5063e49a218e186248030405d54335dcd
unknown <lb@silverliningresearch.com>
2023-11-12 07:22:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-11-2023 15:06:28"
+        "download_time": "12-11-2023 15:21:34"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9143,7 +9143,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-12",
         "quota_id": "Travelex T2 Arrival Baggage Claim Hall South\u2666TERMINAL 2\u2666Airside\u2666#01-181\u2666MC\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-11-12",
