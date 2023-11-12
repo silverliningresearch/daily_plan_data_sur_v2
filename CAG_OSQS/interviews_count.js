089b954a88a8f44f0d28292d3f99fa6ceed05494
unknown <lb@silverliningresearch.com>
2023-11-12 03:07:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-11-2023 10:51:26"
+        "download_time": "12-11-2023 11:06:32"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9093,7 +9093,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-12",
         "quota_id": "NAM NAM\u2666TERMINAL 3\u2666Landside\u2666#02-91\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-12",
@@ -9107,6 +9107,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-11-12",
+        "quota_id": "The Green Market\u2666TERMINAL 2\u2666Airside\u2666#036-083\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-11-12",
         "quota_id": "Travelex T2 DT North (Satellite)\u2666TERMINAL 2\u2666Airside\u2666#02-150A\u2666MC\u2666Min8",
         "Number of interviews": 8
     },
@@ -9114,6 +9119,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-12",
         "quota_id": "Travelex T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209A\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2023-11-12",
+        "quota_id": "UOB T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#026-CTR-01\u2666MC\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
