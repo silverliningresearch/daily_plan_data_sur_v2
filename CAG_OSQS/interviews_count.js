bcc88d23438c5e66da9723fcce236bed386f3325
unknown <lb@silverliningresearch.com>
2023-11-13 02:52:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "13-11-2023 10:36:59"
+        "download_time": "13-11-2023 10:51:31"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9174,6 +9174,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-12",
         "quota_id": "UOB T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#026-CTR-01\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2023-11-13",
+        "quota_id": "LLAOLLAO / TP TEA / KRISPY KREME\u2666TERMINAL 3\u2666Landside\u2666#02-93\u2666LS\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
