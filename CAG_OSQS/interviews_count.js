b7ba0948f6bfd49b118e1cbaa04866e1e6214b7e
unknown <lb@silverliningresearch.com>
2023-11-13 03:07:06 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "13-11-2023 10:51:31"
+        "download_time": "13-11-2023 11:06:29"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9177,8 +9177,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-11-13",
+        "quota_id": "HEAVENLY WANG AND WANG NASI LEMAK\u2666TERMINAL 1\u2666Landside\u2666#02-03/04\u2666FBL\u2666Min8",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2023-11-13",
         "quota_id": "LLAOLLAO / TP TEA / KRISPY KREME\u2666TERMINAL 3\u2666Landside\u2666#02-93\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
