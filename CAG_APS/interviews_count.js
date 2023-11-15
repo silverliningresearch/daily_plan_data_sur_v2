f943ba873b1b25bbacd49ebfc651a8709a70587d
unknown <lb@silverliningresearch.com>
2023-11-15 00:22:17 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "15-11-2023 08:07:17"
+        "download_time": "15-11-2023 08:22:16"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1034,6 +1034,11 @@ let interview_statistics  = `[
         "InterviewDate": "15-11-2023",
         "quota_id": "T3_SQ_PVG",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "15-11-2023",
+        "quota_id": "T3_UK_DEL",
+        "Number of interviews": 2
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "15-11-2023 08:06:39"
+        "download_time": "15-11-2023 08:21:40"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9378,7 +9378,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-15",
         "quota_id": "Perfumes & Cosmetics (T1 DTW #02-25)\u2666TERMINAL 1\u2666Airside\u2666#02-25\u2666PC\u2666Min15",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-15",
