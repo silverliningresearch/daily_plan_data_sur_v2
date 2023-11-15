99c3d3e64aecbc4ffba0bbb4f38ff2101dccc2b6
unknown <lb@silverliningresearch.com>
2023-11-15 01:22:23 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "15-11-2023 09:07:20"
+        "download_time": "15-11-2023 09:22:21"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1014,6 +1014,11 @@ let interview_statistics  = `[
         "InterviewDate": "15-11-2023",
         "quota_id": "T1_SI_ICN",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "15-11-2023",
+        "quota_id": "T3_ID_YIA",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "15-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "15-11-2023 09:06:43"
+        "download_time": "15-11-2023 09:21:46"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9393,7 +9393,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-15",
         "quota_id": "Sunglass Hut (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-28\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-11-15",
