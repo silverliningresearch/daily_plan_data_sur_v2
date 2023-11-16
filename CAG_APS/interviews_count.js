8141dfad06cb8fa1ff0a1d8bc9891e913ec8fa3b
unknown <lb@silverliningresearch.com>
2023-11-16 01:22:24 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "16-11-2023 09:07:13"
+        "download_time": "16-11-2023 09:22:22"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "16-11-2023 09:06:38"
+        "download_time": "16-11-2023 09:21:46"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9542,6 +9542,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-11-16",
+        "quota_id": "Buk Chang Dong Soon Tofu\u2666TERMINAL 2\u2666Airside\u2666#02-K12\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-11-16",
         "quota_id": "Changi Recommends\u2666TERMINAL 2\u2666Airside\u2666#02-K18\u2666CR\u2666Min8",
         "Number of interviews": 2
     },
@@ -9553,7 +9558,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-16",
         "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
-        "Number of interviews": 14
+        "Number of interviews": 15
     }
 ]    
 
