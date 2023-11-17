b8ab37b7c5663e3cca67dc6f89b726dc13c1d78c
unknown <lb@silverliningresearch.com>
2023-11-17 19:07:19 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "17-11-2023 19:51:00"
+        "download_time": "17-11-2023 20:06:02"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -33668,7 +33668,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2023-11-17",
         "quota_id": "LIN-AZ",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-17",
@@ -33694,6 +33694,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-11-17",
         "quota_id": "NSI-SN",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2023-11-17",
+        "quota_id": "OSL-SN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-11-17",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "17-11-2023 19:51:00"
+        "download_time": "17-11-2023 20:06:02"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -36798,7 +36798,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-17",
         "quota_id": "AZ153-LIN",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-17",
@@ -36839,6 +36839,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-11-17",
         "quota_id": "SN203-BJL",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-17",
+        "quota_id": "SN2287-OSL",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-11-17",
