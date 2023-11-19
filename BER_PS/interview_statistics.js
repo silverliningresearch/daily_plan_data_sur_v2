915bac485b963e368aca37a47af29195d984b061
unknown <lb@silverliningresearch.com>
2023-11-19 14:37:27 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "19-11-2023 15:21:35"
+        "download_time": "19-11-2023 15:36:34"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -15193,7 +15193,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-19",
         "quota_id": "LHR - British Airways",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-19",
