be83d06a5cd6d3ef257652497e7b8b94772bbdbd
unknown <lb@silverliningresearch.com>
2023-11-19 13:37:28 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "19-11-2023 14:21:47"
+        "download_time": "19-11-2023 14:36:34"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -15178,7 +15178,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-19",
         "quota_id": "FCO - easyJet Europe",
-        "Number of interviews": 6
+        "Number of interviews": 12
     },
     {
         "InterviewDate": "2023-11-19",
@@ -15193,6 +15193,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-19",
         "quota_id": "MUC - Lufthansa",
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2023-11-19",
+        "quota_id": "VIE - Austrian Airlines",
         "Number of interviews": 10
     },
     {
--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "19-11-2023 14:21:20"
+        "download_time": "19-11-2023 14:36:10"
     },
     {
         "InterviewerID": 1,
@@ -2568,7 +2568,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 10,
         "Interview_Date": "2023-11-19",
-        "completed_interviews": 6
+        "completed_interviews": 8
     },
     {
         "InterviewerID": 11,
