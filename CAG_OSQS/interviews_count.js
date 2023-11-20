5d27bf2ccb47b5a7adba52bb4aaad0499abc5011
unknown <lb@silverliningresearch.com>
2023-11-20 09:37:33 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "20-11-2023 17:21:54"
+        "download_time": "20-11-2023 17:36:54"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9968,7 +9968,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-20",
         "quota_id": "Perfumes & Cosmetics (T2 DTC)\u2666TERMINAL 2\u2666Airside\u2666#02-177 (#026-103E)\u2666PC\u2666Min25",
-        "Number of interviews": 12
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2023-11-20",
@@ -9983,12 +9983,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-20",
         "quota_id": "Tumi\u2666TERMINAL 2\u2666Airside\u2666#02-184\u2666MP\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-20",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     }
 ]    
 
