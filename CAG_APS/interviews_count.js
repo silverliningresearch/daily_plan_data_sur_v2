1adb496893f59044ef1bca3cd5f8a356ec944369
unknown <lb@silverliningresearch.com>
2023-11-20 08:07:26 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "20-11-2023 15:52:38"
+        "download_time": "20-11-2023 16:07:25"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "20-11-2023 15:52:00"
+        "download_time": "20-11-2023 16:06:47"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9978,7 +9978,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-20",
         "quota_id": "Sunglass Hut\u2666TERMINAL 2\u2666Airside\u2666#02-189\u2666MP\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-20",
@@ -9988,7 +9988,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-20",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
