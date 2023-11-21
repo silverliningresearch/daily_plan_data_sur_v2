ad220eedaffbeddcb86ad676cdefc10a2633e5f3
unknown <lb@silverliningresearch.com>
2023-11-21 07:52:30 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "21-11-2023 08:36:05"
+        "download_time": "21-11-2023 08:51:12"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -34038,7 +34038,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2023-11-21",
         "quota_id": "LPA-SN",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-21",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "21-11-2023 15:36:45"
+        "download_time": "21-11-2023 15:51:52"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -9998,7 +9998,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-21",
         "quota_id": "Gassan Watches\u2666TERMINAL 1\u2666Airside\u2666#02-54\u2666LX\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-11-21",
@@ -10018,7 +10018,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-21",
         "quota_id": "Louis Vuitton\u2666TERMINAL 1\u2666Airside\u2666#02-50\u2666LX\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-11-21",
