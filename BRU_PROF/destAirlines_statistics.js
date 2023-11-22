76dbb0f124d45a45a4b292efc7c9cf88df843b50
unknown <lb@silverliningresearch.com>
2023-11-22 15:07:33 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "22-11-2023 15:51:12"
+        "download_time": "22-11-2023 16:06:14"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -34168,7 +34168,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2023-11-22",
         "quota_id": "AOE-TB",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-22",
@@ -34259,6 +34259,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-11-22",
         "quota_id": "MAD-IB",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-22",
+        "quota_id": "MUC-LH",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-11-22",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "22-11-2023 15:51:12"
+        "download_time": "22-11-2023 16:06:14"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -37437,6 +37437,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2023-11-22",
+        "quota_id": "LH2289-MUC",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-11-22",
         "quota_id": "SN2047-BHX",
         "Number of interviews": 7
     },
@@ -37508,7 +37513,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-11-22",
         "quota_id": "TB3313-AOE",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-22",
