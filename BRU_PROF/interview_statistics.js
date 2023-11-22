46885af8d7e54b890e237a4db8c515ef45c54efe
unknown <lb@silverliningresearch.com>
2023-11-22 22:37:43 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "22-11-2023 23:21:17"
+        "download_time": "22-11-2023 23:36:19"
     },
     {
         "InterviewDate": "2023-04-01",
