b23bcbff94a2efce1acf72f52811b57588118811
unknown <lb@silverliningresearch.com>
2023-11-23 11:37:54 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "23-11-2023 19:22:54"
+        "download_time": "23-11-2023 19:37:52"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1554,6 +1554,11 @@ let interview_statistics  = `[
         "InterviewDate": "23-11-2023",
         "quota_id": "T2_3U_TFU",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "23-11-2023",
+        "quota_id": "T4_3K_CGK",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "23-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "23-11-2023 19:22:15"
+        "download_time": "23-11-2023 19:37:14"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10198,7 +10198,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-23",
         "quota_id": "KOI THE\u2666TERMINAL 3\u2666Landside\u2666#B2-12\u2666FBL\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-11-23",
@@ -10243,7 +10243,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-23",
         "quota_id": "ROTIBOY\u2666TERMINAL 3\u2666Landside\u2666#B2-28\u2666FBL\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-11-23",
