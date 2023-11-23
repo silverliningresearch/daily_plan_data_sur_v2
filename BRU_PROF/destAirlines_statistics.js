ad1ec755d64b03921f901e2b3e57c3c27d9b5833
unknown <lb@silverliningresearch.com>
2023-11-23 10:37:38 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "23-11-2023 11:21:16"
+        "download_time": "23-11-2023 11:36:16"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "23-11-2023 18:21:59"
+        "download_time": "23-11-2023 18:36:58"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10168,7 +10168,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-23",
         "quota_id": "Beyond Fruit\u2666TERMINAL 3\u2666Landside\u2666#B2-30A\u2666FBL\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-11-23",
@@ -10198,7 +10198,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-23",
         "quota_id": "KOI THE\u2666TERMINAL 3\u2666Landside\u2666#B2-12\u2666FBL\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-11-23",
@@ -10258,7 +10258,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-23",
         "quota_id": "THEJELLYHEARTS\u2666TERMINAL 3\u2666Landside\u2666#B2-29\u2666FBL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-23",
