281e211b582c03a85c92cd24a9857eb57dd154b6
unknown <lb@silverliningresearch.com>
2023-11-24 14:52:49 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "24-11-2023 22:37:01"
+        "download_time": "24-11-2023 22:52:12"
     },
     {
         "InterviewDate": "2023-08-14",
