7f9b58510e2e4d350e9b91b89f47a3e35dd97e6f
unknown <lb@silverliningresearch.com>
2023-11-25 00:07:53 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "25-11-2023 07:51:59"
+        "download_time": "25-11-2023 08:07:13"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10529,6 +10529,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-25",
         "quota_id": "Noyah La Maison & Zero Degree (Premises A1)\u2666TERMINAL 4\u2666Airside\u2666#02-35 (beside Coffee Bean)\u2666FBA\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-25",
+        "quota_id": "Texas Chicken\u2666TERMINAL 4\u2666Airside\u2666#02-86\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-11-25",
