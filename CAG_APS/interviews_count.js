22606674a75f44fd976f0c326d207e5579f61814
unknown <lb@silverliningresearch.com>
2023-11-26 03:07:35 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "26-11-2023 10:52:35"
+        "download_time": "26-11-2023 11:07:33"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-11-2023 10:51:58"
+        "download_time": "26-11-2023 11:06:57"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10662,6 +10662,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-11-26",
+        "quota_id": "AVIS BUDGET CAR RENTAL\u2666TERMINAL 3\u2666Landside\u2666#01-K12\u2666LS\u2666Min5",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-11-26",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min8",
         "Number of interviews": 2
     },
@@ -10678,7 +10683,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-26",
         "quota_id": "Perfumes & Cosmetics (T1 DTC)\u2666TERMINAL 1\u2666Airside\u2666#02-37/38\u2666PC\u2666Min25",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-11-26",
