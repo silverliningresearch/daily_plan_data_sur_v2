2f63fd1b0d1ce04c08fd5740e0ae5888b5a5d03b
unknown <lb@silverliningresearch.com>
2023-11-26 02:37:46 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "26-11-2023 10:22:38"
+        "download_time": "26-11-2023 10:37:44"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-11-2023 10:22:01"
+        "download_time": "26-11-2023 10:37:06"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10662,6 +10662,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-11-26",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-11-26",
         "quota_id": "FAIRPRICE FINEST\u2666TERMINAL 3\u2666Landside\u2666#B2-10\u2666LS\u2666Min8",
         "Number of interviews": 1
     },
@@ -10669,6 +10674,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-26",
         "quota_id": "LT T1 DT Central\u2666TERMINAL 1\u2666Airside\u2666#02-53,02-46,02-46A\u2666LT\u2666Min25",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2023-11-26",
+        "quota_id": "Perfumes & Cosmetics (T1 DTC)\u2666TERMINAL 1\u2666Airside\u2666#02-37/38\u2666PC\u2666Min25",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-26",
