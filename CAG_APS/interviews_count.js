4234b9643afe1dbc14034caec0e4073b27db3d8c
unknown <lb@silverliningresearch.com>
2023-11-27 01:37:41 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "27-11-2023 09:23:05"
+        "download_time": "27-11-2023 09:37:40"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-11-2023 09:22:27"
+        "download_time": "27-11-2023 09:37:03"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10824,6 +10824,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-26",
         "quota_id": "ZONE X\u2666TERMINAL 3\u2666Landside\u2666#B2-23\u2666LS\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2023-11-27",
+        "quota_id": "Airport Pharmacy\u2666TERMINAL 4\u2666Airside\u2666#02-46\u2666PH\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-11-27",
