1caab88ea522d345085312411b13175f2683310c
unknown <lb@silverliningresearch.com>
2023-11-27 06:22:43 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "27-11-2023 07:06:32"
+        "download_time": "27-11-2023 07:21:19"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -34694,6 +34694,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-11-27",
         "quota_id": "IST-TK",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2023-11-27",
+        "quota_id": "MAN-SN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2023-11-27",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-11-2023 14:07:26"
+        "download_time": "27-11-2023 14:22:04"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -10838,7 +10838,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-27",
         "quota_id": "Bottega Veneta\u2666TERMINAL 2\u2666Airside\u2666#02-224\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-27",
@@ -10853,7 +10853,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-27",
         "quota_id": "Coach & Furla\u2666TERMINAL 4\u2666Airside\u2666#02-32\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-11-27",
@@ -10963,7 +10963,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-27",
         "quota_id": "Tod's\u2666TERMINAL 2\u2666Airside\u2666#02-225-01\u2666LX\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
