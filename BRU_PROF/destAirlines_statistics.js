eefe386ff4770f2441ab8ff94c64e81169cf590a
unknown <lb@silverliningresearch.com>
2023-11-28 08:37:45 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "28-11-2023 09:21:20"
+        "download_time": "28-11-2023 09:36:18"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -34794,6 +34794,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-11-28",
         "quota_id": "DJE-TB",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2023-11-28",
+        "quota_id": "FIH-SN",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-11-28",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "28-11-2023 16:22:07"
+        "download_time": "28-11-2023 16:37:07"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11048,7 +11048,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-28",
         "quota_id": "Lacoste\u2666TERMINAL 4\u2666Airside\u2666#02-49\u2666MP\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-11-28",
