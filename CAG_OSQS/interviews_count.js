e882b08bfaf450921b1c762242d4891e85f2d42e
unknown <lb@silverliningresearch.com>
2023-11-28 03:52:49 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "28-11-2023 11:37:12"
+        "download_time": "28-11-2023 11:52:12"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11043,7 +11043,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-28",
         "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 3\u2666Airside\u2666#02-K4\u2666FBA\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
