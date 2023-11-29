fb0fc5fb1b920883b987d4034bb955f1523256b1
unknown <lb@silverliningresearch.com>
2023-11-29 23:37:47 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "30-11-2023 00:21:25"
+        "download_time": "30-11-2023 00:36:21"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "30-11-2023 07:22:15"
+        "download_time": "30-11-2023 07:37:09"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11123,6 +11123,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-11-30",
         "quota_id": "McDonald's\u2666TERMINAL 2\u2666Airside\u2666#036-070\u2666FBA\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-30",
+        "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 1
     }
 ]    
