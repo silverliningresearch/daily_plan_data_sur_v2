6208f500eff7916148c6aca39b813160154baa88
unknown <lb@silverliningresearch.com>
2023-11-30 00:08:01 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2023",
         "quota_id": "T1_CA_PEK",
         "Number of interviews": 7,
-        "download_time": "30-11-2023 07:52:45"
+        "download_time": "30-11-2023 08:07:59"
     },
     {
         "InterviewDate": "01-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "30-11-2023 07:52:08"
+        "download_time": "30-11-2023 08:07:22"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11129,6 +11129,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-11-30",
         "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-11-30",
+        "quota_id": "The Cocoa Trees (T2 DTS, beside Montblanc)\u2666TERMINAL 2\u2666Airside\u2666#02-218\u2666CCD\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
