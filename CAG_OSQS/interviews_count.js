e2390d25a1cc2cbb2bc9a9ea4aee857773e8d13a
unknown <lb@silverliningresearch.com>
2023-12-02 09:06:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "02-12-2023 16:51:06"
+        "download_time": "02-12-2023 17:06:07"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11193,12 +11193,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-02",
         "quota_id": "8 BY BOTTLES & BOTTLES\u2666TERMINAL 3\u2666Landside\u2666#B2-51A\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-02",
         "quota_id": "CLOUD 9 STUDIO (SHORT TERM TENANCY)\u2666TERMINAL 3\u2666Landside\u2666#B2-44\u2666LS\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-12-02",
@@ -11208,7 +11208,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-02",
         "quota_id": "KAWATA HOUSE OF SOCKS\u2666TERMINAL 3\u2666Landside\u2666#B2-43\u2666LS\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-02",
@@ -11218,7 +11218,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-02",
         "quota_id": "SUGAR\u2666TERMINAL 3\u2666Landside\u2666#B2-48\u2666LS\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-12-02",
