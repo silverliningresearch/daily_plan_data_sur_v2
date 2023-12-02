e6095f887a9fc8ddc86cd128a9425e69decc616d
unknown <lb@silverliningresearch.com>
2023-12-02 03:51:47 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_KHN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "02-12-2023 11:36:44"
+        "download_time": "02-12-2023 11:51:46"
     },
     {
         "InterviewDate": "01-12-2023",
@@ -131,6 +131,12 @@ let interview_statistics  = `[
         "quota_id": "T2_TR_SUB",
         "interviewerID": "02",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "02-12-2023",
+        "quota_id": "T3_OD_KUL",
+        "interviewerID": "12",
+        "Number of interviews": 4
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "02-12-2023 11:36:09"
+        "download_time": "02-12-2023 11:51:11"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11197,6 +11197,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-12-02",
+        "quota_id": "EU YAN SANG\u2666TERMINAL 3\u2666Landside\u2666#B2-55\u2666LS\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-12-02",
         "quota_id": "KAWATA HOUSE OF SOCKS\u2666TERMINAL 3\u2666Landside\u2666#B2-43\u2666LS\u2666Min8",
         "Number of interviews": 1
     },
@@ -11218,6 +11223,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-02",
         "quota_id": "THE BLUES\u2666TERMINAL 3\u2666Landside\u2666#B2-36\u2666LS\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-12-02",
+        "quota_id": "UNITY\u2666TERMINAL 3\u2666Landside\u2666#B2-57\u2666LS\u2666Min8",
         "Number of interviews": 1
     }
 ]    
