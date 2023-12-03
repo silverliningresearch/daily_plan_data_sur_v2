295d355f11c0a015788e8713dd9fbf1abdb058d3
unknown <lb@silverliningresearch.com>
2023-12-03 07:06:43 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "03-12-2023 14:51:06"
+        "download_time": "03-12-2023 15:06:06"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11258,7 +11258,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-03",
         "quota_id": "Hudsons (DTS Gourmet Garden)\u2666TERMINAL 2\u2666Airside\u2666#02-178\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-12-03",
@@ -11273,7 +11273,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-03",
         "quota_id": "Jones The Grocer\u2666TERMINAL 2\u2666Landside\u2666#02-01\u2666FBL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-03",
@@ -11288,7 +11288,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-03",
         "quota_id": "Paris Baguette\u2666TERMINAL 2\u2666Landside\u2666#01-04\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-03",
