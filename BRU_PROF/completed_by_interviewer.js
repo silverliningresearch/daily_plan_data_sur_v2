0efc46192a7116956430a1cd58bebb980df2b252
unknown <lb@silverliningresearch.com>
2023-12-03 16:36:46 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "03-12-2023 17:20:49"
+        "download_time": "03-12-2023 17:35:49"
     },
     {
         "InterviewerID": 1,
@@ -2708,7 +2708,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 10,
         "Interview_Date": "2023-12-03",
-        "completed_interviews": 19
+        "completed_interviews": 25
     },
     {
         "InterviewerID": 11,
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -3,8 +3,8 @@ let interview_statistics  = `[
         "InterviewDate": "01-12-2023",
         "quota_id": "T1_TR_KHN",
         "interviewerID": "02",
-        "Number of interviews": 2,
-        "download_time": "04-12-2023 00:21:48"
+        "Number of interviews": 3,
+        "download_time": "04-12-2023 00:36:45"
     },
     {
         "InterviewDate": "01-12-2023",
@@ -17,12 +17,6 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_MYY",
         "interviewerID": "02",
         "Number of interviews": 3
-    },
-    {
-        "InterviewDate": "01-12-2023",
-        "quota_id": "T1_TRTR112_KHN",
-        "interviewerID": "02",
-        "Number of interviews": 1
     },
     {
         "InterviewDate": "01-12-2023",
@@ -46,13 +40,7 @@ let interview_statistics  = `[
         "InterviewDate": "01-12-2023",
         "quota_id": "T1_QF_MEL",
         "interviewerID": "12",
-        "Number of interviews": 4
-    },
-    {
-        "InterviewDate": "01-12-2023",
-        "quota_id": "T1_QF_MER",
-        "interviewerID": "12",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "01-12-2023",
