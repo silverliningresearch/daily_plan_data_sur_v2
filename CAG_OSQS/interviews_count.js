177932d8e79e6787c54dc580cd799bec6db7050c
unknown <lb@silverliningresearch.com>
2023-12-05 07:06:42 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "05-12-2023 14:51:07"
+        "download_time": "05-12-2023 15:06:07"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11373,7 +11373,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-05",
         "quota_id": "Airport Pharmacy\u2666TERMINAL 4\u2666Airside\u2666#02-46\u2666PH\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-12-05",
@@ -11388,7 +11388,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-05",
         "quota_id": "Charles & Keith\u2666TERMINAL 4\u2666Airside\u2666#02-30\u2666MP\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-05",
@@ -11433,7 +11433,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-05",
         "quota_id": "Sunglass Hut\u2666TERMINAL 4\u2666Airside\u2666#02-43\u2666MP\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-12-05",
@@ -11448,7 +11448,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-05",
         "quota_id": "acme de la vie\u2666TERMINAL 4\u2666Airside\u2666#02-45\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
