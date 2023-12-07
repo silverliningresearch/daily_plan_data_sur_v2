262588705cc9e5c0acc22c5d86e12f644d781c2e
unknown <lb@silverliningresearch.com>
2023-12-07 07:22:12 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "07-12-2023 08:06:06"
+        "download_time": "07-12-2023 08:21:15"
     },
     {
         "InterviewDate": "2023-01-10",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-12-2023 15:06:21"
+        "download_time": "07-12-2023 15:21:30"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11528,7 +11528,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-07",
@@ -11538,7 +11538,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "Premises B - Baggage Storage by Smarte Carte\u2666TERMINAL 1\u2666Airside\u2666#02-17\u2666BS\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-07",
