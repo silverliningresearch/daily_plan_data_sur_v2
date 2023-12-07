5a9089811b68b0d7c2aae784bd4651fb521c1b42
unknown <lb@silverliningresearch.com>
2023-12-07 04:21:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_BPN",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "07-12-2023 12:06:54"
+        "download_time": "07-12-2023 12:21:56"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1413,6 +1413,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "07-12-2023",
         "quota_id": "T4_AK_LGK",
+        "interviewerID": "12",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "07-12-2023",
+        "quota_id": "T4_CX_HKG",
         "interviewerID": "12",
         "Number of interviews": 5
     },
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-12-2023 12:06:18"
+        "download_time": "07-12-2023 12:21:19"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11509,6 +11509,11 @@ let interview_statistics  = `[
         "InterviewDate": "2023-12-07",
         "quota_id": "CTE T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-21\u2666MC\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2023-12-07",
+        "quota_id": "Fendi\u2666TERMINAL 1\u2666Airside\u2666#02-78\u2666LX\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-07",
