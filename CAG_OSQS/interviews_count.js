a340fd1ae86fd245eb092ec4624261db7f8a0020
unknown <lb@silverliningresearch.com>
2023-12-07 12:22:00 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-12-2023 20:06:19"
+        "download_time": "07-12-2023 20:21:19"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11533,7 +11533,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-07",
@@ -11543,7 +11543,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "Premises B - Baggage Storage by Smarte Carte\u2666TERMINAL 1\u2666Airside\u2666#02-17\u2666BS\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-07",
@@ -11558,7 +11558,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "UOB T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-20\u2666MC\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     }
 ]    
 
