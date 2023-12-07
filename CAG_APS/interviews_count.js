36aa333e9ba8a1d0e6b9c3e997bde34c426ed104
unknown <lb@silverliningresearch.com>
2023-12-07 23:22:10 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_BPN",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "08-12-2023 07:07:00"
+        "download_time": "08-12-2023 07:22:07"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1619,6 +1619,12 @@ let interview_statistics  = `[
         "quota_id": "T3_SQ_MEL",
         "interviewerID": "12",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "08-12-2023",
+        "quota_id": "T3_SQ_ICN",
+        "interviewerID": "02",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "09-11-2023",
