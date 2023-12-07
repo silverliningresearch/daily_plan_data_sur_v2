7a4c56b84a694852ac364da831354ac9f456a0ef
unknown <lb@silverliningresearch.com>
2023-12-07 03:36:57 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_BPN",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "07-12-2023 11:22:08"
+        "download_time": "07-12-2023 11:36:55"
     },
     {
         "InterviewDate": "01-11-2023",
@@ -1397,6 +1397,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_PKU",
         "interviewerID": "03 ",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "07-12-2023",
+        "quota_id": "T4_AK_KUL",
+        "interviewerID": "12",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "08-11-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-12-2023 11:21:31"
+        "download_time": "07-12-2023 11:36:18"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11508,12 +11508,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "CTE T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-21\u2666MC\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-12-07",
         "quota_id": "Premises B - Baggage Storage by Smarte Carte\u2666TERMINAL 1\u2666Airside\u2666#02-17\u2666BS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-12-07",
