a1fc034ab1d297ce70aaba737343a254a7312fc3
unknown <lb@silverliningresearch.com>
2023-12-08 06:07:30 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "08-12-2023 06:50:56"
+        "download_time": "08-12-2023 07:06:03"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "08-12-2023 13:51:21"
+        "download_time": "08-12-2023 14:06:34"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11568,7 +11568,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-08",
         "quota_id": "Aerotel\u2666TERMINAL 1\u2666Airside\u2666#03-139&140\u2666HL\u2666Min20",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-08",
@@ -11583,7 +11583,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-08",
         "quota_id": "T1 iSC Departure counter\u2666TERMINAL 1\u2666Airside\u2666NA\u2666PS\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     }
 ]    
 
