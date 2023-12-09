f9c85650e075fb397aa95c0e7e68f8614dad3a8f
unknown <lb@silverliningresearch.com>
2023-12-09 04:52:00 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "09-12-2023 12:36:22"
+        "download_time": "09-12-2023 12:51:21"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11663,7 +11663,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "CLIMB@T3\u2666TERMINAL 3\u2666Landside\u2666#B3-01/02\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-12-09",
@@ -11693,7 +11693,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "SWENSEN'S\u2666TERMINAL 3\u2666Landside\u2666#B2-31/ 32\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-09",
@@ -11718,7 +11718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "iTEA Premium\u2666TERMINAL 3\u2666Landside\u2666#B2-30\u2666FBL\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     }
 ]    
 
