c5a4de0621281fafa688061d17ecfffaf8833c36
unknown <lb@silverliningresearch.com>
2023-12-09 06:07:02 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "09-12-2023 06:50:56"
+        "download_time": "09-12-2023 07:05:55"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "09-12-2023 13:51:22"
+        "download_time": "09-12-2023 14:06:20"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11663,7 +11663,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "CLIMB@T3\u2666TERMINAL 3\u2666Landside\u2666#B3-01/02\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-12-09",
@@ -11703,7 +11703,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "THEJELLYHEARTS\u2666TERMINAL 3\u2666Landside\u2666#B2-29\u2666FBL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-09",
@@ -11723,7 +11723,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-09",
         "quota_id": "ZONE X\u2666TERMINAL 3\u2666Landside\u2666#B2-23\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-09",
