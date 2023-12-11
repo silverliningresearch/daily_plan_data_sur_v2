7b4c8ac42f3946f0e0470a1cf7c39f621b2c7063
unknown <lb@silverliningresearch.com>
2023-12-11 15:22:13 +0000

    update new data

--- BER_PS/interview_statistics.js
+++ BER_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-01-10",
         "quota_id": "Avi - EL AL- Israel Airlines",
         "Number of interviews": 1,
-        "download_time": "11-12-2023 16:06:01"
+        "download_time": "11-12-2023 16:21:14"
     },
     {
         "InterviewDate": "2023-01-10",
@@ -15962,6 +15962,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2023-12-11",
+        "quota_id": "ATH - Aegean Airlines",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2023-12-11",
         "quota_id": "BCN - easyJet Europe",
         "Number of interviews": 2
     },
@@ -15969,6 +15974,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-11",
         "quota_id": "CDG - Air France",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2023-12-11",
+        "quota_id": "ESB - Turkish Airlines",
+        "Number of interviews": 3
     }
 ]    
 
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "11-12-2023 16:05:51"
+        "download_time": "11-12-2023 16:20:53"
     },
     {
         "InterviewDate": "2023-04-01",
