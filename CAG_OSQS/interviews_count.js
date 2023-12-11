64e0186c6b08814b33a45490dade997f3d0b4aa7
unknown <lb@silverliningresearch.com>
2023-12-11 11:22:15 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "11-12-2023 19:06:19"
+        "download_time": "11-12-2023 19:21:33"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -11908,7 +11908,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-11",
         "quota_id": "Balenciaga\u2666TERMINAL 1\u2666Airside\u2666#02-81\u2666LX\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-12-11",
@@ -11963,7 +11963,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-11",
         "quota_id": "Scarves & Glitter, House of Kashkha\u2666TERMINAL 1\u2666Airside\u2666#02-K46\u2666MP\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-11",
