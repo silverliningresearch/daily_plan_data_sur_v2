768f1501b20d3999d73af8a8e947023cf8297295
unknown <lb@silverliningresearch.com>
2023-12-12 01:22:12 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_KHN",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "12-12-2023 09:06:49"
+        "download_time": "12-12-2023 09:22:06"
     },
     {
         "InterviewDate": "01-12-2023",
@@ -1373,6 +1373,12 @@ let interview_statistics  = `[
         "quota_id": "T1_JL_NRT",
         "interviewerID": "02",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "12-12-2023",
+        "quota_id": "T1_SC_SYD",
+        "interviewerID": "02",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "12-12-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-12-2023 09:06:14"
+        "download_time": "12-12-2023 09:21:30"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -12023,7 +12023,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-12",
         "quota_id": "Sprint-Cass (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-36\u2666EL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-12",
