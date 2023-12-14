ad2441a6f6d70733e91bc03a5c26c3cc98eb0c04
unknown <lb@silverliningresearch.com>
2023-12-14 07:07:07 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "14-12-2023 14:51:20"
+        "download_time": "14-12-2023 15:06:20"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -12358,7 +12358,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-14",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#02-K4\u2666CR\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-12-14",
@@ -12373,7 +12373,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-14",
         "quota_id": "Discover Singapore (Premises B) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-33/34\u2666SV\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-14",
@@ -12393,7 +12393,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-14",
         "quota_id": "Guardian\u2666TERMINAL 1\u2666Airside\u2666#02-69\u2666PH\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-14",
@@ -12423,6 +12423,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-14",
         "quota_id": "Longchamp\u2666TERMINAL 1\u2666Airside\u2666#02-77\u2666MP\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2023-12-14",
+        "quota_id": "MA MUM\u2666TERMINAL 4\u2666Landside\u2666#01-04\u2666FBL\u2666Min8",
         "Number of interviews": 4
     },
     {
