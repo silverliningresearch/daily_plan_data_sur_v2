f227d97c8cb6dee357b74ddd07a59ed94ca9494f
unknown <lb@silverliningresearch.com>
2023-12-16 12:52:14 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "16-12-2023 16:37:07"
+        "download_time": "16-12-2023 16:52:07"
     },
     {
         "InterviewDate": "2023-12-09",
--- MLE_PS/interview_statistics.js
+++ MLE_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-14",
         "quota_id": "4-AK073",
         "Number of interviews": 1,
-        "download_time": "16-12-2023 16:37:11"
+        "download_time": "16-12-2023 16:52:11"
     },
     {
         "InterviewDate": "2023-12-14",
@@ -73,7 +73,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-12-16",
         "quota_id": "6-5W7106",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-16",
