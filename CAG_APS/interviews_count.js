061ab7ace5bbdca59b507860d1a6d3dd3496e8c6
unknown <lb@silverliningresearch.com>
2023-12-17 04:22:19 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_KHN",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "17-12-2023 12:07:05"
+        "download_time": "17-12-2023 12:22:10"
     },
     {
         "InterviewDate": "01-12-2023",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "17-12-2023 12:06:27"
+        "download_time": "17-12-2023 12:21:31"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -12838,7 +12838,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2023-12-17",
@@ -12863,12 +12863,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "Jamba\u2666TERMINAL 1\u2666Landside\u2666#02-05\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "NAM NAM\u2666TERMINAL 3\u2666Landside\u2666#02-91\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-17",
@@ -12888,7 +12888,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "STARBUCKS COFFEE\u2666TERMINAL 3\u2666Landside\u2666#02-90\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-17",
