ec1b66b675a9354aaa8e60a925ece194d7b7f7ce
unknown <lb@silverliningresearch.com>
2023-12-17 23:07:18 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "18-12-2023 02:52:11"
+        "download_time": "18-12-2023 03:07:10"
     },
     {
         "InterviewDate": "2023-12-09",
