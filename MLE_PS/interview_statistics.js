38438b48bcdd5a01510b71dc0bca22c9e134d1e0
unknown <lb@silverliningresearch.com>
2023-12-17 11:07:52 +0000

    update new data

--- MLE_PS/interview_statistics.js
+++ MLE_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-14",
         "quota_id": "4-AK073",
         "Number of interviews": 1,
-        "download_time": "17-12-2023 14:52:02"
+        "download_time": "17-12-2023 15:07:49"
     },
     {
         "InterviewDate": "2023-12-14",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-11",
         "quota_id": "LH 2442",
         "Number of interviews": 1,
-        "download_time": "17-12-2023 11:52:03"
+        "download_time": "17-12-2023 12:07:51"
     },
     {
         "InterviewDate": "2023-12-12",
