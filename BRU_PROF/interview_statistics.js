106d46cfe1a833a81ebac4ddc1251fcf356e4933
unknown <lb@silverliningresearch.com>
2023-12-17 06:07:13 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "17-12-2023 06:50:49"
+        "download_time": "17-12-2023 07:05:50"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "17-12-2023 13:51:26"
+        "download_time": "17-12-2023 14:06:27"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -12833,7 +12833,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "BABA NYONYA\u2666TERMINAL 3\u2666Landside\u2666#03-33\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-12-17",
@@ -12873,7 +12873,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-17",
         "quota_id": "Crown Prince Kitchenette\u2666TERMINAL 3\u2666Landside\u2666#03-42/43\u2666FBL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-17",
