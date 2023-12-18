84d6ddfc9757933fd8d717e1269438b152409719
unknown <lb@silverliningresearch.com>
2023-12-18 17:37:08 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "18-12-2023 21:23:09"
+        "download_time": "18-12-2023 21:37:02"
     },
     {
         "InterviewDate": "2023-12-09",
@@ -93,7 +93,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-12-18",
         "quota_id": "AI984",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-12-18",
@@ -103,7 +103,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-12-18",
         "quota_id": "FZ1709",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2023-12-18",
--- MLE_PS/interview_statistics.js
+++ MLE_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-14",
         "quota_id": "4-AK073",
         "Number of interviews": 1,
-        "download_time": "18-12-2023 21:23:12"
+        "download_time": "18-12-2023 21:37:05"
     },
     {
         "InterviewDate": "2023-12-14",
@@ -228,6 +228,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2023-12-18",
         "quota_id": "1-Qr675",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-12-18",
+        "quota_id": "1-SQ437",
         "Number of interviews": 1
     },
     {
