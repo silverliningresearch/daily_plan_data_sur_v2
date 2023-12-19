dc095df9dc5a4ac4fd34ba4a4a94aa42378cdf86
unknown <lb@silverliningresearch.com>
2023-12-19 08:52:34 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "19-12-2023 09:35:50"
+        "download_time": "19-12-2023 09:50:55"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -37158,7 +37158,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2023-12-19",
         "quota_id": "EBB-SN",
-        "Number of interviews": 5
+        "Number of interviews": 11
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "19-12-2023 16:36:28"
+        "download_time": "19-12-2023 16:51:44"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -13128,7 +13128,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-19",
         "quota_id": "Burger King\u2666TERMINAL 3\u2666Airside\u2666#03-13\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2023-12-19",
