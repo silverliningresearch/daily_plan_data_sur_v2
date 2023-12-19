a45f00c54aa559acffed8b9a395a2b64c8733ffa
unknown <lb@silverliningresearch.com>
2023-12-19 23:07:27 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-16",
         "quota_id": "FRA - Lufthansa",
         "Number of interviews": 1,
-        "download_time": "19-12-2023 23:51:12"
+        "download_time": "20-12-2023 00:06:14"
     }
 ]    
 
