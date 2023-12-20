5bad173e403f0d85b557b9d5da98976ca714dd27
unknown <lb@silverliningresearch.com>
2023-12-20 03:52:34 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "20-12-2023 11:36:40"
+        "download_time": "20-12-2023 11:51:40"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -13242,13 +13242,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2023-12-20",
+        "quota_id": "Boost Juice\u2666TERMINAL 2\u2666Airside\u2666#02-K16\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-12-20",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2023-12-20",
         "quota_id": "CRAVE\u2666TERMINAL 2\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-12-20",
@@ -13258,7 +13263,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-20",
         "quota_id": "Chef Wei\u2666TERMINAL 2\u2666Airside\u2666#02-K6\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-20",
@@ -13268,7 +13273,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-20",
         "quota_id": "SIXT RENT A CAR\u2666TERMINAL 1\u2666Landside\u2666#01-K5\u2666LS\u2666Min5",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-20",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "20-12-2023 07:37:22"
+        "download_time": "20-12-2023 07:52:25"
     },
     {
         "InterviewDate": "2023-12-09",
@@ -259,6 +259,11 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-20",
         "quota_id": "EK243",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2023-12-20",
+        "quota_id": "EK261",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-20",
