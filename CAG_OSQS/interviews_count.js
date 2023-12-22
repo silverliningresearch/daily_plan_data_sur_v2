1d95b10d0d842323e5bd36203bd4fc5b506e3dce
unknown <lb@silverliningresearch.com>
2023-12-22 06:37:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "22-12-2023 14:22:17"
+        "download_time": "22-12-2023 14:36:29"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -13453,12 +13453,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-22",
         "quota_id": "Luvenus\u2666TERMINAL 2\u2666Airside\u2666#02-154\u2666JW\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2023-12-22",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2023-12-22",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "22-12-2023 10:23:21"
+        "download_time": "22-12-2023 10:37:12"
     },
     {
         "InterviewDate": "2023-12-09",
