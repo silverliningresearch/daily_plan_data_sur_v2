0a121033e08ae1367546b7dd5241cdad0a56249d
unknown <lb@silverliningresearch.com>
2023-12-25 01:22:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "25-12-2023 09:06:47"
+        "download_time": "25-12-2023 09:21:58"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -13718,7 +13718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-25",
         "quota_id": "Guardian (Premises B) (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-192\u2666PH\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2023-12-25",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-09",
         "quota_id": "EK821",
         "Number of interviews": 5,
-        "download_time": "25-12-2023 05:07:32"
+        "download_time": "25-12-2023 05:22:44"
     },
     {
         "InterviewDate": "2023-12-09",
