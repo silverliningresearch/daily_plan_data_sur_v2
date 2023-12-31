2057d008397939c9f80cfdc63c30954f4d604083
unknown <lb@silverliningresearch.com>
2023-12-31 00:08:54 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-16",
         "quota_id": "FRA - Lufthansa",
         "Number of interviews": 1,
-        "download_time": "31-12-2023 00:51:02"
+        "download_time": "31-12-2023 01:07:15"
     },
     {
         "InterviewDate": "2023-12-20",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "31-12-2023 07:51:29"
+        "download_time": "31-12-2023 08:07:50"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -14163,12 +14163,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2023-12-31",
         "quota_id": "Pret A Manger\u2666TERMINAL 3\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2023-12-31",
         "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA\u2666Min25",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2023-12-31",
+        "quota_id": "UOB T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-K10\u2666MC\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
