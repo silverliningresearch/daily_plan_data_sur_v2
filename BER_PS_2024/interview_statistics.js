3fb831495f1e66c4330a35134784e9b5419cc749
unknown <lb@silverliningresearch.com>
2023-12-31 11:51:22 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,12 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "FRA - Lufthansa",
         "Number of interviews": 1,
-        "download_time": "31-12-2023 12:35:50"
+        "download_time": "31-12-2023 12:50:50"
+    },
+    {
+        "InterviewDate": "2023-12-31",
+        "quota_id": "GYD - Azerbaijan Airlines",
+        "Number of interviews": 1
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "DMM",
         "AirlineCode": "EK",
         "Number of interviews": 5,
-        "download_time": "31-12-2023 15:36:15"
+        "download_time": "31-12-2023 15:51:13"
     },
     {
         "InterviewDate": "2023-12-09",
