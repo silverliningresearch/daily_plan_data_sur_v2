730eaf1110670553ddaf0ba68487b8bd93b0bba7
unknown <lb@silverliningresearch.com>
2024-01-04 00:07:10 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T3_GF_BKK",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "04-01-2024 07:52:21"
+        "download_time": "04-01-2024 08:06:54"
     },
     {
         "InterviewDate": "02-01-2024",
@@ -262,7 +262,7 @@ let interview_statistics  = `[
         "InterviewDate": "04-01-2024",
         "quota_id": "T1_SQ_AKL",
         "interviewerID": "02",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "04-01-2024",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "DMM",
         "AirlineCode": "EK",
         "Number of interviews": 5,
-        "download_time": "04-01-2024 03:52:25"
+        "download_time": "04-01-2024 04:06:57"
     },
     {
         "InterviewDate": "2023-12-09",
