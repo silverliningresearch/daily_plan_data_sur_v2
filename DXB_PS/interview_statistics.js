52d7d970d0db1e8810feffbe24cb7fc5f9f6c455
unknown <lb@silverliningresearch.com>
2024-01-05 21:37:22 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "DMM",
         "AirlineCode": "EK",
         "Number of interviews": 5,
-        "download_time": "06-01-2024 01:22:33"
+        "download_time": "06-01-2024 01:37:10"
     },
     {
         "InterviewDate": "2023-12-09",
