430f08a4679de2d2018d17df02676ec2a5e81f04
unknown <lb@silverliningresearch.com>
2024-01-05 03:37:03 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "05-01-2024 11:21:41"
+        "download_time": "05-01-2024 11:36:13"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -14507,6 +14507,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-01-05",
+        "quota_id": "BURGER KING\u2666TERMINAL 1\u2666Landside\u2666#03-23\u2666FBL\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-05",
         "quota_id": "Braun Buffel\u2666TERMINAL 2\u2666Airside\u2666#02-245\u2666MP\u2666Min8",
         "Number of interviews": 2
     },
@@ -14533,7 +14538,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-05",
         "quota_id": "LT T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#02-401\u2666LT\u2666Min15",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-01-05",
@@ -14554,6 +14559,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-05",
         "quota_id": "Swarovski\u2666TERMINAL 2\u2666Airside\u2666#02-236\u2666MP\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-05",
+        "quota_id": "TERRACE CHINESE KITCHEN\u2666TERMINAL 1\u2666Landside\u2666#03-22\u2666FBL\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-01-05",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "DMM",
         "AirlineCode": "EK",
         "Number of interviews": 5,
-        "download_time": "05-01-2024 07:22:22"
+        "download_time": "05-01-2024 07:36:53"
     },
     {
         "InterviewDate": "2023-12-09",
