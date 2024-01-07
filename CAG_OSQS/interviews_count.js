0e822bdef0cf33606027b8d290fbd81642234985
unknown <lb@silverliningresearch.com>
2024-01-07 01:37:02 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-01-2024 09:21:36"
+        "download_time": "07-01-2024 09:36:15"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -14759,6 +14759,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-07",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-07",
+        "quota_id": "CHEERS\u2666TERMINAL 1\u2666Landside\u2666#02-05A\u2666LS\u2666Min8",
+        "Number of interviews": 3
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "DMM",
         "AirlineCode": "EK",
         "Number of interviews": 5,
-        "download_time": "07-01-2024 05:22:15"
+        "download_time": "07-01-2024 05:36:53"
     },
     {
         "InterviewDate": "2023-12-09",
