4f9673a8335cc90f4881b8259bfc227a46ae76a0
unknown <lb@silverliningresearch.com>
2024-01-07 01:52:14 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "07-01-2024 09:36:15"
+        "download_time": "07-01-2024 09:51:21"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -14748,7 +14748,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-07",
         "quota_id": "CHANGI RECOMMENDS (T1 L2 Landside)\u2666TERMINAL 1\u2666Landside\u2666#02-K39\u2666CR\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-01-07",
@@ -14764,6 +14764,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-07",
         "quota_id": "CHEERS\u2666TERMINAL 1\u2666Landside\u2666#02-05A\u2666LS\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-01-07",
+        "quota_id": "CTE T1 DCI Central\u2666TERMINAL 1\u2666Landside\u2666#02-02\u2666MC\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-07",
+        "quota_id": "UOB T1 DCI Central\u2666TERMINAL 1\u2666Landside\u2666#02-01\u2666MC\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
