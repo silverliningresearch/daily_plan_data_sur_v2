7016e83a1c6edd5cefe1716ed8e0d8b2ac482665
unknown <lb@silverliningresearch.com>
2024-01-08 09:22:46 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "08-01-2024 10:06:07"
+        "download_time": "08-01-2024 10:21:28"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -204,6 +204,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-08",
         "quota_id": "FRA - Lufthansa",
         "Number of interviews": 16
+    },
+    {
+        "InterviewDate": "2024-01-08",
+        "quota_id": "LHR - British Airways",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-01-08",
--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "08-01-2024 10:05:59"
+        "download_time": "08-01-2024 10:21:09"
     },
     {
         "InterviewerID": 1,
@@ -2193,7 +2193,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 5,
         "Interview_Date": "2024-01-08",
-        "completed_interviews": 25
+        "completed_interviews": 27
     },
     {
         "InterviewerID": 6,
