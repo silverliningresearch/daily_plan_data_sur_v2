40f88a6cf4a3c160bf9bb4fe45eeb097a2b3f2bc
unknown <lb@silverliningresearch.com>
2024-01-09 01:22:27 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T3_GF_BKK",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "09-01-2024 09:07:04"
+        "download_time": "09-01-2024 09:22:14"
     },
     {
         "InterviewDate": "02-01-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "09-01-2024 09:06:29"
+        "download_time": "09-01-2024 09:21:38"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -14972,8 +14972,23 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-01-09",
+        "quota_id": "Discover Singapore (Premises B) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-33/34\u2666SV\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-09",
         "quota_id": "Fragrance\u2666TERMINAL 1\u2666Airside\u2666#02-42N\u2666CCD\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-01-09",
+        "quota_id": "JOLLIBEAN\u2666TERMINAL 3\u2666Landside\u2666#B2-13\u2666FBL\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-09",
+        "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-01-09",
@@ -14988,7 +15003,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-09",
         "quota_id": "WH Smith - Premises B (Air) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-41C\u2666OW\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-01-09",
+        "quota_id": "YAKUN FAMILY CAFE & BEARD PAPA'S\u2666TERMINAL 3\u2666Landside\u2666#B2-07\u2666FBL\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
