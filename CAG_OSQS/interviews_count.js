b4a1224e59c15a75b5e19cdc7bdbbf8ce6e2ab6f
unknown <lb@silverliningresearch.com>
2024-01-11 02:07:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "11-01-2024 09:51:14"
+        "download_time": "11-01-2024 10:06:16"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -15278,7 +15278,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-11",
         "quota_id": "Premises C - Baggage Storage by Smarte Carte\u2666TERMINAL 2\u2666Landside\u2666#01-01 (#016-024)\u2666BS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-01-11",
@@ -15299,6 +15299,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-11",
         "quota_id": "Travelex T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209A\u2666MC\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-11",
+        "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min8",
+        "Number of interviews": 4
     }
 ]    
 
