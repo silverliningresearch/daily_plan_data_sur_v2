e7bd8f21c588a55919f7b0d292a80cb5e5a1e1af
unknown <lb@silverliningresearch.com>
2024-01-12 02:52:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "12-01-2024 10:36:24"
+        "download_time": "12-01-2024 10:51:52"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -15457,8 +15457,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-01-12",
+        "quota_id": "LT T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-68\u2666LT\u2666Min15",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-12",
         "quota_id": "O' COFFEE CLUB\u2666TERMINAL 4\u2666Landside\u2666#02-13\u2666FBL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-01-12",
@@ -15478,7 +15483,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-12",
         "quota_id": "Sprint-Cass (T1 DTE)\u2666TERMINAL 1\u2666Airside\u2666#02-63\u2666EL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-01-12",
