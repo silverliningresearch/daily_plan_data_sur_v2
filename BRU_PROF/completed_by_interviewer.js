7dd54576a5bdc24983dabe350ef71e7e6419c2a6
unknown <lb@silverliningresearch.com>
2024-01-13 13:22:43 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "13-01-2024 14:05:55"
+        "download_time": "13-01-2024 14:21:01"
     },
     {
         "InterviewerID": 1,
@@ -3643,7 +3643,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 13,
         "Interview_Date": "2024-01-13",
-        "completed_interviews": 5
+        "completed_interviews": 9
     },
     {
         "InterviewerID": 14,
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "EK-SIN",
         "InterviewerID": "7339101",
         "Number of interviews": 1,
-        "download_time": "13-01-2024 17:07:21"
+        "download_time": "13-01-2024 17:22:37"
     },
     {
         "InterviewDate": "2023-12-14",
@@ -4172,6 +4172,24 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-01-13",
+        "quota_id": "QR-DOH",
+        "InterviewerID": "7339101",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-13",
+        "quota_id": "EK-DXB",
+        "InterviewerID": "7570708",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-13",
+        "quota_id": "QR-DOH",
+        "InterviewerID": "7570708",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-13",
         "quota_id": "6E-BLR",
         "InterviewerID": "7714016",
         "Number of interviews": 4
@@ -4234,7 +4252,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-13",
         "quota_id": "EK-DXB",
         "InterviewerID": "7763039",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-01-13",
@@ -4253,6 +4271,12 @@ let interview_statistics = `[
         "quota_id": "KC-ALA",
         "InterviewerID": "9444533",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-13",
+        "quota_id": "QR-DOH",
+        "InterviewerID": "9444533",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-01-13",
