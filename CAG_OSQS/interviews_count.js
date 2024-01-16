93dc5ff01ee8f84203c25b47722834593bddd909
unknown <lb@silverliningresearch.com>
2024-01-16 06:22:29 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "16-01-2024 14:06:31"
+        "download_time": "16-01-2024 14:21:32"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -15943,7 +15943,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-16",
         "quota_id": "Coffee Cart\u2666TERMINAL 3\u2666Airside\u2666#02-CC (outside A13)\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-16",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "EK-SIN",
         "InterviewerID": "7339101",
         "Number of interviews": 1,
-        "download_time": "16-01-2024 10:07:25"
+        "download_time": "16-01-2024 10:22:24"
     },
     {
         "InterviewDate": "2023-12-14",
@@ -4828,6 +4828,12 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-15",
         "quota_id": "UNKNOWN-DOH",
         "InterviewerID": "9900023",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-16",
+        "quota_id": "DE-FRA",
+        "InterviewerID": "7227736",
         "Number of interviews": 1
     },
     {
