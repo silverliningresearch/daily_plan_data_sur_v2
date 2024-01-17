308d101c150aefc851a6ba273da94f4f040c63ef
unknown <lb@silverliningresearch.com>
2024-01-17 04:24:05 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "17-01-2024 05:06:42"
+        "download_time": "17-01-2024 05:21:11"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -44538,7 +44538,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-01-17",
         "quota_id": "TP645-LIS",
-        "Number of interviews": 1
+        "Number of interviews": 5
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "17-01-2024 12:07:28"
+        "download_time": "17-01-2024 12:22:29"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -16109,6 +16109,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-17",
         "quota_id": "UOB T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#026-CTR-01\u2666MC\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-01-17",
+        "quota_id": "UOB T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#026-CTR-03\u2666MC\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
