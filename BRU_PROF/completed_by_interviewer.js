0d4d395ea0c1e82f585f50b1282e1f977482676e
unknown <lb@silverliningresearch.com>
2024-01-21 15:22:53 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "21-01-2024 16:06:00"
+        "download_time": "21-01-2024 16:21:05"
     },
     {
         "InterviewerID": 1,
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "21-01-2024 19:07:27"
+        "download_time": "21-01-2024 19:22:36"
     },
     {
         "InterviewDate": "2024-01-07",
