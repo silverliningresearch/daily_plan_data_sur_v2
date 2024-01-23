a25b4c7336f3072717cd109784834a0a4e5d212f
unknown <lb@silverliningresearch.com>
2024-01-23 01:52:36 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "23-01-2024 09:36:26"
+        "download_time": "23-01-2024 09:51:32"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -16763,7 +16763,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-23",
         "quota_id": "Guardian (Premises B) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-29\u2666PH\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-23",
@@ -16793,6 +16793,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-23",
         "quota_id": "Sprint-Cass (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-30\u2666EL\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-01-23",
+        "quota_id": "TWG Tea Company (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
         "Number of interviews": 1
     },
     {
