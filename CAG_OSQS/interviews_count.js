235262bde4142ced1b7335e215cd91ca46ce8af7
unknown <lb@silverliningresearch.com>
2024-01-23 04:22:30 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "23-01-2024 12:06:26"
+        "download_time": "23-01-2024 12:21:26"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -16788,7 +16788,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-23",
         "quota_id": "Louis Vuitton\u2666TERMINAL 3\u2666Airside\u2666#02-36A(F) & 03-07A(F)\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-01-23",
@@ -16818,7 +16818,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-23",
         "quota_id": "TWG Tea Company (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-23",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "23-01-2024 08:07:10"
+        "download_time": "23-01-2024 08:22:10"
     },
     {
         "InterviewDate": "2024-01-07",
