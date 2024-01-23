deda8062cff3dae303fe0a30b4cacec48285a2a7
unknown <lb@silverliningresearch.com>
2024-01-23 13:23:02 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "23-01-2024 14:06:06"
+        "download_time": "23-01-2024 14:21:21"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T3_GF_BKK",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "23-01-2024 21:07:21"
+        "download_time": "23-01-2024 21:22:34"
     },
     {
         "InterviewDate": "02-01-2024",
