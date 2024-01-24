cdd2ed727b28a4a414b97bbe640ac32de3b67f30
unknown <lb@silverliningresearch.com>
2024-01-24 02:22:59 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "24-01-2024 10:06:22"
+        "download_time": "24-01-2024 10:21:26"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -16852,8 +16852,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-01-24",
+        "quota_id": "Hard Rock Cafe\u2666TERMINAL 2\u2666Airside\u2666#02-177\u2666FBA\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-24",
         "quota_id": "Hudsons (DTS Gourmet Garden)\u2666TERMINAL 2\u2666Airside\u2666#02-178\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-24",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "24-01-2024 03:07:23"
+        "download_time": "24-01-2024 03:22:55"
     },
     {
         "InterviewDate": "2023-12-21",
