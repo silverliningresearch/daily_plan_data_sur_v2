4c8803daace7fb3194b988bc59d64d52fbdcf632
unknown <lb@silverliningresearch.com>
2024-01-24 07:07:22 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "24-01-2024 07:51:01"
+        "download_time": "24-01-2024 08:05:55"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -919,6 +919,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-24",
         "quota_id": "BRU - Brussels Airlines",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-24",
+        "quota_id": "CGN - Eurowings",
+        "Number of interviews": 5
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "24-01-2024 14:51:30"
+        "download_time": "24-01-2024 15:06:22"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -16883,7 +16883,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-24",
         "quota_id": "Hard Rock Cafe\u2666TERMINAL 2\u2666Airside\u2666#02-177\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-24",
@@ -16918,7 +16918,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-24",
         "quota_id": "Pret A Manger\u2666TERMINAL 2\u2666Airside\u2666#02-165\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-01-24",
@@ -16933,7 +16933,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-24",
         "quota_id": "The Satay Club\u2666TERMINAL 2\u2666Airside\u2666#02-169\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-01-24",
