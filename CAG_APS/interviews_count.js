8074333fbbbbd96c6f3fa346e6fa3da78c6e63a7
unknown <lb@silverliningresearch.com>
2024-01-25 23:25:47 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T3_GF_BKK",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "26-01-2024 07:07:11"
+        "download_time": "26-01-2024 07:25:19"
     },
     {
         "InterviewDate": "02-01-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-01-2024 07:06:32"
+        "download_time": "26-01-2024 07:24:34"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17179,6 +17179,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-26",
         "quota_id": "Premises E - Baggage Storage by Smarte Carte\u2666TERMINAL 3\u2666Landside\u2666#01-40\u2666BS\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-01-26",
+        "quota_id": "UOB T3 Arr Mtg Hall North\u2666TERMINAL 3\u2666Landside\u2666#01-K16\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
