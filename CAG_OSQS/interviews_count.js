5b89226f04bd99c587a68bcf5a09a5b042aadf89
unknown <lb@silverliningresearch.com>
2024-01-26 05:07:39 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "26-01-2024 12:51:32"
+        "download_time": "26-01-2024 13:06:34"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17203,7 +17203,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-26",
         "quota_id": "STARHUB\u2666TERMINAL 3\u2666Landside\u2666#01-K11\u2666CR\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-01-26",
@@ -17214,6 +17214,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-26",
         "quota_id": "TRAVELLERS\u2666TERMINAL 3\u2666Landside\u2666#01-K17\u2666CR\u2666Min8",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-01-26",
+        "quota_id": "The Cocoa Trees (Premises A2) (T1 DTE)\u2666TERMINAL 1\u2666Airside\u2666#02-62\u2666CCD\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-01-26",
