f67c6e9dbfdac052225e1bc0ab376d2b24c638a3
unknown <lb@silverliningresearch.com>
2024-01-26 23:22:52 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "27-01-2024 00:06:02"
+        "download_time": "27-01-2024 00:21:16"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "27-01-2024 07:06:33"
+        "download_time": "27-01-2024 07:21:47"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17313,7 +17313,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-27",
         "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-01-27",
@@ -17323,7 +17323,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-27",
         "quota_id": "Ray-Ban\u2666TERMINAL 3\u2666Airside\u2666#02-49A\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     }
 ]    
 
