4163276e64d2eeb6ca5523ef240b4c4d922a629a
unknown <lb@silverliningresearch.com>
2024-01-27 17:23:39 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "27-01-2024 18:05:56"
+        "download_time": "27-01-2024 18:21:40"
     },
     {
         "InterviewerID": 1,
@@ -863,7 +863,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2024-01-27",
-        "completed_interviews": 31
+        "completed_interviews": 32
     },
     {
         "InterviewerID": 3,
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "27-01-2024 18:05:56"
+        "download_time": "27-01-2024 18:21:40"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -41499,6 +41499,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-01-27",
         "quota_id": "BCN-FR",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-01-27",
+        "quota_id": "CMN-3O",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-01-27",
