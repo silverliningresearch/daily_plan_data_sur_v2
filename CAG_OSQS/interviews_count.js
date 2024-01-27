96e86e38d6c8acdd49658b5bc2455de9b71db846
unknown <lb@silverliningresearch.com>
2024-01-27 23:52:48 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "28-01-2024 07:36:35"
+        "download_time": "28-01-2024 07:51:36"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17452,13 +17452,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-01-28",
+        "quota_id": "Bacha Coffee (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-52\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-01-28",
         "quota_id": "Eureka\u2666TERMINAL 3\u2666Airside\u2666#02-K13R\u2666CCD\u2666Min8",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-01-28",
         "quota_id": "Hudsons Cafe\u2666TERMINAL 3\u2666Airside\u2666#02-60\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-01-28",
@@ -17473,7 +17478,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-01-28",
         "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-01-28",
