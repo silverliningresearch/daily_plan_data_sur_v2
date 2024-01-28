875f30da8f78dddc330d2decdecebe90c6860b45
unknown <lb@silverliningresearch.com>
2024-01-28 23:22:47 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "29-01-2024 07:06:26"
+        "download_time": "29-01-2024 07:21:53"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17564,6 +17564,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "iStudio\u2666TERMINAL 4\u2666Airside\u2666#02-52\u2666EL\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-01-29",
+        "quota_id": "Tip Top Curry Puff\u2666TERMINAL 1\u2666Airside\u2666#02-K11N\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
