18e1445e0b57f2ac61327ff8ef211b6bfcdf2660
unknown <lb@silverliningresearch.com>
2024-01-29 19:23:40 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "29-01-2024 20:05:57"
+        "download_time": "29-01-2024 20:21:06"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -1213,7 +1213,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-01-29",
         "quota_id": "LUX - Luxair",
-        "Number of interviews": 10
+        "Number of interviews": 14
     },
     {
         "InterviewDate": "2024-01-29",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "29-01-2024 23:07:10"
+        "download_time": "29-01-2024 23:23:28"
     },
     {
         "InterviewDate": "2024-01-07",
