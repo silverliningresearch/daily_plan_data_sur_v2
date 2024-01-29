e8e70c6a5811dedcfc629615854b9b539eb7d5bb
unknown <lb@silverliningresearch.com>
2024-01-29 02:37:30 +0000

    update new data

--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "29-01-2024 03:23:01"
+        "download_time": "29-01-2024 03:37:25"
     },
     {
         "InterviewDate": "2023-12-21",
