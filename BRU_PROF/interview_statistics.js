8783240ffcfb3580bdba5b84ccc5d7a1080e0fc8
unknown <lb@silverliningresearch.com>
2024-01-30 01:22:48 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "30-01-2024 02:05:50"
+        "download_time": "30-01-2024 02:20:57"
     },
     {
         "InterviewDate": "2023-04-01",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "30-01-2024 05:07:10"
+        "download_time": "30-01-2024 05:22:36"
     },
     {
         "InterviewDate": "2024-01-07",
