8426769e3e9970038623f496c7933ed1d36238da
unknown <lb@silverliningresearch.com>
2024-01-31 22:52:37 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 06:36:24"
+        "download_time": "01-02-2024 06:51:41"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17639,6 +17639,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "YAKUN FAMILY CAFE & BEARD PAPA'S\u2666TERMINAL 3\u2666Landside\u2666#B2-07\u2666FBL\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-01",
+        "quota_id": "Jamie's Deli\u2666TERMINAL 1\u2666Airside\u2666#02-40N\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "31-01-2024 23:37:13"
+        "download_time": "31-01-2024 23:52:33"
     },
     {
         "InterviewDate": "2024-01-28",
