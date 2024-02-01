c7cc879491e5ab88a248b42bf5af81d71a932c89
unknown <lb@silverliningresearch.com>
2024-02-01 03:08:01 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 10:51:27"
+        "download_time": "01-02-2024 11:07:03"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17648,7 +17648,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-01",
         "quota_id": "Ippudo Express\u2666TERMINAL 1\u2666Airside\u2666#02-K17\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-02-01",
@@ -17673,7 +17673,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-01",
         "quota_id": "Plaza Premium Lounge\u2666TERMINAL 1\u2666Airside\u2666#03-03\u2666HL\u2666Min20",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-02-01",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 03:52:15"
+        "download_time": "01-02-2024 04:07:55"
     },
     {
         "InterviewDate": "2023-12-21",
