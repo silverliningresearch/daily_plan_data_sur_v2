9df2c712ef0b51b6443311c5d6b1b9ffe5e32e03
unknown <lb@silverliningresearch.com>
2024-02-01 15:37:23 +0000

    update new data

--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "QR-FCO",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 16:22:53"
+        "download_time": "01-02-2024 16:37:22"
     },
     {
         "InterviewDate": "2024-01-29",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "01-02-2024 19:22:38"
+        "download_time": "01-02-2024 19:37:12"
     },
     {
         "InterviewDate": "2024-01-07",
