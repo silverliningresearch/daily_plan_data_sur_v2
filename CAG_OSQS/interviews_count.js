67c2e7ee8276407b3bfab44840e424ae26c9fab8
unknown <lb@silverliningresearch.com>
2024-02-01 03:37:22 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 11:22:03"
+        "download_time": "01-02-2024 11:36:27"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17643,7 +17643,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-01",
         "quota_id": "Crave The Original Adam Rd Nasi Lemak by Selera Rasa\u2666TERMINAL 1\u2666Airside\u2666#02-K18\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-02-01",
@@ -17683,6 +17683,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-01",
         "quota_id": "Starbucks Coffee (Premises A)\u2666TERMINAL 1\u2666Airside\u2666#02-66\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-01",
+        "quota_id": "Starbucks Coffee (Premises B)\u2666TERMINAL 1\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
         "Number of interviews": 1
     },
     {
