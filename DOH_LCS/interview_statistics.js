4abdee9e69d86903e5499ab6b521689ee95a2dc3
unknown <lb@silverliningresearch.com>
2024-02-01 15:07:39 +0000

    update new data

--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "QR-FCO",
         "Number of interviews": 1,
-        "download_time": "01-02-2024 15:52:24"
+        "download_time": "01-02-2024 16:07:35"
     },
     {
         "InterviewDate": "2024-01-29",
