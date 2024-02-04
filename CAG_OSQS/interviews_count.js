648a474771e5ca273ce3afb0d71917b95a5b3399
unknown <lb@silverliningresearch.com>
2024-02-04 03:37:21 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2023-08-14",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u266602-32\u2666OS\u2666Min6",
         "Number of interviews": 1,
-        "download_time": "04-02-2024 11:21:48"
+        "download_time": "04-02-2024 11:36:29"
     },
     {
         "InterviewDate": "2023-08-14",
@@ -17923,7 +17923,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-04",
         "quota_id": "EU YAN SANG\u2666TERMINAL 3\u2666Landside\u2666#B2-55\u2666LS\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-02-04",
@@ -17943,7 +17943,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-04",
         "quota_id": "UNITY\u2666TERMINAL 3\u2666Landside\u2666#B2-57\u2666LS\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "QR-FCO",
         "Number of interviews": 1,
-        "download_time": "04-02-2024 04:22:41"
+        "download_time": "04-02-2024 04:37:20"
     },
     {
         "InterviewDate": "2024-01-29",
