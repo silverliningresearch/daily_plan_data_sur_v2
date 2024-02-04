e78a32a4b5d102007d61f56dc33c39a76dfb59b3
unknown <lb@silverliningresearch.com>
2024-02-04 15:52:29 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "04-02-2024 16:35:55"
+        "download_time": "04-02-2024 16:50:54"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -42717,6 +42717,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-02-04",
+        "quota_id": "CMN-AT",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-04",
         "quota_id": "CPH-SN",
         "Number of interviews": 6
     },
@@ -42794,6 +42799,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "ROB-SN",
         "Number of interviews": 13
+    },
+    {
+        "InterviewDate": "2024-02-04",
+        "quota_id": "VIE-OS",
+        "Number of interviews": 1
     }
 ]    
 
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "04-02-2024 16:35:54"
+        "download_time": "04-02-2024 16:50:54"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -46817,6 +46817,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-02-04",
+        "quota_id": "AT833-CMN",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-04",
         "quota_id": "BA397-LHR",
         "Number of interviews": 3
     },
@@ -46849,6 +46854,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "MS726-CAI",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-04",
+        "quota_id": "OS354-VIE",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-04",
