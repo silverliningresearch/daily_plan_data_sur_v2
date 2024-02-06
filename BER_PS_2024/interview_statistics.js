0ce9293499b96bddcda2d6edc2b723f9f76f6d0a
unknown <lb@silverliningresearch.com>
2024-02-06 19:21:21 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "06-02-2024 20:04:59"
+        "download_time": "06-02-2024 20:19:59"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -1544,6 +1544,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-06",
         "quota_id": "LHR - British Airways",
         "Number of interviews": 38
+    },
+    {
+        "InterviewDate": "2024-02-06",
+        "quota_id": "LUX - Luxair",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-06",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "06-02-2024 20:04:51"
+        "download_time": "06-02-2024 20:19:50"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -47324,6 +47324,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-06",
         "quota_id": "SN243-ROB",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-02-06",
+        "quota_id": "SN2629-HAM",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-02-06",
