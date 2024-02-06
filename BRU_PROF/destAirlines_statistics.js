d65221714f887295b597cad93ade3db8261afc2e
unknown <lb@silverliningresearch.com>
2024-02-06 22:06:38 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "06-02-2024 22:49:52"
+        "download_time": "06-02-2024 23:05:16"
     },
     {
         "InterviewDate": "2023-04-01",
