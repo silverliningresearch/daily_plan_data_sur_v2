f7e04d1de9ece10e7dfcf369b3a4f18257ac79a7
unknown <lb@silverliningresearch.com>
2024-02-07 02:50:58 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-02-2024 10:35:07"
+        "download_time": "07-02-2024 10:50:08"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -567,6 +567,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-02-07",
+        "quota_id": "Chloe\u2666TERMINAL 2\u2666Airside\u2666#02-210\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-07",
         "quota_id": "Gassan Watches (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-208\u2666LX\u2666Min8",
         "Number of interviews": 1
     },
@@ -589,6 +594,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-07",
         "quota_id": "T1 iSC Departure counter\u2666TERMINAL 1\u2666Airside\u2666NA\u2666PS\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-02-07",
+        "quota_id": "WH Smith (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-207\u2666OW\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
