2edfecba8ef1531823ce934fae64913057ff6939
unknown <lb@silverliningresearch.com>
2024-02-07 05:06:05 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-02-2024 12:50:18"
+        "download_time": "07-02-2024 13:05:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -568,7 +568,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-07",
         "quota_id": "Chloe\u2666TERMINAL 2\u2666Airside\u2666#02-210\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-02-07",
@@ -578,12 +578,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-07",
         "quota_id": "Hermes\u2666TERMINAL 2\u2666Airside\u2666#02-211\u2666LX\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-02-07",
         "quota_id": "LT T1 Arrival East\u2666TERMINAL 1\u2666Airside\u2666#01-10F\u2666LT\u2666Min15",
-        "Number of interviews": 8
+        "Number of interviews": 12
     },
     {
         "InterviewDate": "2024-02-07",
@@ -603,7 +603,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-07",
         "quota_id": "Pomellato\u2666TERMINAL 2\u2666Airside\u2666#02-212\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-07",
