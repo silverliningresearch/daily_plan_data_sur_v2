db6fba31478bed7735e0dd67f7d875c3dc262c76
unknown <lb@silverliningresearch.com>
2024-02-07 15:06:00 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "QR-LHR",
         "Number of interviews": 1,
-        "download_time": "07-02-2024 18:51:01"
+        "download_time": "07-02-2024 19:05:59"
     }
 ]    
 
