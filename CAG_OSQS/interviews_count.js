ce9aed78cf1d0a64bd6caddb40837d7bfee695ea
unknown <lb@silverliningresearch.com>
2024-02-08 02:21:00 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-02-2024 10:05:41"
+        "download_time": "08-02-2024 10:20:11"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -643,11 +643,16 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-08",
         "quota_id": "BEE CHENG HIANG\u2666TERMINAL 3\u2666Airside\u2666#02-64\u2666CCD\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-08",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K11\u2666CR\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-02-08",
+        "quota_id": "Discover Singapore (Premises E) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-63\u2666SV\u2666Min8",
         "Number of interviews": 1
     },
     {
@@ -668,7 +673,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-08",
         "quota_id": "UOB T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-K7\u2666MC\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     }
 ]    
 
--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "QR-FCO",
         "Number of interviews": 1,
-        "download_time": "08-02-2024 03:06:39"
+        "download_time": "08-02-2024 03:20:58"
     },
     {
         "InterviewDate": "2024-01-29",
