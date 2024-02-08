8e703a907a09999b41bcb2e8dcbe257087110796
unknown <lb@silverliningresearch.com>
2024-02-08 22:36:14 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "09-02-2024 02:20:54"
+        "download_time": "09-02-2024 02:36:03"
     },
     {
         "InterviewDate": "2024-01-07",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "08-02-2024 23:20:58"
+        "download_time": "08-02-2024 23:36:06"
     },
     {
         "InterviewDate": "2023-12-21",
