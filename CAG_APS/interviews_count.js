b0d8e6f00559011dfc7a56984c8b0c127de64732
unknown <lb@silverliningresearch.com>
2024-02-09 02:35:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 5,
-        "download_time": "09-02-2024 10:20:46"
+        "download_time": "09-02-2024 10:35:45"
     },
     {
         "InterviewDate": "01-02-2024",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "09-02-2024 06:20:49"
+        "download_time": "09-02-2024 06:35:48"
     },
     {
         "InterviewDate": "2024-01-07",
