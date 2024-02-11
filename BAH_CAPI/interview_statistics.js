3141dd660c7139c03df3112085943127e7ea8928
unknown <lb@silverliningresearch.com>
2024-02-11 01:21:20 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "QR-LHR",
         "Number of interviews": 1,
-        "download_time": "11-02-2024 05:06:38"
+        "download_time": "11-02-2024 05:21:19"
     }
 ]    
 
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "11-02-2024 02:05:38"
+        "download_time": "11-02-2024 02:20:14"
     },
     {
         "InterviewDate": "2023-12-31",
