48ddec8bafbf4316aaa487ec39062e95ef7ccdb2
unknown <lb@silverliningresearch.com>
2024-02-12 03:21:24 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 5,
-        "download_time": "12-02-2024 11:06:02"
+        "download_time": "12-02-2024 11:21:06"
     },
     {
         "InterviewDate": "01-02-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-02-2024 11:05:23"
+        "download_time": "12-02-2024 11:20:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1072,6 +1072,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-02-12",
+        "quota_id": "Garrett Gold\u2666TERMINAL 1\u2666Airside\u2666#02-32N\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-12",
         "quota_id": "House of Samsonite\u2666TERMINAL 1\u2666Airside\u2666#02-29\u2666MP\u2666Min8",
         "Number of interviews": 1
     },
@@ -1083,7 +1088,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-12",
         "quota_id": "Sunglass Hut (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-28\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-12",
@@ -1098,7 +1103,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-12",
         "quota_id": "WH Smith - Premises B (Air) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-41C\u2666OW\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
