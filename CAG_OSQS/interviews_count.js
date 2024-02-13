660f6bf81f9e06ac8a51f9ae236c744ddce10935
unknown <lb@silverliningresearch.com>
2024-02-13 11:21:27 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-02-2024 19:06:10"
+        "download_time": "13-02-2024 19:20:32"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1143,17 +1143,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-13",
         "quota_id": "Dunkin\u2019 Donuts\u2666TERMINAL 2\u2666Airside\u2666#02-K14\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-02-13",
         "quota_id": "GOPIZZA\u2666TERMINAL 2\u2666Airside\u2666#02-K15\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-02-13",
         "quota_id": "LiHO TEA\u2666TERMINAL 2\u2666Airside\u2666#02-K13\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-02-13",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "13-02-2024 12:06:55"
+        "download_time": "13-02-2024 12:21:19"
     },
     {
         "InterviewDate": "2023-12-21",
@@ -1534,6 +1534,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-13",
         "quota_id": "KL-AMS",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-02-13",
+        "quota_id": "OS-BER",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-13",
