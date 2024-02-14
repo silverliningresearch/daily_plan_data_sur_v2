deb59f262e010a00c2ac6a3677159063987a502f
unknown <lb@silverliningresearch.com>
2024-02-14 23:21:54 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "15-02-2024 00:05:05"
+        "download_time": "15-02-2024 00:20:18"
     },
     {
         "InterviewDate": "2023-04-01",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "15-02-2024 00:06:15"
+        "download_time": "15-02-2024 00:21:48"
     },
     {
         "InterviewDate": "2023-12-21",
