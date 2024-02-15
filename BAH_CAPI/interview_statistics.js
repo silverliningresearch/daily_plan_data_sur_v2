6d77ed9e44db4e6ad4952eb83bc1faebc3478c64
unknown <lb@silverliningresearch.com>
2024-02-15 01:51:24 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-04",
         "quota_id": "QR-LHR",
         "Number of interviews": 1,
-        "download_time": "15-02-2024 05:36:19"
+        "download_time": "15-02-2024 05:51:23"
     },
     {
         "InterviewDate": "2024-02-11",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-02-2024 09:35:30"
+        "download_time": "15-02-2024 09:50:34"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1294,6 +1294,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-14",
         "quota_id": "acme de la vie\u2666TERMINAL 4\u2666Airside\u2666#02-45\u2666MP\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-02-15",
+        "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
+        "Number of interviews": 1
     }
 ]    
 
