d939145fc42bb57741568850cd63d7597d2926e3
unknown <lb@silverliningresearch.com>
2024-02-18 03:50:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-02-2024 11:34:59"
+        "download_time": "18-02-2024 11:49:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1592,6 +1592,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-02-18",
+        "quota_id": "FAIRPRICE FINEST\u2666TERMINAL 3\u2666Landside\u2666#B2-10\u2666LS\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-18",
         "quota_id": "HEAVENLY WANG\u2666TERMINAL 3\u2666Landside\u2666#01-25\u2666FBL\u2666Min8",
         "Number of interviews": 10
     },
@@ -1613,7 +1618,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-18",
         "quota_id": "PARADISE DYNASTY\u2666TERMINAL 3\u2666Landside\u2666#03-32\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-02-18",
@@ -1624,6 +1629,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-18",
         "quota_id": "STARBUCKS COFFEE\u2666TERMINAL 3\u2666Landside\u2666#02-90\u2666FBL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-02-18",
+        "quota_id": "T3B2 CSO Counter\u2666TERMINAL 3\u2666Landside\u2666NA\u2666PS\u2666Min25",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-02-18",
