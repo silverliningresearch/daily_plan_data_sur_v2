3d2fb9893cf9cc0edb00272c695d6f9c66689c65
unknown <lb@silverliningresearch.com>
2024-02-19 01:21:09 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "19-02-2024 09:05:00"
+        "download_time": "19-02-2024 09:20:00"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1674,6 +1674,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-18",
         "quota_id": "ZONE X\u2666TERMINAL 3\u2666Landside\u2666#B2-23\u2666LS\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-02-19",
+        "quota_id": "International Food Hall\u2666TERMINAL 4\u2666Airside\u2666#02-228N\u2666FBA\u2666Min25",
+        "Number of interviews": 2
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "19-02-2024 02:05:42"
+        "download_time": "19-02-2024 02:20:42"
     },
     {
         "InterviewDate": "2023-12-21",
