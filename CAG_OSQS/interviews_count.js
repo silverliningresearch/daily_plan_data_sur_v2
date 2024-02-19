345fe1c566e9d43ab6b061a2601d085e2c4a13cd
unknown <lb@silverliningresearch.com>
2024-02-19 02:05:46 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "19-02-2024 09:50:01"
+        "download_time": "19-02-2024 10:05:00"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1678,7 +1678,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-19",
         "quota_id": "International Food Hall\u2666TERMINAL 4\u2666Airside\u2666#02-228N\u2666FBA\u2666Min25",
-        "Number of interviews": 11
+        "Number of interviews": 14
+    },
+    {
+        "InterviewDate": "2024-02-19",
+        "quota_id": "Michael Kors\u2666TERMINAL 1\u2666Airside\u2666#02-76\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-19",
+        "quota_id": "Pedro\u2666TERMINAL 1\u2666Airside\u2666#02-74\u2666MP\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "19-02-2024 02:50:43"
+        "download_time": "19-02-2024 03:05:40"
     },
     {
         "InterviewDate": "2023-12-21",
