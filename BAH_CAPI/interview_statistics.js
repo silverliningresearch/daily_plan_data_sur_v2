44f0b59a502cedcec10e6fea92c491be94aa1151
unknown <lb@silverliningresearch.com>
2024-02-20 00:05:52 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "20-02-2024 03:50:47"
+        "download_time": "20-02-2024 04:05:51"
     },
     {
         "InterviewDate": "2024-02-18",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 5,
-        "download_time": "20-02-2024 07:50:37"
+        "download_time": "20-02-2024 08:05:40"
     },
     {
         "InterviewDate": "01-02-2024",
@@ -1991,6 +1991,12 @@ let interview_statistics  = `[
         "quota_id": "T3_SQ_SFO",
         "interviewerID": "O6",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "20-02-2024",
+        "quota_id": "T2_AI_MAA",
+        "interviewerID": "02",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "20-02-2024",
