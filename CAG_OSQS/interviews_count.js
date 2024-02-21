4dfebe6c9b69bc7a2fb3747836842ca32b5cfa6b
unknown <lb@silverliningresearch.com>
2024-02-21 04:05:50 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-02-2024 11:50:02"
+        "download_time": "21-02-2024 12:05:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -1973,7 +1973,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-21",
         "quota_id": "Hudsons Cafe\u2666TERMINAL 3\u2666Airside\u2666#02-60\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-21",
@@ -1988,6 +1988,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-21",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-21",
+        "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min8",
         "Number of interviews": 1
     },
     {
@@ -2014,6 +2019,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-21",
         "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 3\u2666Airside\u2666#02-K4\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-02-21",
+        "quota_id": "The Kitchen by Wolfgang Puck\u2666TERMINAL 3\u2666Airside\u2666#02-42\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
