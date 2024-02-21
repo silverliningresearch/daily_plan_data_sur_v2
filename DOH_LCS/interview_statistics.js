c0fc4ebd8339e6ca73e344ff40541e6ff82f29d8
unknown <lb@silverliningresearch.com>
2024-02-21 01:35:49 +0000

    update new data

--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-16",
         "quota_id": "QR-JED",
         "Number of interviews": 2,
-        "download_time": "21-02-2024 04:21:09"
+        "download_time": "21-02-2024 04:35:47"
     },
     {
         "InterviewDate": "2024-02-17",
@@ -269,6 +269,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-21",
         "quota_id": "EY-AUH",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-02-21",
+        "quota_id": "G9-SHJ",
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-02-21",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "21-02-2024 02:20:56"
+        "download_time": "21-02-2024 02:35:43"
     },
     {
         "InterviewDate": "2023-12-21",
