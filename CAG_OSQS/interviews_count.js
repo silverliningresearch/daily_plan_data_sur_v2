09a9d50482d99b3adcaca9b51fbcdd31bf5b29b6
unknown <lb@silverliningresearch.com>
2024-02-23 12:20:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-02-2024 20:05:06"
+        "download_time": "23-02-2024 20:20:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -2222,6 +2222,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-02-23",
+        "quota_id": "Bacha Coffee (DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-195\u2666CCD\u2666Min9",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-23",
         "quota_id": "CANTON PARADISE CONGEE & NOODLE BAR & LE SHRIMP RAMEN\u2666TERMINAL 3\u2666Landside\u2666#B2-49/50\u2666FBL\u2666Min8",
         "Number of interviews": 8
     },
@@ -2288,7 +2293,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-23",
         "quota_id": "Moncler\u2666TERMINAL 2\u2666Airside\u2666#02-225-03\u2666LX\u2666Min9",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-02-23",
--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-16",
         "quota_id": "QR-JED",
         "Number of interviews": 2,
-        "download_time": "23-02-2024 15:05:51"
+        "download_time": "23-02-2024 15:20:51"
     },
     {
         "InterviewDate": "2024-02-17",
@@ -494,6 +494,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-23",
         "quota_id": "QR-DXB",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-02-23",
+        "quota_id": "QR-FCO",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-23",
