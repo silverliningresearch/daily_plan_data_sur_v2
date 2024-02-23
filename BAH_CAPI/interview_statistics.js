3c46efde898804514782675cf69c4460f0353f3c
unknown <lb@silverliningresearch.com>
2024-02-23 21:06:03 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "24-02-2024 00:50:55"
+        "download_time": "24-02-2024 01:06:02"
     },
     {
         "InterviewDate": "2024-02-18",
@@ -413,7 +413,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-02-24",
         "quota_id": "GF-KHI",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "24-02-2024 00:50:46"
+        "download_time": "24-02-2024 01:05:51"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -1190,7 +1190,7 @@ let interview_statistics = `[
         "Flight": "EK701",
         "DestCode": "MRU",
         "AirlineCode": "EK",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
