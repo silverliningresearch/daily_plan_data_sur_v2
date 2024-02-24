795d91f7f10ed80dd7d4f7dd9f4a2df8ccdc4398
unknown <lb@silverliningresearch.com>
2024-02-24 05:21:15 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "24-02-2024 09:05:56"
+        "download_time": "24-02-2024 09:21:13"
     },
     {
         "InterviewDate": "2024-02-18",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "24-02-2024 09:05:48"
+        "download_time": "24-02-2024 09:20:53"
     },
     {
         "InterviewDate": "2024-01-07",
