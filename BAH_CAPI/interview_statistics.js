ccd50fc1ac3492ae7d5bf95f4884e5da1e91ac9b
unknown <lb@silverliningresearch.com>
2024-02-24 02:36:08 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "24-02-2024 06:20:55"
+        "download_time": "24-02-2024 06:36:07"
     },
     {
         "InterviewDate": "2024-02-18",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-02-2024 10:20:07"
+        "download_time": "24-02-2024 10:35:16"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -2359,6 +2359,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-23",
         "quota_id": "UOB T3 DCI Central\u2666TERMINAL 3\u2666Landside\u2666#02-82\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-02-24",
+        "quota_id": "Kenangan Coffee\u2666TERMINAL 2\u2666Landside\u2666#02-12\u2666FBL\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
