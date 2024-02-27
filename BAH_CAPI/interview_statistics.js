868423eeed26d5330d1c1d6a86a3f27e7db34201
unknown <lb@silverliningresearch.com>
2024-02-27 01:06:07 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "27-02-2024 04:51:10"
+        "download_time": "27-02-2024 05:06:06"
     },
     {
         "InterviewDate": "2024-02-18",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-02-2024 08:50:15"
+        "download_time": "27-02-2024 09:05:16"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -2704,6 +2704,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-27",
         "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-02-27",
+        "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-02-27",
