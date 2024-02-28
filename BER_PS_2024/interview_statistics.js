b1402bf3aae07b9fb36dcd58ef95e58a9e46e63e
unknown <lb@silverliningresearch.com>
2024-02-28 19:06:45 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "28-02-2024 19:49:59"
+        "download_time": "28-02-2024 20:05:10"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -2594,6 +2594,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-28",
         "quota_id": "ATH - Aegean Airlines",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-02-28",
+        "quota_id": "BEG - Wizz Air",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-28",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "28-02-2024 19:49:48"
+        "download_time": "28-02-2024 20:04:50"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -46284,6 +46284,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-02-28",
         "quota_id": "YUL-AC",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-02-28",
+        "quota_id": "ZAG-OU",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-28",
