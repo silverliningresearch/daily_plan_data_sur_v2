8cb68a984886f4c3d988911018501330ff14d4e9
unknown <lb@silverliningresearch.com>
2024-02-28 03:51:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-02-2024 11:35:26"
+        "download_time": "28-02-2024 11:50:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -2918,7 +2918,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-28",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#02-K4\u2666CR\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-02-28",
@@ -2928,7 +2928,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-02-28",
         "quota_id": "Giordano\u2666TERMINAL 2\u2666Airside\u2666#026-096\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-02-28",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "28-02-2024 07:36:06"
+        "download_time": "28-02-2024 07:51:05"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -1254,7 +1254,7 @@ let interview_statistics = `[
         "Flight": "EK127",
         "DestCode": "VIE",
         "AirlineCode": "EK",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
