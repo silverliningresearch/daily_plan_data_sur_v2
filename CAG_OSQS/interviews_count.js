089cff786635f9ecb93c9f6127f5ac45544346e7
unknown <lb@silverliningresearch.com>
2024-02-28 01:36:17 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-02-2024 09:20:26"
+        "download_time": "28-02-2024 09:35:24"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -2904,6 +2904,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-02-27",
         "quota_id": "iTEA Premium\u2666TERMINAL 3\u2666Landside\u2666#B2-30\u2666FBL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-02-28",
+        "quota_id": "Travelex T2 DCI Central\u2666TERMINAL 2\u2666Landside\u2666#02-17\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
