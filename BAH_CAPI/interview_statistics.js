011e024e5aaac0a44d4c267cdcac18a0bc4ee559
unknown <lb@silverliningresearch.com>
2024-02-29 02:51:24 +0000

    update new data

--- BAH_CAPI/interview_statistics.js
+++ BAH_CAPI/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-17",
         "quota_id": "GF-BOM",
         "Number of interviews": 1,
-        "download_time": "29-02-2024 06:36:19"
+        "download_time": "29-02-2024 06:51:23"
     },
     {
         "InterviewDate": "2024-02-18",
--- DOH_LCS/interview_statistics.js
+++ DOH_LCS/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-02-16",
         "quota_id": "QR-JED",
         "Number of interviews": 2,
-        "download_time": "29-02-2024 05:36:16"
+        "download_time": "29-02-2024 05:51:20"
     },
     {
         "InterviewDate": "2024-02-17",
