3b02c5c2d0e54cce7bbc4b69e220fd2ff5d03dee
unknown <lb@silverliningresearch.com>
2024-03-02 12:05:31 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "02-03-2024 19:50:25"
+        "download_time": "02-03-2024 20:05:25"
     },
     {
         "InterviewDate": "01-03-2024",
@@ -161,6 +161,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_MFM",
         "interviewerID": "12",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "02-03-2024",
+        "quota_id": "T2_SQ_DAC",
+        "interviewerID": "14",
+        "Number of interviews": 2
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "02-03-2024 12:50:29"
+        "download_time": "02-03-2024 13:05:29"
     },
     {
         "InterviewDate": "2023-12-21",
@@ -2163,6 +2163,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-02",
         "quota_id": "OS-WAW",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-03-02",
+        "quota_id": "TK-IST",
         "Number of interviews": 3
     }
 ]    
