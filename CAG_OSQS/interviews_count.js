b4e295d59855cc748c0f6f201653dc29c89f7c64
unknown <lb@silverliningresearch.com>
2024-03-04 09:20:39 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "04-03-2024 17:04:56"
+        "download_time": "04-03-2024 17:19:56"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3238,7 +3238,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-04",
         "quota_id": "Maison de Chronus (Premises A2) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-24\u2666LX\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-04",
@@ -3263,7 +3263,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-04",
         "quota_id": "Tiffany & Co.\u2666TERMINAL 3\u2666Airside\u2666#02-18\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
