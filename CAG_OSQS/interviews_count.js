bc1e4220f15e041ce7702879a299c2aa73a9d0e2
unknown <lb@silverliningresearch.com>
2024-03-05 04:05:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "05-03-2024 11:49:54"
+        "download_time": "05-03-2024 12:04:57"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3268,7 +3268,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-05",
         "quota_id": "Archipelago Craft Brew House\u2666TERMINAL 2\u2666Airside\u2666#02-157\u2666FBA\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-05",
