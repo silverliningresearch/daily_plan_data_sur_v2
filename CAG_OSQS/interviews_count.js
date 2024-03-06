846d72efd9163f2de8b11404013e005cfdf04e38
unknown <lb@silverliningresearch.com>
2024-03-06 01:50:49 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-03-2024 09:35:00"
+        "download_time": "06-03-2024 09:50:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3324,6 +3324,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-05",
         "quota_id": "Yakun Kaya Toast\u2666TERMINAL 2\u2666Airside\u2666#02-803\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-03-06",
+        "quota_id": "UOB T4 Arr Mtg Hall Central\u2666TERMINAL 4\u2666Airside\u2666#01-06\u2666MC\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
