397134785cb56bb2156a93347973037bab8337bd
unknown <lb@silverliningresearch.com>
2024-03-06 05:05:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-03-2024 12:50:01"
+        "download_time": "06-03-2024 13:05:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3328,7 +3328,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-06",
         "quota_id": "Crystal Jade GO\u2666TERMINAL 4\u2666Airside\u2666#02-41\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-06",
@@ -3343,7 +3343,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-06",
         "quota_id": "Noyah La Maison & Zero Degree (Premises A1)\u2666TERMINAL 4\u2666Airside\u2666#02-35 (beside Coffee Bean)\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-06",
