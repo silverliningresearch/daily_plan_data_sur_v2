bce79f8fc436b4c5ed6230d3f3a2134ed40761fa
unknown <lb@silverliningresearch.com>
2024-03-06 03:50:46 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-03-2024 11:35:02"
+        "download_time": "06-03-2024 11:50:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3329,6 +3329,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-06",
         "quota_id": "Crystal Jade GO\u2666TERMINAL 4\u2666Airside\u2666#02-41\u2666FBA\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-03-06",
+        "quota_id": "Heavenly Wang\u2666TERMINAL 4\u2666Airside\u2666#02-80\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-06",
