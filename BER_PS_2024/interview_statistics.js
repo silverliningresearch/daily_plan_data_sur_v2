a27b554061ff0d007f8ba05eb6276c76d828c9fe
unknown <lb@silverliningresearch.com>
2024-03-07 11:20:55 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "07-03-2024 12:04:55"
+        "download_time": "07-03-2024 12:19:54"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -2963,7 +2963,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-07",
         "quota_id": "LHR - British Airways",
-        "Number of interviews": 7
+        "Number of interviews": 9
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-03-2024 19:05:01"
+        "download_time": "07-03-2024 19:20:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3413,7 +3413,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-07",
         "quota_id": "Hudsons Cafe\u2666TERMINAL 3\u2666Airside\u2666#02-60\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-07",
@@ -3438,7 +3438,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-07",
         "quota_id": "Pret A Manger\u2666TERMINAL 3\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-03-07",
@@ -3448,7 +3448,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-07",
         "quota_id": "The Kitchen by Wolfgang Puck\u2666TERMINAL 3\u2666Airside\u2666#02-42\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     }
 ]    
 
