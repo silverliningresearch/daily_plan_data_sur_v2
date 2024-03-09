451b9eae23de4d0b80ec0af72971cda21a5a4274
unknown <lb@silverliningresearch.com>
2024-03-09 07:21:11 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-03-2024 15:05:07"
+        "download_time": "09-03-2024 15:20:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3533,7 +3533,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-09",
         "quota_id": "ORCHID CAFETERIA\u2666TERMINAL 1\u2666Landside\u2666#B1-12A\u2666FBL\u2666Min25",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-09",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "09-03-2024 08:05:49"
+        "download_time": "09-03-2024 08:21:07"
     },
     {
         "InterviewDate": "2023-12-21",
@@ -2263,7 +2263,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-09",
         "quota_id": "XQ-AYT",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
