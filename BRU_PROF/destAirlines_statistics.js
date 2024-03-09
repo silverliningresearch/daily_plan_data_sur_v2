4dba668b7e286e6fdb9774e7c768f4a41e83d3c0
unknown <lb@silverliningresearch.com>
2024-03-09 09:35:52 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "09-03-2024 10:19:48"
+        "download_time": "09-03-2024 10:34:50"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -47244,6 +47244,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-03-09",
         "quota_id": "HRG-TB",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-03-09",
+        "quota_id": "IAD-UA",
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-09",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-03-2024 17:20:06"
+        "download_time": "09-03-2024 17:35:09"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3518,7 +3518,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-09",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-09",
@@ -3568,7 +3568,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-09",
         "quota_id": "UOB T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K27\u2666MC\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-09",
