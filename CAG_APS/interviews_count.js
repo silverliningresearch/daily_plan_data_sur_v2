a0937f7cf62a2ed87df011a3de9c32df305c158f
unknown <lb@silverliningresearch.com>
2024-03-10 07:50:52 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "10-03-2024 15:35:56"
+        "download_time": "10-03-2024 15:50:44"
     },
     {
         "InterviewDate": "01-03-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "10-03-2024 15:35:19"
+        "download_time": "10-03-2024 15:50:08"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3613,7 +3613,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-10",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K11\u2666CR\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-10",
@@ -3653,7 +3653,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-10",
         "quota_id": "LIHO TEA\u2666TERMINAL 4\u2666Landside\u2666#01-08\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-10",
