139272b76004a7821794902bd74c0ce90a1239a0
unknown <lb@silverliningresearch.com>
2024-03-11 04:50:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-03-2024 12:35:07"
+        "download_time": "11-03-2024 12:50:08"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3718,12 +3718,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-11",
         "quota_id": "LT T2 Arrival North\u2666TERMINAL 2\u2666Airside\u2666#01-151\u2666LT\u2666Min15",
-        "Number of interviews": 9
+        "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2024-03-11",
+        "quota_id": "Liquor & Tobacco (T2 Arrival South)\u2666TERMINAL 2\u2666Airside\u2666#01-176\u2666LT\u2666Min15",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-11",
         "quota_id": "Mango Tree Kitchen / Archipelago\u2666TERMINAL 1\u2666Airside\u2666#02-39N\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-11",
@@ -3738,7 +3743,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-11",
         "quota_id": "Perfumes & Cosmetics (T2 ABN)\u2666TERMINAL 2\u2666Airside\u2666#01-150\u2666PC\u2666Min15",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-11",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "11-03-2024 08:35:46"
+        "download_time": "11-03-2024 08:50:48"
     },
     {
         "InterviewDate": "2024-01-07",
