69ede99213f533f00799bc837cf374b29f68bc50
unknown <lb@silverliningresearch.com>
2024-03-11 05:50:56 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-03-2024 13:35:10"
+        "download_time": "11-03-2024 13:50:11"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3707,6 +3707,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-11",
+        "quota_id": "Crystal Jade La Mian Xiao Long Bao\u2666TERMINAL 1\u2666Airside\u2666#03-54\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-11",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
         "Number of interviews": 1
     },
@@ -3723,7 +3728,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-11",
         "quota_id": "Liquor & Tobacco (T2 Arrival South)\u2666TERMINAL 2\u2666Airside\u2666#01-176\u2666LT\u2666Min15",
-        "Number of interviews": 9
+        "Number of interviews": 15
     },
     {
         "InterviewDate": "2024-03-11",
@@ -3764,6 +3769,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-11",
         "quota_id": "Ray-Ban\u2666TERMINAL 2\u2666Airside\u2666#02-152 (#026-094)\u2666MP\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-03-11",
+        "quota_id": "SG Hawker\u2666TERMINAL 1\u2666Airside\u2666#03-47/48\u2666FBA\u2666Min25",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-11",
