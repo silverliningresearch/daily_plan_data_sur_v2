bc753d0fb7dd9d3874a1c80604267e349b147adf
unknown <lb@silverliningresearch.com>
2024-03-12 04:35:42 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "12-03-2024 12:20:34"
+        "download_time": "12-03-2024 12:35:35"
     },
     {
         "InterviewDate": "01-03-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-03-2024 12:19:58"
+        "download_time": "12-03-2024 12:34:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3818,7 +3818,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "CHUTNEY MARY INDIAN FAST FOOD\u2666TERMINAL 2\u2666Landside\u2666#03-26 &27\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-12",
@@ -3833,7 +3833,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "Perfumes & Cosmetics (T2 DTC)\u2666TERMINAL 2\u2666Airside\u2666#02-174 (#026-103E)\u2666PC\u2666Min25",
-        "Number of interviews": 16
+        "Number of interviews": 17
     },
     {
         "InterviewDate": "2024-03-12",
@@ -3853,7 +3853,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "Tumi\u2666TERMINAL 2\u2666Airside\u2666#02-184\u2666MP\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-12",
