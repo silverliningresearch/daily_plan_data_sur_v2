291034914445d1cc6cb3851930c3d5d60c1846c7
unknown <lb@silverliningresearch.com>
2024-03-12 04:05:50 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "12-03-2024 04:50:04"
+        "download_time": "12-03-2024 05:04:55"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-03-2024 11:50:14"
+        "download_time": "12-03-2024 12:05:04"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3812,6 +3812,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-12",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-05 (beside iSC counter near lifts)\u2666CR\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-12",
         "quota_id": "LT T2 Duplex\u2666TERMINAL 2\u2666Airside\u2666#02-167 & #03-189\u2666LT\u2666Min25",
         "Number of interviews": 25
     },
@@ -3828,7 +3833,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "Premises C - Baggage Storage by Smarte Carte\u2666TERMINAL 2\u2666Landside\u2666#01-01 (#016-024)\u2666BS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-12",
@@ -3838,7 +3843,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "Sprint-Cass (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-186\u2666EL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-12",
@@ -3848,7 +3853,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     }
 ]    
 
