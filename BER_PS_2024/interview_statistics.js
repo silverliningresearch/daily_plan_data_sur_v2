f7f8e92d873298635734e901863b2ad20731f695
unknown <lb@silverliningresearch.com>
2024-03-12 21:05:43 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "12-03-2024 21:49:50"
+        "download_time": "12-03-2024 22:04:51"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -3168,7 +3168,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-03-12",
