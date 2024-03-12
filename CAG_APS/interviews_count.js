cec330ea2cb516b54efb363034d63791fb3459f5
unknown <lb@silverliningresearch.com>
2024-03-12 01:35:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "12-03-2024 09:21:01"
+        "download_time": "12-03-2024 09:35:50"
     },
     {
         "InterviewDate": "01-03-2024",
@@ -1072,7 +1072,7 @@ let interview_statistics  = `[
         "InterviewDate": "12-03-2024",
         "quota_id": "T4_5J_MNL",
         "interviewerID": "02",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-03-2024 09:20:13"
+        "download_time": "12-03-2024 09:35:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3818,7 +3818,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-12",
         "quota_id": "Perfumes & Cosmetics (T2 DTC)\u2666TERMINAL 2\u2666Airside\u2666#02-174 (#026-103E)\u2666PC\u2666Min25",
-        "Number of interviews": 4
+        "Number of interviews": 6
     }
 ]    
 
