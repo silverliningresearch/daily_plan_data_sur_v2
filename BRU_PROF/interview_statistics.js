03b67ae1ca41f6d29c22d10f5eb68e4d15a3c4ad
unknown <lb@silverliningresearch.com>
2024-03-13 08:05:46 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "13-03-2024 08:49:50"
+        "download_time": "13-03-2024 09:04:44"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -52448,7 +52448,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "HU760-SZX",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-03-13",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "13-03-2024 15:50:47"
+        "download_time": "13-03-2024 16:05:38"
     },
     {
         "InterviewDate": "01-03-2024",
@@ -1235,6 +1235,18 @@ let interview_statistics  = `[
         "quota_id": "T4_QZ_YIA",
         "interviewerID": "03",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "13-03-2024",
+        "quota_id": "T1_CA_PEK",
+        "interviewerID": "12",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "13-03-2024",
+        "quota_id": "T1_CA_PEKk",
+        "interviewerID": "12",
+        "Number of interviews": 1
     }
 ]    
 
