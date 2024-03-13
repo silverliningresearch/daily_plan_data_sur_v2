95299eed7a7ee3074a81b7f0890869633b5c11b1
unknown <lb@silverliningresearch.com>
2024-03-13 00:50:58 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-03-2024 08:35:13"
+        "download_time": "13-03-2024 08:50:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3923,12 +3923,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "Bengawan Solo\u2666TERMINAL 4\u2666Airside\u2666#02-84\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "Discover Singapore\u2666TERMINAL 4\u2666Airside\u2666#02-73/74/75/76/77\u2666SV\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-13",
@@ -3943,7 +3943,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "Taste Singapore\u2666TERMINAL 4\u2666Airside\u2666#02-72\u2666CCD\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-13",
