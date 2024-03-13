baa9f60e2555fac87c35b78eab9061bd3671dffc
unknown <lb@silverliningresearch.com>
2024-03-13 00:20:59 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "13-03-2024 08:06:59"
+        "download_time": "13-03-2024 08:20:50"
     },
     {
         "InterviewDate": "01-03-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-03-2024 08:06:19"
+        "download_time": "13-03-2024 08:20:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3918,7 +3918,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "Bee Cheng Hiang\u2666TERMINAL 4\u2666Airside\u2666#02-85\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-13",
@@ -3938,7 +3938,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-13",
         "quota_id": "Taste Singapore\u2666TERMINAL 4\u2666Airside\u2666#02-72\u2666CCD\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-13",
