671fff933de04a86449e0db5aafe97e105dabca8
unknown <lb@silverliningresearch.com>
2024-03-13 08:20:45 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "13-03-2024 16:05:38"
+        "download_time": "13-03-2024 16:20:37"
     },
     {
         "InterviewDate": "01-03-2024",
@@ -1245,6 +1245,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "13-03-2024",
         "quota_id": "T1_CA_PEKk",
+        "interviewerID": "12",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "13-03-2024",
+        "quota_id": "T1_ca_pek",
         "interviewerID": "12",
         "Number of interviews": 1
     }
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-03-2024 16:05:01"
+        "download_time": "13-03-2024 16:20:00"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -3914,6 +3914,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-12",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-03-13",
+        "quota_id": "7-Eleven (T3)\u2666TERMINAL 3\u2666Airside\u2666#03-12\u2666CS\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-13",
