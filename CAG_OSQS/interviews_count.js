2ad21c7bfb9e101cb12149b9ec4cffa5cf6ab04d
unknown <lb@silverliningresearch.com>
2024-03-14 08:50:48 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "14-03-2024 16:35:04"
+        "download_time": "14-03-2024 16:50:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4048,7 +4048,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-14",
         "quota_id": "Boarding Gate & The Wallet Shop\u2666TERMINAL 3\u2666Landside\u2666#B2-08/09\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-14",
@@ -4083,7 +4083,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-14",
         "quota_id": "LT T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-57\u2666LT\u2666Min15",
-        "Number of interviews": 3
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-03-14",
@@ -4128,7 +4128,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-14",
         "quota_id": "UNITY\u2666TERMINAL 3\u2666Landside\u2666#B2-57\u2666LS\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-14",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "14-03-2024 12:35:42"
+        "download_time": "14-03-2024 12:50:42"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -1574,7 +1574,7 @@ let interview_statistics = `[
         "Flight": "EK67",
         "DestCode": "STN",
         "AirlineCode": "EK",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-14",
