b282b20d05eeb09986499c034a8539797e99eef6
unknown <lb@silverliningresearch.com>
2024-03-16 04:21:07 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-03-2024 12:05:22"
+        "download_time": "16-03-2024 12:20:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4212,6 +4212,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-16",
+        "quota_id": "Buk Chang Dong Soon Tofu\u2666TERMINAL 2\u2666Airside\u2666#02-K12\u2666FBA\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-16",
         "quota_id": "Chef Wei\u2666TERMINAL 2\u2666Airside\u2666#02-K6\u2666FBA\u2666Min8",
         "Number of interviews": 6
     },
@@ -4248,7 +4253,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "LT T1 DT Central\u2666TERMINAL 1\u2666Airside\u2666#02-53,02-46,02-46A\u2666LT\u2666Min25",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-16",
@@ -4269,6 +4274,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-16",
         "quota_id": "Perfumes & Cosmetics (T1 DTC)\u2666TERMINAL 1\u2666Airside\u2666#02-37/38\u2666PC\u2666Min25",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-16",
+        "quota_id": "Sprint-Cass (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-36\u2666EL\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-16",
@@ -4298,12 +4308,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     }
 ]    
 
