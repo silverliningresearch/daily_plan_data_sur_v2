ffd4ad4e9147da38d2c40ad6acd73688eab3429b
unknown <lb@silverliningresearch.com>
2024-03-16 07:21:12 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "16-03-2024 08:05:08"
+        "download_time": "16-03-2024 08:20:07"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -3274,6 +3274,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-03-16",
         "quota_id": "AMS - easyJet Europe",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-03-16",
+        "quota_id": "LCY - British Airways",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-16",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-03-2024 15:05:20"
+        "download_time": "16-03-2024 15:20:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4228,7 +4228,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "Dunkin\u2019 Donuts\u2666TERMINAL 2\u2666Airside\u2666#02-K14\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-03-16",
@@ -4278,7 +4278,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "LiHO TEA\u2666TERMINAL 2\u2666Airside\u2666#02-K13\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-16",
