0f937919802db237a2b49bcd9e61a5e519a8dd85
unknown <lb@silverliningresearch.com>
2024-03-16 04:36:08 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "16-03-2024 05:19:52"
+        "download_time": "16-03-2024 05:34:53"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -47899,6 +47899,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-03-15",
         "quota_id": "TIA-TB",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-03-16",
+        "quota_id": "SID-TB",
+        "Number of interviews": 7
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-03-2024 12:20:20"
+        "download_time": "16-03-2024 12:35:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4278,7 +4278,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "Sprint-Cass (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-36\u2666EL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-16",
@@ -4308,12 +4308,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-16",
         "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     }
 ]    
 
