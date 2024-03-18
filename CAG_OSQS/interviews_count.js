ad1b7d21a2f5301552cdf2faded579bb1e23bce0
unknown <lb@silverliningresearch.com>
2024-03-18 06:06:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-03-2024 13:50:22"
+        "download_time": "18-03-2024 14:05:31"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4473,7 +4473,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-18",
         "quota_id": "LT T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#02-401\u2666LT\u2666Min15",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-18",
@@ -4503,7 +4503,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-18",
         "quota_id": "The Cocoa Trees (Premises B3) (T2 DTN, near E20)\u2666TERMINAL 2\u2666Airside\u2666#02-403\u2666CCD\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-18",
@@ -4523,7 +4523,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-18",
         "quota_id": "WH Smith - Premises J (Air)\u2666TERMINAL 4\u2666Airside\u2666#02-59\u2666OW\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     }
 ]    
 
