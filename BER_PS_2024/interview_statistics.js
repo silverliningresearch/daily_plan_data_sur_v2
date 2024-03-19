0eaf033523b48a4d68b331a6ebdc568d8012c720
unknown <lb@silverliningresearch.com>
2024-03-19 17:50:58 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "19-03-2024 18:35:01"
+        "download_time": "19-03-2024 18:50:00"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -3369,6 +3369,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-03-19",
         "quota_id": "AGP - easyJet Europe",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-03-19",
+        "quota_id": "AMS - easyJet Europe",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-19",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "19-03-2024 18:34:49"
+        "download_time": "19-03-2024 18:49:48"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -52968,7 +52968,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-19",
         "quota_id": "SN2617-FRA",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-19",
