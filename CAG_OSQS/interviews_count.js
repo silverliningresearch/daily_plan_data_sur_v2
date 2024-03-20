723818e0849b1cfbed29c8afc4c6b9e024dde686
unknown <lb@silverliningresearch.com>
2024-03-20 23:51:17 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-03-2024 07:35:28"
+        "download_time": "21-03-2024 07:50:29"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4749,6 +4749,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-20",
         "quota_id": "The Killiney Experience\u2666TERMINAL 1\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-03-21",
+        "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 3\u2666Airside\u2666#03-15\u2666HL\u2666Min20",
+        "Number of interviews": 2
     }
 ]    
 
