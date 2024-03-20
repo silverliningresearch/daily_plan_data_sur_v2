16f187916958239cb33f32af9bf18b11ec79a544
unknown <lb@silverliningresearch.com>
2024-03-20 04:20:57 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-03-2024 12:06:20"
+        "download_time": "20-03-2024 12:20:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4657,6 +4657,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-20",
+        "quota_id": "BEE CHENG HIANG\u2666TERMINAL 1\u2666Airside\u2666#02-K24\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-20",
         "quota_id": "Balenciaga\u2666TERMINAL 1\u2666Airside\u2666#02-81\u2666LX\u2666Min8",
         "Number of interviews": 1
     },
@@ -4678,6 +4683,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Qeelin\u2666TERMINAL 1\u2666Airside\u2666#02-78\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-20",
+        "quota_id": "Scarves & Glitter, House of Kashkha\u2666TERMINAL 1\u2666Airside\u2666#02-K46\u2666MP\u2666Min8",
         "Number of interviews": 1
     },
     {
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "20-03-2024 08:07:02"
+        "download_time": "20-03-2024 08:20:51"
     },
     {
         "InterviewDate": "2024-01-07",
