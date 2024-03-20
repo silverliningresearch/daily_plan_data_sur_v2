5987ccc2b51478f48e173d86fff86fac954f8404
unknown <lb@silverliningresearch.com>
2024-03-20 11:51:15 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-03-2024 19:35:30"
+        "download_time": "20-03-2024 19:50:27"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4673,7 +4673,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Asian Artistry\u2666TERMINAL 2\u2666Airside\u2666#02-153\u2666JW\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4683,7 +4683,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Balenciaga\u2666TERMINAL 1\u2666Airside\u2666#02-81\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4723,7 +4723,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Qeelin\u2666TERMINAL 1\u2666Airside\u2666#02-78\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-20",
