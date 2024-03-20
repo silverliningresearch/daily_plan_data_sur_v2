7b771a84b4fe85a855fa34c1037b7b1e9a767c6c
unknown <lb@silverliningresearch.com>
2024-03-20 09:21:16 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "20-03-2024 10:05:02"
+        "download_time": "20-03-2024 10:20:13"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -3487,8 +3487,13 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-03-20",
-        "quota_id": "TFS - easyJet Europe",
+        "quota_id": "TFS - Eurowings",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-20",
+        "quota_id": "TFS - easyJet Europe",
+        "Number of interviews": 2
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-03-2024 17:05:15"
+        "download_time": "20-03-2024 17:20:26"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4668,7 +4668,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Ambassador Transit Lounge\u2666TERMINAL 2\u2666Airside\u2666#036-123&123-A\u2666HL\u2666Min20",
-        "Number of interviews": 9
+        "Number of interviews": 11
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4703,7 +4703,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Hugo Boss\u2666TERMINAL 1\u2666Airside\u2666#02-82\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4718,7 +4718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Qeelin\u2666TERMINAL 1\u2666Airside\u2666#02-78\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-20",
