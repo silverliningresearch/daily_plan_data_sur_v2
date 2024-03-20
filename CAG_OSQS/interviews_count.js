8108bc652cbceda4cce335794ab958094def7ef6
unknown <lb@silverliningresearch.com>
2024-03-20 11:21:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-03-2024 19:05:24"
+        "download_time": "20-03-2024 19:20:28"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4663,7 +4663,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 2\u2666Airside\u2666#03-175/176\u2666HL\u2666Min20",
-        "Number of interviews": 18
+        "Number of interviews": 19
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4683,12 +4683,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Balenciaga\u2666TERMINAL 1\u2666Airside\u2666#02-81\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Burberry\u2666TERMINAL 1\u2666Airside\u2666#02-80\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-20",
@@ -4723,7 +4723,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-20",
         "quota_id": "Qeelin\u2666TERMINAL 1\u2666Airside\u2666#02-78\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-20",
