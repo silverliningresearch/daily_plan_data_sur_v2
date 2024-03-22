7fcbfe8cfdb444982261a3edfd6ecf88bd117346
unknown <lb@silverliningresearch.com>
2024-03-22 01:51:15 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-03-2024 09:35:30"
+        "download_time": "22-03-2024 09:50:28"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4878,6 +4878,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-21",
         "quota_id": "Travelex T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-K9\u2666MC\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-03-22",
+        "quota_id": "Guardian (Premises B) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-29\u2666PH\u2666Min8",
         "Number of interviews": 3
     },
     {
