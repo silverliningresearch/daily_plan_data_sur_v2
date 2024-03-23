82dc8425e05a55cee1f1b03dd5bdd392fb39ee1b
unknown <lb@silverliningresearch.com>
2024-03-23 03:36:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-03-2024 11:20:37"
+        "download_time": "23-03-2024 11:35:33"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4987,6 +4987,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-23",
+        "quota_id": "Bvlgari\u2666TERMINAL 3\u2666Airside\u2666#02-45-02\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-23",
         "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 1\u2666Landside\u2666#01-K7\u2666CR\u2666Min8",
         "Number of interviews": 3
     },
@@ -5004,6 +5009,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-23",
         "quota_id": "CTE T1 DCI Central\u2666TERMINAL 1\u2666Landside\u2666#02-02\u2666MC\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-23",
+        "quota_id": "CTE T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-21\u2666MC\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-23",
+        "quota_id": "Cartier\u2666TERMINAL 3\u2666Airside\u2666#02-45-01\u2666LX\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-23",
@@ -5028,7 +5043,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "Penang Culture and Woke Ramen\u2666TERMINAL 1\u2666Landside\u2666#03-19\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-23",
@@ -5038,7 +5053,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "SG Hawker\u2666TERMINAL 1\u2666Airside\u2666#03-47/48\u2666FBA\u2666Min25",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-03-23",
