caf993ed48fc8a01bb01dbdf063e8a2b6502f04c
unknown <lb@silverliningresearch.com>
2024-03-23 04:36:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-03-2024 12:20:30"
+        "download_time": "23-03-2024 12:35:29"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -4993,7 +4993,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "Bvlgari\u2666TERMINAL 3\u2666Airside\u2666#02-45-02\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-23",
@@ -5003,7 +5003,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-23",
@@ -5023,7 +5023,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "Cartier\u2666TERMINAL 3\u2666Airside\u2666#02-45-01\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-23",
@@ -5073,7 +5073,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-23",
         "quota_id": "UOB T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-20\u2666MC\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-23",
