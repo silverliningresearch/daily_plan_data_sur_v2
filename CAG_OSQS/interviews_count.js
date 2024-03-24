ef3a051d869a5b35acace2acfd005009b16c075e
unknown <lb@silverliningresearch.com>
2024-03-24 09:36:21 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-03-2024 17:20:30"
+        "download_time": "24-03-2024 17:35:33"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5253,12 +5253,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-24",
         "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
-        "Number of interviews": 8
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-03-24",
         "quota_id": "Straits Food Village - Premises B (Pho Street, Subway, Kaveri)\u2666TERMINAL 2\u2666Airside\u2666#036-169\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-24",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "24-03-2024 10:21:20"
+        "download_time": "24-03-2024 10:36:19"
     },
     {
         "InterviewDate": "2023-12-21",
@@ -3073,12 +3073,17 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-03-24",
         "quota_id": "FR-ATH",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-03-24",
+        "quota_id": "FR-INI",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-24",
         "quota_id": "FR-MAN",
-        "Number of interviews": 5
+        "Number of interviews": 8
     }
 ]    
 
