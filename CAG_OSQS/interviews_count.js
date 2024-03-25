30859006f912290236c119c98028f91a5114b86d
unknown <lb@silverliningresearch.com>
2024-03-25 23:51:32 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-03-2024 07:35:40"
+        "download_time": "26-03-2024 07:50:39"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5429,6 +5429,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-26",
         "quota_id": "Bee Cheng Hiang (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-156 (#026-095-01)\u2666CCD\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-03-26",
+        "quota_id": "Liquor & Tobacco (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-197\u2666LT\u2666Min15",
+        "Number of interviews": 2
     }
 ]    
 
