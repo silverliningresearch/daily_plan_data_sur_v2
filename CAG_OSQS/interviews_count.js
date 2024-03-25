23f8bc8357c04fc12ef3917f28fa3597271d7fc2
unknown <lb@silverliningresearch.com>
2024-03-25 23:21:27 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-03-2024 07:05:38"
+        "download_time": "26-03-2024 07:20:37"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5428,7 +5428,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "Bee Cheng Hiang (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-156 (#026-095-01)\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "26-03-2024 00:06:26"
+        "download_time": "26-03-2024 00:21:24"
     },
     {
         "InterviewDate": "2023-12-21",
