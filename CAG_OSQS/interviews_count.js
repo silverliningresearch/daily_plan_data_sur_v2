f9ea005545a2b207f7b84d00d23aa47396ad219d
unknown <lb@silverliningresearch.com>
2024-03-25 01:06:12 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "25-03-2024 08:50:18"
+        "download_time": "25-03-2024 09:05:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5289,6 +5289,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-25",
         "quota_id": "Lululemon\u2666TERMINAL 3\u2666Airside\u2666#02-50A\u2666MP\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-25",
+        "quota_id": "Smarte Carte Premises D (T2 Airside)\u2666TERMINAL 2\u2666Airside\u2666#02-242\u2666BS\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-25",
