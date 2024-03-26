a226d825ef71702fb4d63cc9af66c712dbef5cc5
unknown <lb@silverliningresearch.com>
2024-03-26 10:21:49 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-03-2024 18:05:33"
+        "download_time": "26-03-2024 18:20:53"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5489,6 +5489,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-26",
         "quota_id": "Liquor & Tobacco (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-197\u2666LT\u2666Min15",
         "Number of interviews": 17
+    },
+    {
+        "InterviewDate": "2024-03-26",
+        "quota_id": "Luvenus\u2666TERMINAL 3\u2666Airside\u2666#02-K11\u2666JW\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-26",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "26-03-2024 14:06:13"
+        "download_time": "26-03-2024 14:21:39"
     },
     {
         "InterviewDate": "2024-01-07",
