7058c4f92d5c21fe621594af9e1d2a9623fef353
unknown <lb@silverliningresearch.com>
2024-03-26 11:36:15 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "26-03-2024 12:19:57"
+        "download_time": "26-03-2024 12:35:00"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-03-2024 19:20:26"
+        "download_time": "26-03-2024 19:35:26"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5478,7 +5478,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "Guardian (Premises A) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-66\u2666PH\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-26",
@@ -5548,7 +5548,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "TWG Tea (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-26",
