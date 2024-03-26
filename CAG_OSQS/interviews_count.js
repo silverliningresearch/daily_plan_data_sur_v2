2123be88479bc9ab43c14664db0a0a9f16bcda9d
unknown <lb@silverliningresearch.com>
2024-03-26 12:21:16 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-03-2024 20:05:39"
+        "download_time": "26-03-2024 20:20:26"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5488,7 +5488,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "LT T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-65\u2666LT\u2666Min15",
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-03-26",
@@ -5508,7 +5508,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "Luvenus\u2666TERMINAL 3\u2666Airside\u2666#02-K11\u2666JW\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-03-26",
@@ -5518,7 +5518,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-26",
         "quota_id": "Maison de Chronus (Premises A1) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-51\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-03-26",
