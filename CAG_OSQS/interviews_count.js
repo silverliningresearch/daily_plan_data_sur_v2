62778bd187440e3aa5df8add7a11757ff16ff0aa
unknown <lb@silverliningresearch.com>
2024-03-27 22:51:28 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-03-2024 06:35:24"
+        "download_time": "28-03-2024 06:50:38"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5809,6 +5809,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-27",
         "quota_id": "iStudio\u2666TERMINAL 3\u2666Airside\u2666#02-54\u2666EL\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-28",
+        "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min15",
+        "Number of interviews": 1
     }
 ]    
 
