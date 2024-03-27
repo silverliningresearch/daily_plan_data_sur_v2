bc5c480507f66f883e3f78abf230decd523441a4
unknown <lb@silverliningresearch.com>
2024-03-27 03:21:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-03-2024 11:05:23"
+        "download_time": "27-03-2024 11:20:23"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5582,6 +5582,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-27",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-03-27",
         "quota_id": "CURRY TIMES\u2666TERMINAL 3\u2666Landside\u2666#B2-51\u2666FBL\u2666Min8",
         "Number of interviews": 2
     },
@@ -5602,13 +5607,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-27",
+        "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-27",
         "quota_id": "Oakley\u2666TERMINAL 2\u2666Airside\u2666#02-244\u2666MP\u2666Min8",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-03-27",
         "quota_id": "Omega\u2666TERMINAL 2\u2666Airside\u2666#02-233\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-27",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "27-03-2024 07:06:04"
+        "download_time": "27-03-2024 07:21:10"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -2066,11 +2066,19 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-03-27",
+        "quota_id": "FZ133",
+        "Flight": "FZ133",
+        "DestCode": "MCT",
+        "AirlineCode": "FZ",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-27",
         "quota_id": "FZ317",
         "Flight": "FZ317",
         "DestCode": "MUX",
         "AirlineCode": "FZ",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-03-27",
@@ -2078,7 +2086,7 @@ let interview_statistics = `[
         "Flight": "FZ847",
         "DestCode": "RUH",
         "AirlineCode": "FZ",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-03-27",
