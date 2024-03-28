717381a870183222054ff0cd991ada127ba25842
unknown <lb@silverliningresearch.com>
2024-03-28 04:06:07 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-03-2024 11:50:16"
+        "download_time": "28-03-2024 12:05:16"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5817,6 +5817,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-28",
+        "quota_id": "Braun Buffel\u2666TERMINAL 2\u2666Airside\u2666#02-245\u2666MP\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-28",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min8",
         "Number of interviews": 4
     },
@@ -5878,7 +5883,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-28",
         "quota_id": "T3 iSC Departure counter\u2666TERMINAL 3\u2666Airside\u2666NA\u2666PS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-03-28",
