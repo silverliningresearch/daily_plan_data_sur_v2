382247eec8238ccd1e0b00a1475a46860fd1cb1b
unknown <lb@silverliningresearch.com>
2024-03-28 09:08:07 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "28-03-2024 09:50:02"
+        "download_time": "28-03-2024 10:05:02"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -48534,6 +48534,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-03-28",
         "quota_id": "GVA-SN",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-03-28",
+        "quota_id": "LIN-SN",
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-03-28",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-03-2024 16:52:44"
+        "download_time": "28-03-2024 17:05:33"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -5942,6 +5942,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-28",
+        "quota_id": "Roger Vivier\u2666TERMINAL 1\u2666Airside\u2666#02-13AF\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-03-28",
         "quota_id": "Sony\u2666TERMINAL 3\u2666Airside\u2666#02-62\u2666EL\u2666Min8",
         "Number of interviews": 4
     },
@@ -5949,6 +5954,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-28",
         "quota_id": "Sprint-Cass (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-61\u2666EL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-03-28",
+        "quota_id": "Sunglass Hut (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-28\u2666MP\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-28",
