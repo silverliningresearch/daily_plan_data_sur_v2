b0ff2d9a64fb25f774ba6d25f11f9f8614417bf0
unknown <lb@silverliningresearch.com>
2024-03-30 04:06:19 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-03-2024 11:50:26"
+        "download_time": "30-03-2024 12:05:29"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6338,6 +6338,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-30",
         "quota_id": "Straits Food Village - Premises B (Pho Street, Subway, Kaveri)\u2666TERMINAL 2\u2666Airside\u2666#036-169\u2666FBA\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-30",
+        "quota_id": "TWG Tea Boutique\u2666TERMINAL 2\u2666Airside\u2666#02-216\u2666CCD\u2666Min8",
         "Number of interviews": 2
     },
     {
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "30-03-2024 07:51:10"
+        "download_time": "30-03-2024 08:06:11"
     },
     {
         "InterviewDate": "2024-01-07",
