033e6503b865f54e08be62bece30b8fb505c616e
unknown <lb@silverliningresearch.com>
2024-03-30 01:36:21 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "30-03-2024 09:21:07"
+        "download_time": "30-03-2024 09:36:08"
     },
     {
         "InterviewDate": "01-03-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-03-2024 09:20:29"
+        "download_time": "30-03-2024 09:35:30"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6298,6 +6298,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-03-30",
         "quota_id": "LiHO TEA\u2666TERMINAL 2\u2666Airside\u2666#02-K13\u2666FBA\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-30",
+        "quota_id": "The Hainan Story Coffee & Toast\u2666TERMINAL 2\u2666Airside\u2666#02-K11\u2666FBA\u2666Min8",
         "Number of interviews": 1
     }
 ]    
