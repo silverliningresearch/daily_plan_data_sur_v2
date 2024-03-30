8c5292e49ec03e81c86d446726358d52c04be598
unknown <lb@silverliningresearch.com>
2024-03-30 05:06:19 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-03-2024 12:50:30"
+        "download_time": "30-03-2024 13:05:29"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6287,6 +6287,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-03-30",
+        "quota_id": "Bee Cheng Hiang (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#026-135 (#02-199)\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-30",
         "quota_id": "Bengawan Solo\u2666TERMINAL 2\u2666Airside\u2666#026-096-01\u2666CCD\u2666Min8",
         "Number of interviews": 2
     },
@@ -6339,6 +6344,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-03-30",
         "quota_id": "Perfumes & Cosmetics (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-194 (#026-116)\u2666PC\u2666Min15",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-03-30",
+        "quota_id": "Sprint-Cass (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-186\u2666EL\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-03-30",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "30-03-2024 08:51:12"
+        "download_time": "30-03-2024 09:06:11"
     },
     {
         "InterviewDate": "2024-01-07",
