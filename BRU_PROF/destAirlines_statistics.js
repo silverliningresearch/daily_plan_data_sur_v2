238f0bc0b6411153af9029fe77c6a9d3e983082e
unknown <lb@silverliningresearch.com>
2024-04-01 16:20:39 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "01-04-2024 18:04:45"
+        "download_time": "01-04-2024 18:19:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -48802,6 +48802,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "ARN-SK",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "AYT-XQ",
         "Number of interviews": 3
     },
@@ -48898,7 +48903,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "IST-TK",
-        "Number of interviews": 3
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-04-01",
@@ -48913,7 +48918,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "MIR-BJ",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-01",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-04-2024 18:04:44"
+        "download_time": "01-04-2024 18:19:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -53653,7 +53653,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "BJ363-MIR",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-01",
@@ -53699,6 +53699,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "QR194-DOH",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "SK1590-ARN",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-01",
@@ -53783,6 +53788,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "TB2941-PRN",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "TK1940-IST",
         "Number of interviews": 5
     },
     {
