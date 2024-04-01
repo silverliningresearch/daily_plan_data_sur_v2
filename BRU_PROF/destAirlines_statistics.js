9f95cc55697ec5d3b0310667ed57ae03052a9727
unknown <lb@silverliningresearch.com>
2024-04-01 15:50:40 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "01-04-2024 17:34:44"
+        "download_time": "01-04-2024 17:49:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -48808,7 +48808,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "BCN-SN",
-        "Number of interviews": 9
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2024-04-01",
@@ -48829,6 +48829,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "CAI-MS",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "CMN-AT",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-01",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-04-2024 17:34:43"
+        "download_time": "01-04-2024 17:49:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -53642,6 +53642,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "AT833-CMN",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "AZ159-FCO",
         "Number of interviews": 7
     },
@@ -53739,6 +53744,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "SN3703-BCN",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "SN3705-BCN",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-01",
