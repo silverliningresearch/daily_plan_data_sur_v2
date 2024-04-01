bfd934705512a8105e566dc659b1f8c1a71161bb
unknown <lb@silverliningresearch.com>
2024-04-01 15:20:40 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "01-04-2024 17:04:43"
+        "download_time": "01-04-2024 17:19:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -48882,6 +48882,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "GRO-FR",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "HRG-XR",
         "Number of interviews": 4
     },
@@ -48904,6 +48909,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "NCE-SN",
         "Number of interviews": 17
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "PRN-TB",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-01",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-04-2024 17:04:43"
+        "download_time": "01-04-2024 17:19:43"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -53657,6 +53657,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "FR5233-GRO",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "HV9007-FAO",
         "Number of interviews": 1
     },
@@ -53759,6 +53764,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "TB1013-AGP",
         "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "TB2941-PRN",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-01",
