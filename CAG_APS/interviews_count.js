ace117789bf9e9503ecd26229a0dc24dbf79f4db
unknown <lb@silverliningresearch.com>
2024-04-01 05:51:24 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_EK_DXB",
         "interviewerID": "02",
         "Number of interviews": 10,
-        "download_time": "01-04-2024 13:36:10"
+        "download_time": "01-04-2024 13:51:11"
     },
     {
         "InterviewDate": "01-03-2024",
@@ -137,6 +137,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_UPG",
         "interviewerID": "02",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "01-04-2024",
+        "quota_id": "T3_TR_KHN",
+        "interviewerID": "02",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "01-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "01-04-2024 13:35:33"
+        "download_time": "01-04-2024 13:50:34"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6453,7 +6453,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 4\u2666Landside\u2666#01-K3\u2666CR\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-01",
@@ -6463,7 +6463,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K1\u2666CR\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-01",
@@ -6503,7 +6503,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "Pontian Wanton Noodle\u2666TERMINAL 1\u2666Airside\u2666#02-K10N\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-04-01",
@@ -6518,7 +6518,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-01",
