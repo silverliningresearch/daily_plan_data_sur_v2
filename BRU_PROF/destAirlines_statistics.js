41a900f2ec64c60b855bef6e9d98fe97c90c11d4
unknown <lb@silverliningresearch.com>
2024-04-01 12:36:02 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "01-04-2024 14:19:43"
+        "download_time": "01-04-2024 14:34:58"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -48812,6 +48812,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "BUD-SN",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "DXB-EK",
         "Number of interviews": 9
     },
@@ -48833,6 +48838,11 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-04-01",
         "quota_id": "FRA-LH",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "HRG-XR",
         "Number of interviews": 3
     },
     {
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "01-04-2024 14:19:43"
+        "download_time": "01-04-2024 14:34:58"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -53667,6 +53667,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-04-01",
+        "quota_id": "SN2825-BUD",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-01",
         "quota_id": "SN3125-BLQ",
         "Number of interviews": 6
     },
@@ -53729,6 +53734,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-01",
         "quota_id": "VY8987-BCN",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-01",
+        "quota_id": "XR7693-HRG",
+        "Number of interviews": 3
     }
 ]    
 
