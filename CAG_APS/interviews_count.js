8963a01938356664cb9bad12effe9f6b58e423ee
unknown <lb@silverliningresearch.com>
2024-04-02 05:20:35 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_EY_FRA",
         "interviewerID": " ......",
         "Number of interviews": 1,
-        "download_time": "02-04-2024 13:05:35"
+        "download_time": "02-04-2024 13:20:30"
     },
     {
         "InterviewDate": "01-04-2024",
@@ -239,6 +239,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_NGB",
         "interviewerID": "02",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "02-04-2024",
+        "quota_id": "T4_AK_PEN",
+        "interviewerID": "03",
+        "Number of interviews": 1
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-04-2024 13:04:59"
+        "download_time": "02-04-2024 13:19:54"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6583,12 +6583,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-02",
         "quota_id": "Prada\u2666TERMINAL 2\u2666Airside\u2666#026-072 (#02-229)\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-02",
         "quota_id": "Saint Laurent\u2666TERMINAL 2\u2666Airside\u2666#02-225-02\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-02",
@@ -6603,7 +6603,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-02",
         "quota_id": "WH Smith - Premises D (Air) (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#026-055-02A (#02-240)\u2666OW\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
