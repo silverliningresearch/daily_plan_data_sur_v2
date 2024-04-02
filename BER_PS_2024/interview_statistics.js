b71b72eccc83dd5f506016eba47d93e85394674b
unknown <lb@silverliningresearch.com>
2024-04-02 05:05:43 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "02-04-2024 06:49:52"
+        "download_time": "02-04-2024 07:04:51"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-04-2024 12:50:00"
+        "download_time": "02-04-2024 13:04:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6578,7 +6578,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-02",
         "quota_id": "Maison Christian Dior/ Diptyque/ Byredo\u2666TERMINAL 2\u2666Airside\u2666#026-100-03\u2666PC\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-02",
