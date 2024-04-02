0901991df8fd59bad0764dca69284b47de455127
unknown <lb@silverliningresearch.com>
2024-04-02 04:50:42 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "02-04-2024 06:34:44"
+        "download_time": "02-04-2024 06:49:45"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -53888,7 +53888,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-02",
         "quota_id": "TK1942-IST",
-        "Number of interviews": 4
+        "Number of interviews": 7
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-04-2024 12:34:58"
+        "download_time": "02-04-2024 12:50:00"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6574,6 +6574,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-02",
         "quota_id": "Giordano\u2666TERMINAL 2\u2666Airside\u2666#026-096\u2666MP\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-02",
+        "quota_id": "Maison Christian Dior/ Diptyque/ Byredo\u2666TERMINAL 2\u2666Airside\u2666#026-100-03\u2666PC\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-02",
