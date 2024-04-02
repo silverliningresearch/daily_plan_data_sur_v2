f576d645aa1526227057f32b39a0e0b6789b4052
unknown <lb@silverliningresearch.com>
2024-04-02 23:50:35 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-04-2024 07:34:56"
+        "download_time": "03-04-2024 07:49:55"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6618,7 +6618,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-03",
         "quota_id": "Pret A Manger\u2666TERMINAL 2\u2666Airside\u2666#02-165\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     }
 ]    
 
