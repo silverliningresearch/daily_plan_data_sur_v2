a0189bac935076332e9a29170a5e3775255da318
unknown <lb@silverliningresearch.com>
2024-04-03 00:05:47 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "03-04-2024 07:50:29"
+        "download_time": "03-04-2024 08:05:41"
     },
     {
         "InterviewDate": "01-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-04-2024 07:49:55"
+        "download_time": "03-04-2024 08:05:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6618,7 +6618,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-03",
         "quota_id": "Pret A Manger\u2666TERMINAL 2\u2666Airside\u2666#02-165\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-04-03",
+        "quota_id": "Starbucks\u2666TERMINAL 2\u2666Airside\u2666#026-095-06\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
