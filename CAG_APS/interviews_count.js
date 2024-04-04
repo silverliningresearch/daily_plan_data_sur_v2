794d8393408dba8847f36f9eeb9fe7c36e278c6d
unknown <lb@silverliningresearch.com>
2024-04-04 02:20:41 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "04-04-2024 10:05:34"
+        "download_time": "04-04-2024 10:20:33"
     },
     {
         "InterviewDate": "01-04-2024",
@@ -544,7 +544,7 @@ let interview_statistics  = `[
         "InterviewDate": "04-04-2024",
         "quota_id": "T4_KE_ICN",
         "interviewerID": "02",
-        "Number of interviews": 1
+        "Number of interviews": 6
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "04-04-2024 10:04:58"
+        "download_time": "04-04-2024 10:19:57"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6728,7 +6728,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-04",
         "quota_id": "International Food Hall\u2666TERMINAL 4\u2666Airside\u2666#02-228N\u2666FBA\u2666Min25",
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-04-04",
