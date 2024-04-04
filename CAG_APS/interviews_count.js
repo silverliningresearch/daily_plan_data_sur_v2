eb938dc2101f2ddb8cde261b07e3ff19751b6dd7
unknown <lb@silverliningresearch.com>
2024-04-04 23:05:49 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "05-04-2024 06:50:48"
+        "download_time": "05-04-2024 07:05:40"
     },
     {
         "InterviewDate": "01-04-2024",
@@ -718,7 +718,7 @@ let interview_statistics  = `[
         "InterviewDate": "05-04-2024",
         "quota_id": "T3_SQ_BOM",
         "interviewerID": "02",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "05-04-2024 06:50:12"
+        "download_time": "05-04-2024 07:05:04"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6814,6 +6814,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-04",
         "quota_id": "UOB T4 Arr Mtg Hall Central\u2666TERMINAL 4\u2666Airside\u2666#01-06\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-04-05",
+        "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 3\u2666Airside\u2666#03-15\u2666HL\u2666Min20",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-05",
+        "quota_id": "Ambassador Transit Lounge\u2666TERMINAL 3\u2666Airside\u2666#03-14\u2666HL\u2666Min20",
+        "Number of interviews": 1
     }
 ]    
 
