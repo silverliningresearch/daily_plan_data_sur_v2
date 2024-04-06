d1625486396abe558348082cae0af8be064a17d4
unknown <lb@silverliningresearch.com>
2024-04-06 00:06:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-04-2024 07:50:07"
+        "download_time": "06-04-2024 08:05:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6903,12 +6903,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "Bacha Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-15A\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "Boost Juice\u2666TERMINAL 2\u2666Airside\u2666#02-K16\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-06",
@@ -6923,7 +6923,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "GOPIZZA\u2666TERMINAL 2\u2666Airside\u2666#02-K15\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-06",
