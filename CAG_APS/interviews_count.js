fa723652669fd42cc803b3956cc051addf337037
unknown <lb@silverliningresearch.com>
2024-04-06 03:21:11 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "06-04-2024 11:05:47"
+        "download_time": "06-04-2024 11:20:57"
     },
     {
         "InterviewDate": "01-04-2024",
@@ -905,6 +905,12 @@ let interview_statistics  = `[
         "quota_id": "T2_HO_153",
         "interviewerID": "\u5feb",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "06-04-2024",
+        "quota_id": "T4_3K_BKK",
+        "interviewerID": "12",
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-04-2024 11:05:11"
+        "download_time": "06-04-2024 11:20:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -6918,7 +6918,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "Chef Wei\u2666TERMINAL 2\u2666Airside\u2666#02-K6\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-06",
@@ -6942,6 +6942,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-06",
+        "quota_id": "MA MUM TO GO\u2666TERMINAL 1\u2666Landside\u2666#01-K23\u2666FBL\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-06",
         "quota_id": "Old Chang Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K7\u2666FBA\u2666Min8",
         "Number of interviews": 1
     },
@@ -6958,7 +6963,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "Stefano Ricci\u2666TERMINAL 1\u2666Airside\u2666#02-12B\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-06",
@@ -6968,7 +6973,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-06",
         "quota_id": "The Hainan Story Bakery\u2666TERMINAL 2\u2666Airside\u2666#02-K05\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-06",
