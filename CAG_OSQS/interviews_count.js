522855d7c6a323ba390547ab3ea3509096c5e81e
unknown <lb@silverliningresearch.com>
2024-04-07 01:05:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-04-2024 08:50:13"
+        "download_time": "07-04-2024 09:05:11"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7062,6 +7062,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-07",
+        "quota_id": "Bee Cheng Hiang (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-156 (#026-095-01)\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-07",
         "quota_id": "Garrett Popcorn Shops\u2666TERMINAL 2\u2666Airside\u2666#02-164 (#026-095-05R)\u2666CCD\u2666Min8",
         "Number of interviews": 8
     },
@@ -7083,7 +7088,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-07",
         "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#02-24\u2666PC\u2666Min25",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-04-07",
