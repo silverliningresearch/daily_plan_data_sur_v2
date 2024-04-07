546950e36d8fb9fc5a62dde1683cb1b2a625f0e8
unknown <lb@silverliningresearch.com>
2024-04-07 01:21:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-04-2024 09:05:11"
+        "download_time": "07-04-2024 09:20:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7073,17 +7073,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-07",
         "quota_id": "Kaboom\u2666TERMINAL 2\u2666Airside\u2666#02-162 (#026-098)\u2666CT\u2666Min8",
-        "Number of interviews": 4
-    },
-    {
-        "InterviewDate": "2024-04-07",
-        "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
         "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-07",
+        "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-04-07",
         "quota_id": "Lacoste\u2666TERMINAL 2\u2666Airside\u2666#02-163 (#026-098-01)\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-07",
