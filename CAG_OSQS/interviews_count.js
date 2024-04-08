bf9cf4bb2ca6fc8f0d046e58076b68e2f150230c
unknown <lb@silverliningresearch.com>
2024-04-08 00:51:03 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-04-2024 08:35:26"
+        "download_time": "08-04-2024 08:50:19"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7174,6 +7174,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-08",
         "quota_id": "Airport Pharmacy\u2666TERMINAL 4\u2666Airside\u2666#02-46\u2666PH\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-04-08",
+        "quota_id": "Bacha Coffee\u2666TERMINAL 4\u2666Airside\u2666#02-31\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-08",
+        "quota_id": "Garrett Popcorn Shops\u2666TERMINAL 4\u2666Airside\u2666#02-42\u2666CCD\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-08",
