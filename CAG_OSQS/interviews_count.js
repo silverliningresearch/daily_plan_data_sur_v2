0c91a983c8d553aaf2a0c65440ff7b74a3ea5bc1
unknown <lb@silverliningresearch.com>
2024-04-09 04:21:32 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-04-2024 12:06:04"
+        "download_time": "09-04-2024 12:20:31"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7312,8 +7312,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-09",
-        "quota_id": "The Green Market\u2666TERMINAL 2\u2666Airside\u2666#036-083\u2666FBA\u2666Min8",
+        "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA\u2666Min25",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-09",
+        "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-09",
+        "quota_id": "The Green Market\u2666TERMINAL 2\u2666Airside\u2666#036-083\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-09",
