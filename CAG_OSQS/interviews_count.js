413adaa2a2f2c103a63939b660444d7d20225ba0
unknown <lb@silverliningresearch.com>
2024-04-09 00:08:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-04-2024 07:50:23"
+        "download_time": "09-04-2024 08:06:36"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7288,7 +7288,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-09",
         "quota_id": "Premises F - Baggage Storage by Smarte Carte\u2666TERMINAL 3\u2666Airside\u2666#02-68/69\u2666BS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 5
     }
 ]    
 
