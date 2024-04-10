b8fa93af3a8c4300564dcd77931d9d143750b790
unknown <lb@silverliningresearch.com>
2024-04-10 23:21:15 +0000

    update new data

--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "11-04-2024 01:06:08"
+        "download_time": "11-04-2024 01:21:14"
     },
     {
         "InterviewDate": "2024-01-28",
