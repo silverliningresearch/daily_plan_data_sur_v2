b1242462c854b2b7507b218e7633e9797d051ca1
unknown <lb@silverliningresearch.com>
2024-04-10 16:36:12 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "10-04-2024 18:19:52"
+        "download_time": "10-04-2024 18:34:52"
     },
     {
         "InterviewerID": 1,
@@ -1033,7 +1033,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2024-04-10",
-        "completed_interviews": 34
+        "completed_interviews": 36
     },
     {
         "InterviewerID": 3,
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "10-04-2024 18:19:52"
+        "download_time": "10-04-2024 18:34:52"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -50348,7 +50348,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-04-10",
         "quota_id": "BCN-VY",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-10",
