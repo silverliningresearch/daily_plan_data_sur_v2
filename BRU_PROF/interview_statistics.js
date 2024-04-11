3c9b313c30dedb1eb8425ced6046d583e6b4c07c
unknown <lb@silverliningresearch.com>
2024-04-11 04:06:45 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "11-04-2024 05:49:52"
+        "download_time": "11-04-2024 06:05:08"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -55544,6 +55544,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-10",
         "quota_id": "XR7697-HRG",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-11",
+        "quota_id": "AY1550-HEL",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-11",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-04-2024 11:50:14"
+        "download_time": "11-04-2024 12:06:00"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7432,6 +7432,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-11",
+        "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-11",
         "quota_id": "Discover Singapore (Premises B) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-33/34\u2666SV\u2666Min8",
         "Number of interviews": 9
     },
@@ -7458,7 +7463,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-11",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-11",
@@ -7474,6 +7479,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-11",
         "quota_id": "Tumi\u2666TERMINAL 1\u2666Airside\u2666#02-31N\u2666MP\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-11",
+        "quota_id": "UOB T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K27\u2666MC\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-11",
