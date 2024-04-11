b0717d1a68bd4afe010bdd94df2ebde7a81653b5
unknown <lb@silverliningresearch.com>
2024-04-11 03:50:57 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-04-2024 11:35:14"
+        "download_time": "11-04-2024 11:50:14"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7443,7 +7443,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-11",
         "quota_id": "LT T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-10/11/12\u2666LT\u2666Min15",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-11",
@@ -7458,7 +7458,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-11",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 3
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-04-11",
+        "quota_id": "Saint Laurent\u2666TERMINAL 3\u2666Airside\u2666#02-06(N)\u2666LX\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-11",
