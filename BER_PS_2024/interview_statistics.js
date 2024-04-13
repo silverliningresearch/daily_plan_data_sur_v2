950ab85b5baa2ab061769eb6b5bbca68521f4ef0
unknown <lb@silverliningresearch.com>
2024-04-13 09:05:42 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "13-04-2024 10:49:51"
+        "download_time": "13-04-2024 11:04:51"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -4399,6 +4399,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-12",
         "quota_id": "ZRH - Eurowings",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-04-13",
+        "quota_id": "ATH - Aegean Airlines",
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-04-2024 16:49:58"
+        "download_time": "13-04-2024 17:04:57"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7643,7 +7643,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-13",
         "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-13",
