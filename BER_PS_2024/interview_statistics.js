6d6c1efc9180480d0bbb0bae8b1ad10e93298a54
unknown <lb@silverliningresearch.com>
2024-04-13 10:20:40 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "13-04-2024 12:05:46"
+        "download_time": "13-04-2024 12:19:50"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -4418,7 +4418,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-13",
         "quota_id": "MUC - Lufthansa",
-        "Number of interviews": 21
+        "Number of interviews": 22
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-04-2024 18:05:53"
+        "download_time": "13-04-2024 18:19:55"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7644,6 +7644,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-13",
         "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP\u2666Min8",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-13",
+        "quota_id": "KOPITIAM\u2666TERMINAL 4\u2666Landside\u2666#02-205\u2666FBL\u2666Min25",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-13",
