9eb45d5665f42eb78744fb8a809de743969bc678
unknown <lb@silverliningresearch.com>
2024-04-14 04:35:52 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "14-04-2024 08:20:52"
+        "download_time": "14-04-2024 08:35:51"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -61,7 +61,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-14",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "14-04-2024 12:20:05"
+        "download_time": "14-04-2024 12:35:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7678,7 +7678,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-14",
         "quota_id": "Famous Amos\u2666TERMINAL 3\u2666Landside\u2666#02-97\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-14",
