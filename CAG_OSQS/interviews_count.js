d71cecefba3831146e2e4fa691f7881b74e8ffc2
unknown <lb@silverliningresearch.com>
2024-04-14 04:20:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "14-04-2024 12:05:34"
+        "download_time": "14-04-2024 12:20:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7673,7 +7673,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-14",
         "quota_id": "CHEERS\u2666TERMINAL 3\u2666Landside\u2666#02-98\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-14",
+        "quota_id": "Famous Amos\u2666TERMINAL 3\u2666Landside\u2666#02-97\u2666LS\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-14",
@@ -7693,7 +7698,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-14",
         "quota_id": "Simply Toys\u2666TERMINAL 3\u2666Landside\u2666#02-95/96\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "14-04-2024 06:07:07"
+        "download_time": "14-04-2024 06:20:47"
     },
     {
         "InterviewDate": "2023-12-21",
@@ -3599,6 +3599,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-14",
         "quota_id": "AF-CDG",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-14",
+        "quota_id": "FR-DBV",
+        "Number of interviews": 4
     }
 ]    
 
