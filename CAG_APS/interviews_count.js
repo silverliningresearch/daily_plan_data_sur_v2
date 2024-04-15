240b4e44c78c713903bc54d8a0697865c0a411e5
unknown <lb@silverliningresearch.com>
2024-04-15 09:21:13 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "15-04-2024 17:05:34"
+        "download_time": "15-04-2024 17:20:34"
     },
     {
         "InterviewDate": "01-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-04-2024 17:04:59"
+        "download_time": "15-04-2024 17:19:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7738,7 +7738,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-15",
         "quota_id": "Burberry\u2666TERMINAL 1\u2666Airside\u2666#02-80\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-15",
@@ -7768,12 +7768,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-15",
         "quota_id": "Michael Kors\u2666TERMINAL 1\u2666Airside\u2666#02-76\u2666LX\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-15",
         "quota_id": "Pedro\u2666TERMINAL 1\u2666Airside\u2666#02-74\u2666MP\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-15",
@@ -7788,7 +7788,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-15",
         "quota_id": "adidas Originals\u2666TERMINAL 1\u2666Airside\u2666#02-73\u2666MP\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     }
 ]    
 
