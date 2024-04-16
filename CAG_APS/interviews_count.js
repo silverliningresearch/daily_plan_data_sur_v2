6e12173b6be1042899cd1e749831be542dbf59c4
unknown <lb@silverliningresearch.com>
2024-04-16 07:05:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "16-04-2024 14:50:48"
+        "download_time": "16-04-2024 15:05:46"
     },
     {
         "InterviewDate": "01-04-2024",
@@ -1612,7 +1612,7 @@ let interview_statistics  = `[
         "InterviewDate": "16-04-2024",
         "quota_id": "T3_GA_CGK",
         "interviewerID": "02",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "16-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-04-2024 14:50:11"
+        "download_time": "16-04-2024 15:05:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7828,17 +7828,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-16",
         "quota_id": "Hugo Boss\u2666TERMINAL 3\u2666Airside\u2666#02-23\u2666LX\u2666Min8",
-        "Number of interviews": 4
-    },
-    {
-        "InterviewDate": "2024-04-16",
-        "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
         "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-16",
+        "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-04-16",
         "quota_id": "Maison de Chronus (Premises A2) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-24\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-16",
@@ -7883,7 +7883,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-16",
         "quota_id": "XW Western Grill \u2666TERMINAL 2\u2666Landside\u2666#03-23\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
