f340ac5340abfafddaa81e163a90cffc9f72354c
unknown <lb@silverliningresearch.com>
2024-04-16 03:50:58 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-04-2024 11:35:07"
+        "download_time": "16-04-2024 11:50:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7792,8 +7792,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-16",
+        "quota_id": "Burberry\u2666TERMINAL 3\u2666Airside\u2666#02-20\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-16",
         "quota_id": "CHUTNEY MARY INDIAN FAST FOOD\u2666TERMINAL 2\u2666Landside\u2666#03-26 &27\u2666FBL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-04-16",
+        "quota_id": "Foodies' Clan\u2666TERMINAL 2\u2666Landside\u2666#0610-ASC\u2666FBL\u2666Min25",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-04-16",
