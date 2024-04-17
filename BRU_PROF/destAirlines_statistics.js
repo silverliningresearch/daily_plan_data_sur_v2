6dfabc22953959e5076bb02b2368fb54544e64af
unknown <lb@silverliningresearch.com>
2024-04-17 01:50:54 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "17-04-2024 03:34:51"
+        "download_time": "17-04-2024 03:49:50"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "17-04-2024 09:35:08"
+        "download_time": "17-04-2024 09:50:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7913,7 +7913,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-17",
         "quota_id": "Metapod\u2666TERMINAL 4\u2666Airside\u2666#02-51\u2666EL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-17",
@@ -7923,7 +7923,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-17",
         "quota_id": "WH Smith - Premises J (Air)\u2666TERMINAL 4\u2666Airside\u2666#02-59\u2666OW\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     }
 ]    
 
