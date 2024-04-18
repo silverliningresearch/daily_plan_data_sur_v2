43017e6f197c109fbc4b4d8abaccac8112fbe1a6
unknown <lb@silverliningresearch.com>
2024-04-18 05:51:00 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "18-04-2024 09:35:46"
+        "download_time": "18-04-2024 09:50:59"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -250,7 +250,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-18",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-04-2024 13:35:00"
+        "download_time": "18-04-2024 13:50:11"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8033,7 +8033,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-18",
         "quota_id": "UOB T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209B\u2666MC\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
