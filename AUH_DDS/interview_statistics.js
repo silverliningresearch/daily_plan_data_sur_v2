1f43e3872532947e6c19fa05877ae84317220fbf
unknown <lb@silverliningresearch.com>
2024-04-18 19:36:05 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "18-04-2024 23:21:47"
+        "download_time": "18-04-2024 23:36:05"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -292,7 +292,7 @@ let interview_statistics = `[
         "quota_id": "6-6",
         "Survey": 6,
         "Location": 1,
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-18",
@@ -306,7 +306,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "18-04-2024 21:20:03"
+        "download_time": "18-04-2024 21:35:06"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -4644,6 +4644,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-18",
         "quota_id": "PSA - Ryanair",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-04-18",
+        "quota_id": "SKP - Wizz Air",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-18",
