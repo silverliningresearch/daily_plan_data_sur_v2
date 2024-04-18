3f7b2dfd480d2b08c28893497bfefd1fe7782631
unknown <lb@silverliningresearch.com>
2024-04-18 11:21:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-04-2024 19:05:11"
+        "download_time": "18-04-2024 19:20:11"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -7998,7 +7998,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-18",
         "quota_id": "Adidas\u2666TERMINAL 2\u2666Airside\u2666#02-168 (#026-106/7)\u2666MP\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-04-18",
@@ -8019,6 +8019,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-18",
         "quota_id": "LT T2 Arrival North\u2666TERMINAL 2\u2666Airside\u2666#01-151\u2666LT\u2666Min15",
         "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2024-04-18",
+        "quota_id": "LT T2 Duplex\u2666TERMINAL 2\u2666Airside\u2666#02-167 & #03-189\u2666LT\u2666Min25",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-18",
