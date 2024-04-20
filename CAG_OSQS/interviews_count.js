96320eda4b0f8798ed05f34c6139edfbf4a49b1b
unknown <lb@silverliningresearch.com>
2024-04-20 02:06:50 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-04-2024 09:50:19"
+        "download_time": "20-04-2024 10:05:19"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8179,6 +8179,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-19",
         "quota_id": "WH Smith - Premises A (Air) (T1 DTE)\u2666TERMINAL 1\u2666Airside\u2666#02-64\u2666OW\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-04-20",
+        "quota_id": "Charles & Keith\u2666TERMINAL 1\u2666Airside\u2666#02-51\u2666MP\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-20",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "20-04-2024 05:51:02"
+        "download_time": "20-04-2024 06:06:31"
     },
     {
         "InterviewDate": "2024-01-07",
