474162598a6965ef790f884b50623286f84a6738
unknown <lb@silverliningresearch.com>
2024-04-21 11:51:15 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "21-04-2024 15:36:24"
+        "download_time": "21-04-2024 15:51:14"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -446,7 +446,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 4
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-04-21",
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "21-04-2024 13:35:11"
+        "download_time": "21-04-2024 13:50:13"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -4758,7 +4758,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-21",
         "quota_id": "EDI - Ryanair",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-21",
