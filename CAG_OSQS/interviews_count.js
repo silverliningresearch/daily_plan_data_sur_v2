770cd4bc4ce334b110528e47523c7357a3be89e7
unknown <lb@silverliningresearch.com>
2024-04-22 03:06:08 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-04-2024 10:50:26"
+        "download_time": "22-04-2024 11:05:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8384,6 +8384,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-22",
         "quota_id": "Charles & Keith\u2666TERMINAL 3\u2666Airside\u2666#02-53\u2666MP\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-04-22",
+        "quota_id": "Discover Singapore (Premises E) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-63\u2666SV\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-22",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "22-04-2024 06:51:04"
+        "download_time": "22-04-2024 07:05:58"
     },
     {
         "InterviewDate": "2024-01-07",
