a4d77fd5d857c78670f93139b9a7cc5806f06240
unknown <lb@silverliningresearch.com>
2024-04-24 03:51:34 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-04-2024 11:35:30"
+        "download_time": "24-04-2024 11:50:40"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8597,6 +8597,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-24",
+        "quota_id": "Avenue Kids\u2666TERMINAL 4\u2666Airside\u2666#02-63\u2666CT\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-24",
         "quota_id": "Discover Singapore\u2666TERMINAL 4\u2666Airside\u2666#02-73/74/75/76/77\u2666SV\u2666Min8",
         "Number of interviews": 8
     },
@@ -8618,7 +8623,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-24",
         "quota_id": "PARIS BAGUETTE\u2666TERMINAL 3\u2666Landside\u2666#01-22\u2666FBL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-24",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "24-04-2024 05:36:18"
+        "download_time": "24-04-2024 05:51:28"
     },
     {
         "InterviewDate": "2024-01-28",
@@ -628,7 +628,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-24",
         "quota_id": "W4-EIN",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
