b3cf0173a315b8fa1cff9f57f2f23d87c7fa1336
unknown <lb@silverliningresearch.com>
2024-04-25 05:36:08 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "25-04-2024 13:20:18"
+        "download_time": "25-04-2024 13:35:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8738,6 +8738,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-25",
         "quota_id": "Burberry\u2666TERMINAL 2\u2666Airside\u2666#02-214\u2666LX\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-25",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-05 (beside iSC counter near lifts)\u2666CR\u2666Min8",
         "Number of interviews": 1
     },
     {
@@ -8748,7 +8753,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-25",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-K15 (near taxi stand)\u2666CR\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-04-25",
@@ -8763,7 +8768,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-25",
         "quota_id": "Discover Singapore (Premises D) (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-191\u2666SV\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-04-25",
@@ -8803,7 +8808,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-25",
         "quota_id": "Samsonite\u2666TERMINAL 2\u2666Airside\u2666#02-190\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-04-25",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "25-04-2024 07:21:19"
+        "download_time": "25-04-2024 07:36:00"
     },
     {
         "InterviewDate": "2023-12-21",
