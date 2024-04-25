d20a7808a6eaf13153fb104f20e2046105af1780
unknown <lb@silverliningresearch.com>
2024-04-25 20:51:07 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "26-04-2024 00:36:19"
+        "download_time": "26-04-2024 00:51:06"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -691,7 +691,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-04-26",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "25-04-2024 22:35:02"
+        "download_time": "25-04-2024 22:49:54"
     },
     {
         "InterviewDate": "2023-04-01",
