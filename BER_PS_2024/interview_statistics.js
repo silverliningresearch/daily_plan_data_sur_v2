609507f6e44f6b6fee0df740962ce09d551cafda
unknown <lb@silverliningresearch.com>
2024-04-25 19:51:44 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "25-04-2024 21:35:07"
+        "download_time": "25-04-2024 21:50:43"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -4913,7 +4913,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-04-25",
         "quota_id": "DUB - Aer Lingus",
-        "Number of interviews": 10
+        "Number of interviews": 21
     },
     {
         "InterviewDate": "2024-04-25",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "26-04-2024 03:35:54"
+        "download_time": "26-04-2024 03:51:30"
     },
     {
         "InterviewDate": "01-04-2024",
