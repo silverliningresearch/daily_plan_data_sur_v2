1ff9cd35e2fca109731124be9161e6f22fa41711
unknown <lb@silverliningresearch.com>
2024-04-26 01:06:10 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "26-04-2024 08:50:55"
+        "download_time": "26-04-2024 09:05:54"
     },
     {
         "InterviewDate": "01-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-04-2024 08:50:16"
+        "download_time": "26-04-2024 09:05:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -8894,6 +8894,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-25",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-04-26",
+        "quota_id": "CHEERS\u2666TERMINAL 4\u2666Landside\u2666#01-09\u2666LS\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
