7e8bcc72d45367c9c0ed2c1899fb3b19f5fb705a
unknown <lb@silverliningresearch.com>
2024-04-27 02:21:09 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "27-04-2024 06:06:52"
+        "download_time": "27-04-2024 06:21:08"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-04-2024 10:05:59"
+        "download_time": "27-04-2024 10:20:16"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9048,7 +9048,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-27",
         "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-04-27",
