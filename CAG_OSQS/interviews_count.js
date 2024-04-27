ded29abf503d93923ce1db662d74af4e89f5cef5
unknown <lb@silverliningresearch.com>
2024-04-27 04:51:08 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-04-2024 12:35:23"
+        "download_time": "27-04-2024 12:50:17"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9032,6 +9032,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-27",
+        "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 3\u2666Landside\u2666#01-K20\u2666CR\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-27",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 3\u2666Landside\u2666#02-90A\u2666CR\u2666Min8",
         "Number of interviews": 3
     },
@@ -9048,7 +9053,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-27",
         "quota_id": "Gassan Watches (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-226\u2666LX\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-04-27",
@@ -9084,6 +9089,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-04-27",
         "quota_id": "Polo Ralph Lauren\u2666TERMINAL 2\u2666Airside\u2666#02-225-04\u2666LX\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-04-27",
+        "quota_id": "TRAVELLERS\u2666TERMINAL 3\u2666Landside\u2666#01-K17\u2666CR\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-04-27",
