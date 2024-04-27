7f4ce181a9d56e9e45c193e40399b4229d3d20d0
unknown <lb@silverliningresearch.com>
2024-04-27 01:36:12 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-04-2024 09:20:18"
+        "download_time": "27-04-2024 09:35:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9037,6 +9037,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-04-27",
+        "quota_id": "Gassan Watches (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-226\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-04-27",
         "quota_id": "Gucci\u2666TERMINAL 2\u2666Airside\u2666#02-223\u2666LX\u2666Min8",
         "Number of interviews": 7
     },
@@ -9053,6 +9058,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-27",
         "quota_id": "Moncler\u2666TERMINAL 2\u2666Airside\u2666#02-225-03\u2666LX\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-04-27",
+        "quota_id": "Polo Ralph Lauren\u2666TERMINAL 2\u2666Airside\u2666#02-225-04\u2666LX\u2666Min8",
         "Number of interviews": 1
     },
     {
