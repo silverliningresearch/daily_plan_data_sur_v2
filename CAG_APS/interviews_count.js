7782a37e4f17a187ab9762d7caf62f174c531097
unknown <lb@silverliningresearch.com>
2024-04-30 01:51:25 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_XMN",
         "interviewerID": "02",
         "Number of interviews": 2,
-        "download_time": "30-04-2024 09:35:57"
+        "download_time": "30-04-2024 09:51:08"
     },
     {
         "InterviewDate": "01-04-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-04-2024 09:35:21"
+        "download_time": "30-04-2024 09:50:30"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9398,7 +9398,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-04-30",
         "quota_id": "YAKUN FAMILY CAFE & BEARD PAPA'S\u2666TERMINAL 3\u2666Landside\u2666#B2-07\u2666FBL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
