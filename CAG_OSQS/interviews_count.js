02d105fb39720fef7b6d2f3eab21bb9115e0ac7f
unknown <lb@silverliningresearch.com>
2024-05-02 23:36:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-05-2024 07:20:21"
+        "download_time": "03-05-2024 07:35:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9554,6 +9554,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-02",
         "quota_id": "Tip Top Curry Puff\u2666TERMINAL 1\u2666Airside\u2666#02-K11N\u2666FBA\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-05-03",
+        "quota_id": "Travelex T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209A\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
