ff9cd9468f85fc20d85969c2904b859732172a97
unknown <lb@silverliningresearch.com>
2024-05-02 01:51:29 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-05-2024 09:35:24"
+        "download_time": "02-05-2024 09:50:34"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9462,6 +9462,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-05-02",
+        "quota_id": "Irvins Salted Egg\u2666TERMINAL 1\u2666Airside\u2666#02-K07A\u2666CCD\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-05-02",
         "quota_id": "Jamie's Deli\u2666TERMINAL 1\u2666Airside\u2666#02-40N\u2666FBA\u2666Min8",
         "Number of interviews": 2
     },
@@ -9479,6 +9484,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-02",
         "quota_id": "SG Hawker\u2666TERMINAL 1\u2666Airside\u2666#03-47/48\u2666FBA\u2666Min25",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-05-02",
+        "quota_id": "Tip Top Curry Puff\u2666TERMINAL 1\u2666Airside\u2666#02-K11N\u2666FBA\u2666Min8",
+        "Number of interviews": 3
     }
 ]    
 
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "02-05-2024 03:36:10"
+        "download_time": "02-05-2024 03:51:20"
     },
     {
         "InterviewDate": "2024-05-01",
