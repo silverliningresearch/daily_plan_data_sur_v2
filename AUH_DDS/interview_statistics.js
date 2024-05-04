f9be475244e2a1da43301400fea6112cea3c6342
unknown <lb@silverliningresearch.com>
2024-05-04 06:51:55 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "04-05-2024 10:36:53"
+        "download_time": "04-05-2024 10:51:54"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -1090,7 +1090,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-05-04",
@@ -1104,7 +1104,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 2,
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "04-05-2024 14:35:52"
+        "download_time": "04-05-2024 14:50:52"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9678,7 +9678,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-04",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#02-K7\u2666CR\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-05-04",
@@ -9703,7 +9703,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-04",
         "quota_id": "LIHO TEA\u2666TERMINAL 4\u2666Landside\u2666#01-08\u2666LS\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-04",
@@ -9718,7 +9718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-04",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-04",
