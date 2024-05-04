2ef095e59a7e706fd728a03617a4c3657cbcdab7
unknown <lb@silverliningresearch.com>
2024-05-04 10:51:53 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "04-05-2024 12:35:48"
+        "download_time": "04-05-2024 12:50:15"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "04-05-2024 18:36:24"
+        "download_time": "04-05-2024 18:50:52"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9718,7 +9718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-04",
         "quota_id": "PENIWERN\u2666TERMINAL 3\u2666Landside\u2666#B2-38\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-05-04",
