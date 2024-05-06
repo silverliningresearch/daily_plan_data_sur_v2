b989859e0d3e293f4b2e1221f539e1c70f07395f
unknown <lb@silverliningresearch.com>
2024-05-06 06:21:26 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "06-05-2024 10:07:25"
+        "download_time": "06-05-2024 10:21:24"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -1223,7 +1223,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-05-06",
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "06-05-2024 08:06:00"
+        "download_time": "06-05-2024 08:20:16"
     },
     {
         "InterviewDate": "2023-12-31",
