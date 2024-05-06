03a5623ddffb08575f0eea3886f0ec621f4aa45a
unknown <lb@silverliningresearch.com>
2024-05-06 04:07:15 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-05-2024 11:50:32"
+        "download_time": "06-05-2024 12:06:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9898,7 +9898,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-06",
         "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 2\u2666Airside\u2666#02-800\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-06",
