02528aded5f226fe9958865f1566aed40368632d
unknown <lb@silverliningresearch.com>
2024-05-08 02:36:01 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-05-2024 10:20:09"
+        "download_time": "08-05-2024 10:35:08"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -9969,6 +9969,21 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-08",
         "quota_id": "Guardian (Premises B) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-16\u2666PH\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-05-08",
+        "quota_id": "Prada\u2666TERMINAL 1\u2666Airside\u2666#02-12A\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-08",
+        "quota_id": "Stefano Ricci\u2666TERMINAL 1\u2666Airside\u2666#02-12B\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-08",
+        "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
