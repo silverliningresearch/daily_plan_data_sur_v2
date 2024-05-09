c996a4b338b8ddf9367261820864dc0bcea64223
unknown <lb@silverliningresearch.com>
2024-05-09 10:06:58 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-05-2024 17:50:11"
+        "download_time": "09-05-2024 18:05:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10028,12 +10028,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Boost Juice\u2666TERMINAL 2\u2666Airside\u2666#02-K16\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Buk Chang Dong Soon Tofu\u2666TERMINAL 2\u2666Airside\u2666#02-K12\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-09",
@@ -10063,7 +10063,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "GOPIZZA\u2666TERMINAL 2\u2666Airside\u2666#02-K15\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-09",
@@ -10078,7 +10078,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Jinjja Kitchen\u2666TERMINAL 2\u2666Landside\u2666#02-02\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-05-09",
@@ -10098,7 +10098,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "LiHO TEA\u2666TERMINAL 2\u2666Airside\u2666#02-K13\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-05-09",
