e9a893194259c43e6ffcdf35628d14ad8d590609
unknown <lb@silverliningresearch.com>
2024-05-09 08:36:07 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "09-05-2024 10:19:54"
+        "download_time": "09-05-2024 10:34:54"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -54863,7 +54863,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "NSI-SN",
-        "Number of interviews": 3
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2024-05-09",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-05-2024 16:20:13"
+        "download_time": "09-05-2024 16:35:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10072,13 +10072,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-05-09",
+        "quota_id": "Jinjja Kitchen\u2666TERMINAL 2\u2666Landside\u2666#02-02\u2666FBL\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-09",
         "quota_id": "Jones The Grocer\u2666TERMINAL 2\u2666Landside\u2666#02-01 & #03-01\u2666FBL\u2666Min8",
         "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Kenangan Coffee\u2666TERMINAL 2\u2666Landside\u2666#02-12\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-05-09",
