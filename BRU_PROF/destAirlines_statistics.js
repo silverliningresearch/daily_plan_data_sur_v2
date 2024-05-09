e1c0b74c0d41c8f45ba5708af018f8dd2c758192
unknown <lb@silverliningresearch.com>
2024-05-09 09:06:11 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "09-05-2024 10:49:54"
+        "download_time": "09-05-2024 11:04:55"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-05-2024 16:50:14"
+        "download_time": "09-05-2024 17:05:15"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10078,7 +10078,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Jones The Grocer\u2666TERMINAL 2\u2666Landside\u2666#02-01 & #03-01\u2666FBL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-09",
@@ -10098,7 +10098,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Marc Jacobs\u2666TERMINAL 2\u2666Airside\u2666#02-227\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-05-09",
@@ -10118,7 +10118,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-09",
         "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-05-09",
