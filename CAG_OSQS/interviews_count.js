62bf855cc6d8568f697aa30118f10ca1a836868b
unknown <lb@silverliningresearch.com>
2024-05-11 11:36:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-05-2024 19:20:17"
+        "download_time": "11-05-2024 19:35:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10268,7 +10268,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-11",
         "quota_id": "KOPITIAM\u2666TERMINAL 4\u2666Landside\u2666#02-205\u2666FBL\u2666Min25",
-        "Number of interviews": 14
+        "Number of interviews": 15
     },
     {
         "InterviewDate": "2024-05-11",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "11-05-2024 15:21:15"
+        "download_time": "11-05-2024 15:35:55"
     },
     {
         "InterviewDate": "2024-01-07",
