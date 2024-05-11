4e9ad0424ec61eb21894ba3e4367e7f199468593
unknown <lb@silverliningresearch.com>
2024-05-11 21:51:20 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "11-05-2024 23:35:09"
+        "download_time": "11-05-2024 23:50:11"
     },
     {
         "InterviewDate": "2023-12-31",
