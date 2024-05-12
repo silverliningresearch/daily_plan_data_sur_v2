4d1e328a365aa0d6f59562974ce6cc017ed703b1
unknown <lb@silverliningresearch.com>
2024-05-12 03:06:13 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "12-05-2024 06:51:12"
+        "download_time": "12-05-2024 07:06:12"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-05-2024 10:50:17"
+        "download_time": "12-05-2024 11:05:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10298,7 +10298,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-12",
         "quota_id": "Discover Singapore (Premises F) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03A\u2666SV\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-05-12",
