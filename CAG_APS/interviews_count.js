b219f23fc6c97aede6c21c470e0dbfc6e478a6f2
unknown <lb@silverliningresearch.com>
2024-05-13 10:21:25 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_MH_KUL",
         "interviewerID": "02",
         "Number of interviews": 7,
-        "download_time": "13-05-2024 18:07:22"
+        "download_time": "13-05-2024 18:21:02"
     },
     {
         "InterviewDate": "01-05-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-05-2024 18:06:38"
+        "download_time": "13-05-2024 18:20:27"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10368,12 +10368,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-13",
         "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u2666#02-32\u2666OS\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-05-13",
         "quota_id": "Burger King\u2666TERMINAL 3\u2666Airside\u2666#03-13\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-05-13",
@@ -10438,7 +10438,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-13",
         "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA\u2666Min25",
-        "Number of interviews": 8
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-05-13",
