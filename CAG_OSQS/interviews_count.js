cb7e7bf23fca0ed3341a890e3600628a3650d196
unknown <lb@silverliningresearch.com>
2024-05-13 23:21:32 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "14-05-2024 07:05:23"
+        "download_time": "14-05-2024 07:20:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10484,6 +10484,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-13",
         "quota_id": "WH Smith - Premises D (Air) (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#026-055-02A (#02-240)\u2666OW\u2666Min8",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-05-14",
+        "quota_id": "SG Hawker\u2666TERMINAL 1\u2666Airside\u2666#03-47/48\u2666FBA\u2666Min25",
+        "Number of interviews": 3
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "14-05-2024 01:06:07"
+        "download_time": "14-05-2024 01:21:17"
     },
     {
         "InterviewDate": "2023-12-21",
