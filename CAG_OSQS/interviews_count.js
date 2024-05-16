d07f3c40566cba8c3ec41b12eafa58f1bc05f27c
unknown <lb@silverliningresearch.com>
2024-05-16 23:21:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "17-05-2024 07:05:35"
+        "download_time": "17-05-2024 07:20:38"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -10983,7 +10983,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-17",
         "quota_id": "STARBUCKS COFFEE\u2666TERMINAL 2\u2666Landside\u2666#02-33\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "17-05-2024 01:06:23"
+        "download_time": "17-05-2024 01:21:27"
     },
     {
         "InterviewDate": "2023-12-21",
