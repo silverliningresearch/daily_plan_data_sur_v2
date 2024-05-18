b6ad1e90f1468ff39dd9e4548e98c8d2a078c3af
unknown <lb@silverliningresearch.com>
2024-05-18 02:36:42 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "18-05-2024 04:20:23"
+        "download_time": "18-05-2024 04:35:23"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-05-2024 10:20:36"
+        "download_time": "18-05-2024 10:35:36"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11098,7 +11098,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-18",
         "quota_id": "7-Eleven Cafe\u2666TERMINAL 3\u2666Landside\u2666#B2-05/06\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-05-18",
@@ -11128,7 +11128,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-18",
         "quota_id": "Metapod\u2666TERMINAL 4\u2666Airside\u2666#02-51\u2666EL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-18",
