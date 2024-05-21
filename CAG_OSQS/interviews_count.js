8931bb9a3d3816366abf1eb23a3e086cc904b4e2
unknown <lb@silverliningresearch.com>
2024-05-21 00:37:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-05-2024 08:20:41"
+        "download_time": "21-05-2024 08:35:43"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11303,7 +11303,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-21",
         "quota_id": "Irvin's\u2666TERMINAL 4\u2666Airside\u2666#02-71\u2666CCD\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-05-21",
@@ -11312,8 +11312,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-05-21",
+        "quota_id": "The Fashion Place\u2666TERMINAL 4\u2666Airside\u2666#02-60/61\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-21",
         "quota_id": "WH Smith - Premises J (Air)\u2666TERMINAL 4\u2666Airside\u2666#02-59\u2666OW\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
