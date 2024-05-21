7fd25a60030f7c5b57f8baa24373e8021bba2669
unknown <lb@silverliningresearch.com>
2024-05-21 03:36:51 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-05-2024 11:20:51"
+        "download_time": "21-05-2024 11:35:44"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11303,7 +11303,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-21",
         "quota_id": "Airport Pharmacy\u2666TERMINAL 4\u2666Airside\u2666#02-55/56\u2666PH\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-21",
@@ -11334,6 +11334,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-21",
         "quota_id": "The Fashion Place\u2666TERMINAL 4\u2666Airside\u2666#02-60/61\u2666LX\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-05-21",
+        "quota_id": "Travelex T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#02-235\u2666MC\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-05-21",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "21-05-2024 07:21:42"
+        "download_time": "21-05-2024 07:36:25"
     },
     {
         "InterviewDate": "2024-01-07",
