880d42cac026dc543a6f6383437496e08357995f
unknown <lb@silverliningresearch.com>
2024-05-21 02:21:48 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-05-2024 10:06:53"
+        "download_time": "21-05-2024 10:20:41"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11327,13 +11327,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-05-21",
+        "quota_id": "UOB T2 DT North\u2666TERMINAL 2\u2666Airside\u2666#02-237\u2666MC\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-21",
         "quota_id": "WH Smith - Premises J (Air)\u2666TERMINAL 4\u2666Airside\u2666#02-59\u2666OW\u2666Min8",
         "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-05-21",
         "quota_id": "iStudio\u2666TERMINAL 4\u2666Airside\u2666#02-52\u2666EL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
