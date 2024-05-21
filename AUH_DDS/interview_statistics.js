41583e05f46630ba280566a7445f5c8ef3759e5f
unknown <lb@silverliningresearch.com>
2024-05-21 07:51:53 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "21-05-2024 11:37:04"
+        "download_time": "21-05-2024 11:51:51"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-05-2024 15:35:58"
+        "download_time": "21-05-2024 15:50:45"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11313,7 +11313,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-21",
         "quota_id": "Ambassador Transit Lounge\u2666TERMINAL 2\u2666Airside\u2666#036-123&123-A\u2666HL\u2666Min20",
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-05-21",
@@ -11328,7 +11328,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-21",
         "quota_id": "Charles & Keith\u2666TERMINAL 4\u2666Airside\u2666#02-30\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-05-21",
