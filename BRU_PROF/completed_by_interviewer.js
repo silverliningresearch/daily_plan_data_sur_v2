a71388ba923740e4360339f5e49f1fa7a0d656a5
unknown <lb@silverliningresearch.com>
2024-05-22 22:51:55 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "23-05-2024 00:35:14"
+        "download_time": "23-05-2024 00:50:12"
     },
     {
         "InterviewerID": 1,
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-05-2024 06:35:52"
+        "download_time": "23-05-2024 06:50:47"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11473,6 +11473,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-23",
         "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-05-23",
+        "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#02-24\u2666PC\u2666Min25",
         "Number of interviews": 1
     }
 ]    
