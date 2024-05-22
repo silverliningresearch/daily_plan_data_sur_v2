91681bf219336c5c0f0e4ecf95048d128fb52af2
unknown <lb@silverliningresearch.com>
2024-05-22 06:52:00 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-05-2024 14:35:46"
+        "download_time": "22-05-2024 14:50:33"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11448,7 +11448,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-22",
         "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min15",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-05-22",
@@ -11463,7 +11463,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-22",
         "quota_id": "The Cocoa Trees (Premises C1) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-27\u2666CCD\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "22-05-2024 10:36:26"
+        "download_time": "22-05-2024 10:51:15"
     },
     {
         "InterviewDate": "2024-01-07",
