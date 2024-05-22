60dce4ea9784d1b982dde7a1dac383132a97ab4d
unknown <lb@silverliningresearch.com>
2024-05-22 05:36:24 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-05-2024 13:20:28"
+        "download_time": "22-05-2024 13:35:24"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -11423,7 +11423,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-22",
         "quota_id": "Hugo Boss\u2666TERMINAL 3\u2666Airside\u2666#02-23\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-05-22",
@@ -11438,7 +11438,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-22",
         "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-05-22",
@@ -11453,7 +11453,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-22",
         "quota_id": "TWG Tea Company (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-22",
