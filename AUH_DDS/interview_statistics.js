2cb1b00f09a9115650c25bf6b1195df2176af716
unknown <lb@silverliningresearch.com>
2024-05-25 19:21:37 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "25-05-2024 23:06:33"
+        "download_time": "25-05-2024 23:21:35"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -2336,7 +2336,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 5
+        "Number of interviews": 9
     }
 ]    
 
