c736036fde7f7e85bfbb7a5d6c8cf4a5f39b0966
unknown <lb@silverliningresearch.com>
2024-05-27 00:37:19 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_MH_KUL",
         "interviewerID": "02",
         "Number of interviews": 7,
-        "download_time": "27-05-2024 08:21:39"
+        "download_time": "27-05-2024 08:36:45"
     },
     {
         "InterviewDate": "01-05-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-05-2024 08:21:01"
+        "download_time": "27-05-2024 08:36:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -12074,6 +12074,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-05-27",
         "quota_id": "Maison de Chronus (Premises A1) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-51\u2666LX\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-27",
+        "quota_id": "TWG Tea (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-05-27",
