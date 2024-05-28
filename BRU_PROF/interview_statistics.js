7a7c099847e84184cd6e7f6cd297bf68e8cac78b
unknown <lb@silverliningresearch.com>
2024-05-28 07:37:12 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "28-05-2024 09:20:21"
+        "download_time": "28-05-2024 09:35:20"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -63169,6 +63169,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-05-28",
         "quota_id": "SN3139-FLR",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-05-28",
+        "quota_id": "SN359-FIH",
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_MH_KUL",
         "interviewerID": "02",
         "Number of interviews": 7,
-        "download_time": "28-05-2024 15:22:07"
+        "download_time": "28-05-2024 15:36:39"
     },
     {
         "InterviewDate": "01-05-2024",
