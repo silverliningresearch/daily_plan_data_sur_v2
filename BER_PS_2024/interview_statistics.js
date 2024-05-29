2b9cacebd127d16c51bbac91b71283b6bd042400
unknown <lb@silverliningresearch.com>
2024-05-29 11:22:07 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "29-05-2024 13:05:23"
+        "download_time": "29-05-2024 13:20:26"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "29-05-2024 19:05:41"
+        "download_time": "29-05-2024 19:20:49"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -12328,7 +12328,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-29",
         "quota_id": "AVIS BUDGET CAR RENTAL\u2666TERMINAL 3\u2666Landside\u2666#01-K12\u2666LS\u2666Min5",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-05-29",
@@ -12418,7 +12418,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-29",
         "quota_id": "Premises E - Baggage Storage by Smarte Carte\u2666TERMINAL 3\u2666Landside\u2666#01-40\u2666BS\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-05-29",
@@ -12473,7 +12473,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-29",
         "quota_id": "myCarriage\u2666TERMINAL 3\u2666Landside\u2666#01-K13\u2666LS\u2666Min5",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
