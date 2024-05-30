3ec1f5404faf2f1930936a3cf1f4d59f656f675d
unknown <lb@silverliningresearch.com>
2024-05-30 17:07:36 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "30-05-2024 18:50:51"
+        "download_time": "30-05-2024 19:05:59"
     },
     {
         "InterviewDate": "2023-12-31",
@@ -6268,7 +6268,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-05-30",
         "quota_id": "CDG - Air France",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-05-30",
@@ -6294,6 +6294,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-05-30",
         "quota_id": "TIA - Wizz Air Malta",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-05-30",
+        "quota_id": "VIE - Austrian Airlines",
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-05-30",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "30-05-2024 20:51:56"
+        "download_time": "30-05-2024 21:07:02"
     },
     {
         "InterviewDate": "2024-01-07",
