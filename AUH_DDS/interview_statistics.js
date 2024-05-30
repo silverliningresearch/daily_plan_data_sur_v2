db08718c5e2f4764c14c6b86bb6980a840d8e211
unknown <lb@silverliningresearch.com>
2024-05-30 23:37:20 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "31-05-2024 03:22:42"
+        "download_time": "31-05-2024 03:37:18"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "31-05-2024 07:21:28"
+        "download_time": "31-05-2024 07:36:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -12638,6 +12638,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-05-30",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-05-31",
+        "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min8",
         "Number of interviews": 1
     },
     {
