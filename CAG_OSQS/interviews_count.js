e658024ff375b349a6e17497b4a2a0dc50d6ba41
unknown <lb@silverliningresearch.com>
2024-06-01 04:52:24 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "01-06-2024 12:36:12"
+        "download_time": "01-06-2024 12:51:12"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -12678,7 +12678,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-01",
         "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 4\u2666Landside\u2666#01-K3\u2666CR\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-01",
@@ -12718,7 +12718,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-01",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-01",
@@ -12733,7 +12733,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-01",
         "quota_id": "UOB T1 DCI Central\u2666TERMINAL 1\u2666Landside\u2666#02-01\u2666MC\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-01",
