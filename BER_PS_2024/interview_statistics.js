87058f4a327b66bd04b04be489c4e3d383cd8a03
unknown <lb@silverliningresearch.com>
2024-06-06 02:08:22 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "06-06-2024 03:50:15"
+        "download_time": "06-06-2024 04:06:28"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-06-2024 09:50:49"
+        "download_time": "06-06-2024 10:06:57"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13048,7 +13048,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-06",
         "quota_id": "NAM NAM\u2666TERMINAL 3\u2666Landside\u2666#02-91\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     }
 ]    
 
