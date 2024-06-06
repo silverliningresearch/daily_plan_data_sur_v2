b776a7e8a620d8c16fd79f07162a5bd2b39ea9d1
unknown <lb@silverliningresearch.com>
2024-06-06 04:51:49 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "06-06-2024 06:34:52"
+        "download_time": "06-06-2024 06:49:52"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -64643,7 +64643,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-06-06",
         "quota_id": "IB3207-MAD",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-06-06",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-06-2024 12:35:25"
+        "download_time": "06-06-2024 12:50:27"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13048,7 +13048,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-06",
         "quota_id": "4FINGERS CRISPY CHICKEN & MELTISE\u2666TERMINAL 3\u2666Landside\u2666#B2-02/02A\u2666FBL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-06-06",
