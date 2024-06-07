0886568b1803b869023102a7f6bce387e025f07f
unknown <lb@silverliningresearch.com>
2024-06-07 09:51:49 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "07-06-2024 11:35:13"
+        "download_time": "07-06-2024 11:50:07"
     },
     {
         "InterviewDate": "2023-12-31",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-06-2024 17:35:39"
+        "download_time": "07-06-2024 17:50:30"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13098,7 +13098,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-07",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-06-07",
@@ -13143,7 +13143,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-07",
         "quota_id": "Tip Top Curry Puff\u2666TERMINAL 1\u2666Airside\u2666#02-K11N\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     }
 ]    
 
