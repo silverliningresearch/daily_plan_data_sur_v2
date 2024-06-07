63fd2a1af7cf65b167c65aba8396fcac7e8c81cd
unknown <lb@silverliningresearch.com>
2024-06-07 18:52:09 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-31",
         "quota_id": "EWR - United Airlines",
         "Number of interviews": 1,
-        "download_time": "07-06-2024 20:35:07"
+        "download_time": "07-06-2024 20:50:09"
     },
     {
         "InterviewDate": "2023-12-31",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-12-21",
         "quota_id": "FR-HEL",
         "Number of interviews": 1,
-        "download_time": "07-06-2024 20:36:20"
+        "download_time": "07-06-2024 20:51:36"
     },
     {
         "InterviewDate": "2023-12-21",
