1411458de28bd59c589e8bbd3ba041807d058fe6
unknown <lb@silverliningresearch.com>
2024-06-10 23:36:34 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_PR_MNL",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "11-06-2024 07:20:38"
+        "download_time": "11-06-2024 07:35:41"
     },
     {
         "InterviewDate": "03-06-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-06-2024 07:20:01"
+        "download_time": "11-06-2024 07:35:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13384,6 +13384,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "iTEA Premium\u2666TERMINAL 3\u2666Landside\u2666#B2-30\u2666FBL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-06-11",
+        "quota_id": "7-Eleven (T4)\u2666TERMINAL 4\u2666Airside\u2666#02-240\u2666CS\u2666Min8",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-11",
