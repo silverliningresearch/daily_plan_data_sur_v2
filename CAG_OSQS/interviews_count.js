46cd9e2b728cbe5dfd584f18493ebf79b1740f02
unknown <lb@silverliningresearch.com>
2024-06-10 06:21:05 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "10-06-2024 14:06:04"
+        "download_time": "10-06-2024 14:19:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13303,7 +13303,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-195\u2666CCD\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-06-10",
@@ -13313,7 +13313,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-10",
         "quota_id": "Perfumes & Cosmetics (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-194 (#026-116)\u2666PC\u2666Min15",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-06-10",
@@ -13323,7 +13323,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-10",
         "quota_id": "Sprint-Cass (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-186\u2666EL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-06-10",
