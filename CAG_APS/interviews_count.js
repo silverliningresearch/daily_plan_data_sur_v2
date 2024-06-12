160e197cea541bb0131c44fe804a925c6a5edcfa
unknown <lb@silverliningresearch.com>
2024-06-12 05:22:00 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_PR_MNL",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "12-06-2024 13:05:40"
+        "download_time": "12-06-2024 13:21:03"
     },
     {
         "InterviewDate": "03-06-2024",
@@ -1034,9 +1034,27 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "12-06-2024",
+        "quota_id": "T4_AK_PEN",
+        "interviewerID": "02",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "12-06-2024",
+        "quota_id": "T4_AA_PEN",
+        "interviewerID": "03",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "12-06-2024",
         "quota_id": "T4_AK_KUL",
         "interviewerID": "03",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "12-06-2024",
+        "quota_id": "T4_AK_PEN",
+        "interviewerID": "03",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "12-06-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-06-2024 13:05:05"
+        "download_time": "12-06-2024 13:20:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13548,7 +13548,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-12",
         "quota_id": "CHEERS\u2666TERMINAL 4\u2666Landside\u2666#01-09\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-06-12",
@@ -13568,7 +13568,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-12",
         "quota_id": "Travelex T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-K8\u2666MC\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-06-12",
