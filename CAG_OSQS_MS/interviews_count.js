1e532530127f230f822c728349101ca82dde37e8
unknown <lb@silverliningresearch.com>
2024-06-12 03:51:13 +0000

    update new data

--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "12-06-2024 11:34:58"
+        "download_time": "12-06-2024 11:49:58"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -193,6 +193,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-11",
         "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-12",
+        "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#01-13\u2666PCPerfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#01-13\u2666PC",
         "Number of interviews": 1
     }
 ]    
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "12-06-2024 05:35:52"
+        "download_time": "12-06-2024 05:50:55"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -6359,6 +6359,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-06-12",
         "quota_id": "FR-JTR",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-12",
+        "quota_id": "FR-NAP",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-12",
