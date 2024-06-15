00a9d033dd1a6060e577d28a753f3d0512959587
unknown <lb@silverliningresearch.com>
2024-06-15 07:06:52 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "15-06-2024 08:49:56"
+        "download_time": "15-06-2024 09:04:56"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -60468,7 +60468,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-06-15",
         "quota_id": "LIS-SN",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-06-15",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-06-2024 14:50:18"
+        "download_time": "15-06-2024 15:05:17"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -13968,7 +13968,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-15",
         "quota_id": "CTE T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K26\u2666MC\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-06-15",
