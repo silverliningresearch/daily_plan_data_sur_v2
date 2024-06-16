bc74537761a041534f2b29aa87c337787eadd087
unknown <lb@silverliningresearch.com>
2024-06-16 02:36:46 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "16-06-2024 06:21:46"
+        "download_time": "16-06-2024 06:36:45"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -3414,7 +3414,7 @@ let interview_statistics = `[
         "quota_id": "5-5",
         "Survey": 5,
         "Location": 1,
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-16",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-06-2024 10:20:18"
+        "download_time": "16-06-2024 10:35:17"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -14113,7 +14113,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-16",
         "quota_id": "Airport Pharmacy\u2666TERMINAL 4\u2666Airside\u2666#02-55/56\u2666PH\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-16",
@@ -14127,8 +14127,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-06-16",
+        "quota_id": "GUARDIAN HEALTH & BEAUTY\u2666TERMINAL 3\u2666Landside\u2666#B2-24\u2666LS\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-16",
         "quota_id": "Irvin's\u2666TERMINAL 4\u2666Airside\u2666#02-71\u2666CCD\u2666Min8",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-06-16",
+        "quota_id": "LT T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-57\u2666LT\u2666Min15",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-06-16",
@@ -14139,6 +14149,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-16",
         "quota_id": "Mr. Bucket Chocolaterie\u2666TERMINAL 3\u2666Airside\u2666#02-K14\u2666CCD\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-06-16",
+        "quota_id": "SEVEN STOP\u2666TERMINAL 3\u2666Landside\u2666#B2-19\u2666LS\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-06-16",
