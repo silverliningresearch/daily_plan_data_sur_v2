215b5264e3998fdfba26d9c07d46e1a0a90ef26d
unknown <lb@silverliningresearch.com>
2024-06-18 01:36:52 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "18-06-2024 05:21:51"
+        "download_time": "18-06-2024 05:36:50"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -3547,6 +3547,13 @@ let interview_statistics = `[
         "quota_id": "6-6",
         "Survey": 6,
         "Location": 2,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-06-18",
+        "quota_id": "7-7",
+        "Survey": 7,
+        "Location": 8888888,
         "Number of interviews": 2
     }
 ]    
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_PR_MNL",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "18-06-2024 09:21:01"
+        "download_time": "18-06-2024 09:36:00"
     },
     {
         "InterviewDate": "03-06-2024",
@@ -1469,6 +1469,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_KIX",
         "interviewerID": "14",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "18-06-2024",
+        "quota_id": "T4_3K_BKK",
+        "interviewerID": "02",
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "18-06-2024",
