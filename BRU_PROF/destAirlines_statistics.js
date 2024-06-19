b0d0d6bac0058d0d1f2813e39014bdd2d69ca9a6
unknown <lb@silverliningresearch.com>
2024-06-19 16:37:01 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "19-06-2024 18:20:52"
+        "download_time": "19-06-2024 18:35:05"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -61312,6 +61312,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-06-19",
+        "quota_id": "FRA-SN",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-19",
         "quota_id": "GVA-SN",
         "Number of interviews": 3
     },
@@ -61373,7 +61378,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-06-19",
         "quota_id": "RIX-BT",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-19",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "19-06-2024 18:20:52"
+        "download_time": "19-06-2024 18:35:05"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -67278,7 +67278,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-06-19",
         "quota_id": "BT604-RIX",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-19",
@@ -67354,6 +67354,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-06-19",
         "quota_id": "SN2259-CPH",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-06-19",
+        "quota_id": "SN2617-FRA",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-06-19",
