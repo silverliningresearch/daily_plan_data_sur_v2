b9d6bcb52b15b002368e9b5b0d0447be613f6c39
unknown <lb@silverliningresearch.com>
2024-06-21 03:51:44 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "21-06-2024 05:34:54"
+        "download_time": "21-06-2024 05:50:07"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -67709,6 +67709,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-06-20",
         "quota_id": "XQ757-AOE",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-06-21",
+        "quota_id": "TB2251-HER",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-21",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-06-2024 11:35:12"
+        "download_time": "21-06-2024 11:50:28"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -14583,7 +14583,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-21",
         "quota_id": "CRAVE\u2666TERMINAL 2\u2666Airside\u2666#02-K8\u2666FBA\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-06-21",
@@ -14603,7 +14603,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-21",
         "quota_id": "Perfumes & Cosmetics (T1 DTW #02-25)\u2666TERMINAL 1\u2666Airside\u2666#02-25\u2666PC\u2666Min15",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-21",
@@ -14628,12 +14628,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-21",
         "quota_id": "Uniqlo\u2666TERMINAL 1\u2666Airside\u2666#02-23\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-21",
         "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     }
 ]    
 
