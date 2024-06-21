d7264fd4332b86e527f64e27b4f035f7181ead11
unknown <lb@silverliningresearch.com>
2024-06-21 00:51:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-06-2024 08:35:14"
+        "download_time": "21-06-2024 08:50:15"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -14589,6 +14589,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-21",
         "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-06-21",
+        "quota_id": "Travelex T2 DT North (Satellite)\u2666TERMINAL 2\u2666Airside\u2666#02-150A\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "21-06-2024 04:35:57"
+        "download_time": "21-06-2024 04:51:00"
     },
     {
         "InterviewDate": "2024-01-07",
