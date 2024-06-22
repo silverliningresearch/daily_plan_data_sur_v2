5ec5f46419d729e40af931067143677cb6871474
unknown <lb@silverliningresearch.com>
2024-06-22 00:22:24 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_PR_MNL",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "22-06-2024 08:07:20"
+        "download_time": "22-06-2024 08:21:27"
     },
     {
         "InterviewDate": "03-06-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-06-2024 08:06:39"
+        "download_time": "22-06-2024 08:20:40"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -14693,7 +14693,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-22",
         "quota_id": "SATS PPG Blossom Lounge\u2666TERMINAL 4\u2666Airside\u2666#02-225&226\u2666HL\u2666Min20",
-        "Number of interviews": 5
+        "Number of interviews": 9
     }
 ]    
 
