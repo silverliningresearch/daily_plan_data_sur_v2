3b817d8926588a114f44ef165ab97183aa38f47e
unknown <lb@silverliningresearch.com>
2024-06-24 10:52:50 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-06-2024 18:35:49"
+        "download_time": "24-06-2024 18:50:56"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -14923,7 +14923,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-24",
         "quota_id": "Asian Artistry\u2666TERMINAL 2\u2666Airside\u2666#02-153\u2666JW\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-24",
@@ -14968,12 +14968,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-24",
         "quota_id": "Luvenus\u2666TERMINAL 2\u2666Airside\u2666#02-154\u2666JW\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-06-24",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-24",
