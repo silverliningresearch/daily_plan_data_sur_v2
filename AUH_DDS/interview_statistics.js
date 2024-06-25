80475eb03e7de7fcfe004a80b81d4d4abc18c482
unknown <lb@silverliningresearch.com>
2024-06-25 15:06:51 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "25-06-2024 18:52:43"
+        "download_time": "25-06-2024 19:06:48"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -3904,7 +3904,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 21
+        "Number of interviews": 23
     },
     {
         "InterviewDate": "2024-06-25",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "25-06-2024 22:51:03"
+        "download_time": "25-06-2024 23:05:26"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -1608,6 +1608,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-25",
         "quota_id": "Foodies' Clan\u2666TERMINAL 2\u2666Landside\u2666#0610-ASC\u2666FBL",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-25",
+        "quota_id": "GHN4\u2666TERMINAL 3\u2666Airside\u2666GHN4 (B1-4)\u2666FBA",
         "Number of interviews": 1
     },
     {
