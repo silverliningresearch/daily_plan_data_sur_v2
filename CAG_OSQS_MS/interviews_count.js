8cd962de71e8069951ae494251cbea5e13bb47eb
unknown <lb@silverliningresearch.com>
2024-06-25 00:21:51 +0000

    update new data

--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "25-06-2024 08:06:51"
+        "download_time": "25-06-2024 08:20:26"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -1598,6 +1598,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-24",
         "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-25",
+        "quota_id": "Foodies' Clan\u2666TERMINAL 2\u2666Landside\u2666#0610-ASC\u2666FBL",
         "Number of interviews": 1
     },
     {
