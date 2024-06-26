294f8107e126dcf4cb856ed5b5749d0f70dc1a95
unknown <lb@silverliningresearch.com>
2024-06-26 02:08:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-06-2024 09:50:56"
+        "download_time": "26-06-2024 10:07:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -15157,6 +15157,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-06-26",
+        "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-26",
         "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
         "Number of interviews": 10
     },
@@ -15164,6 +15169,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-26",
         "quota_id": "TWG Tea (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-06-26",
+        "quota_id": "The Cocoa Trees (Premises C1) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-27\u2666CCD\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "26-06-2024 03:52:09"
+        "download_time": "26-06-2024 04:08:22"
     },
     {
         "InterviewDate": "2024-05-01",
