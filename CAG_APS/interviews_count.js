4f7a24ceb6839cbddc08f9e6ffac98ec33572244
unknown <lb@silverliningresearch.com>
2024-06-26 14:51:51 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_PR_MNL",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "26-06-2024 22:36:27"
+        "download_time": "26-06-2024 22:51:06"
     },
     {
         "InterviewDate": "03-06-2024",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "26-06-2024 22:35:48"
+        "download_time": "26-06-2024 22:50:28"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -1768,6 +1768,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-26",
         "quota_id": "Heavenly Wang\u2666TERMINAL 1\u2666Airside\u2666#02-K8\u2666FBA",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-26",
+        "quota_id": "I Love Taimei\u2666TERMINAL 1\u2666Airside\u2666#03-53\u2666FBA",
         "Number of interviews": 1
     },
     {
