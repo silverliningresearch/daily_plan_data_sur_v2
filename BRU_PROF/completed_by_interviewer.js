affefa1f1f695e5739a55f73ae62c9a1377346df
unknown <lb@silverliningresearch.com>
2024-06-26 17:37:28 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "26-06-2024 19:20:22"
+        "download_time": "26-06-2024 19:35:15"
     },
     {
         "InterviewerID": 1,
@@ -4943,7 +4943,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 10,
         "Interview_Date": "2024-06-26",
-        "completed_interviews": 35
+        "completed_interviews": 36
     },
     {
         "InterviewerID": 11,
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "26-06-2024 19:20:22"
+        "download_time": "26-06-2024 19:35:15"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -68718,7 +68718,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-06-26",
         "quota_id": "OS356-VIE",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-06-26",
