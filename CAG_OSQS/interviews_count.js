e476d562e77a5e354d98a290259e1877b16c6c24
unknown <lb@silverliningresearch.com>
2024-06-27 09:22:38 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-06-2024 17:06:09"
+        "download_time": "27-06-2024 17:20:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -15367,6 +15367,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-06-27",
+        "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-27",
         "quota_id": "Jewel Changi Airport Attractions Ticketing\u2666TERMINAL 2\u2666Landside\u2666#01-K12\u2666LS\u2666Min8",
         "Number of interviews": 3
     },
@@ -15412,8 +15417,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-06-27",
+        "quota_id": "Marc Jacobs\u2666TERMINAL 2\u2666Airside\u2666#02-227\u2666MP\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-27",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-06-27",
+        "quota_id": "Omega\u2666TERMINAL 2\u2666Airside\u2666#02-233\u2666LX\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-06-27",
@@ -15423,7 +15438,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-06-27",
         "quota_id": "Perfumes & Cosmetics (T2 DTC)\u2666TERMINAL 2\u2666Airside\u2666#02-174 (#026-103E)\u2666PC\u2666Min25",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-06-27",
