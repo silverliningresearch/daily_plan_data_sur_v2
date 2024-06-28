e2ce249bdf4baa7dc512ac930f9224052343e9e5
unknown <lb@silverliningresearch.com>
2024-06-28 15:38:33 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "28-06-2024 19:23:22"
+        "download_time": "28-06-2024 19:38:29"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -4037,14 +4037,14 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 16
+        "Number of interviews": 17
     },
     {
         "InterviewDate": "2024-06-28",
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 17
+        "Number of interviews": 19
     },
     {
         "InterviewDate": "2024-06-28",
--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "28-06-2024 17:20:36"
+        "download_time": "28-06-2024 17:35:36"
     },
     {
         "InterviewerID": 1,
@@ -5838,7 +5838,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 13,
         "Interview_Date": "2024-06-28",
-        "completed_interviews": 36
+        "completed_interviews": 39
     },
     {
         "InterviewerID": 14,
