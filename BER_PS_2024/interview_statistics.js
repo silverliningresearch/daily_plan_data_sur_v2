73daaad1e8b2331212d6f68f50496ac4277b9c54
unknown <lb@silverliningresearch.com>
2024-06-29 08:22:59 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "29-06-2024 10:06:53"
+        "download_time": "29-06-2024 10:20:49"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -7298,7 +7298,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-06-29",
         "quota_id": "STN - Ryanair",
-        "Number of interviews": 3
+        "Number of interviews": 7
     }
 ]    
 
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "29-06-2024 10:06:28"
+        "download_time": "29-06-2024 10:20:29"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -63023,7 +63023,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-06-29",
         "quota_id": "IAD-SN",
-        "Number of interviews": 5
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-06-29",
@@ -63038,7 +63038,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-06-29",
         "quota_id": "MAD-UX",
-        "Number of interviews": 12
+        "Number of interviews": 14
     },
     {
         "InterviewDate": "2024-06-29",
