1229e26ae4f46cc6aabf365ff043611bed9e374d
unknown <lb@silverliningresearch.com>
2024-06-30 00:51:25 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "30-06-2024 04:36:41"
+        "download_time": "30-06-2024 04:51:22"
     },
     {
         "InterviewDate": "2024-04-13",
