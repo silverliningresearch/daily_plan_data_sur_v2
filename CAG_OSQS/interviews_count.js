5ebcd4a64a48d766c1d1ca2d810649c9fe9229ce
unknown <lb@silverliningresearch.com>
2024-06-30 00:36:43 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-06-2024 08:20:04"
+        "download_time": "30-06-2024 08:34:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -15914,6 +15914,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-29",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-06-30",
+        "quota_id": "MR TEH TARIK EXPRESS\u2666TERMINAL 3\u2666Landside\u2666#B2-22\u2666FBL\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
