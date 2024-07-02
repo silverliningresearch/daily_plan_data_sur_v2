9f4d70a52d5b81abb37f19c8b06d1874506f227e
unknown <lb@silverliningresearch.com>
2024-07-02 03:51:40 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-07-2024 11:35:04"
+        "download_time": "02-07-2024 11:50:22"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16023,7 +16023,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-02",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#02-K4\u2666CR\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-07-02",
@@ -16044,6 +16044,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-02",
         "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 2\u2666Landside\u2666#02-10/11 (#026-022/023)\u2666PC\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-02",
+        "quota_id": "T2 UOB Bank\u2666TERMINAL 2\u2666Landside\u2666#B1-52 (#B16-017)\u2666MC\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-02",
