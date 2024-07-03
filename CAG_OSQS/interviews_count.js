0461abf81be383e0a41684b7bd922541225562e0
unknown <lb@silverliningresearch.com>
2024-07-03 10:21:45 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-07-2024 18:06:16"
+        "download_time": "03-07-2024 18:20:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16093,7 +16093,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-03",
         "quota_id": "LT T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-65\u2666LT\u2666Min15",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-07-03",
@@ -16103,7 +16103,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-03",
         "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min15",
-        "Number of interviews": 18
+        "Number of interviews": 19
     },
     {
         "InterviewDate": "2024-07-03",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "03-07-2024 18:06:35"
+        "download_time": "03-07-2024 18:20:24"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -2643,6 +2643,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-03",
         "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-03",
+        "quota_id": "Texas Chicken\u2666TERMINAL 4\u2666Airside\u2666#02-86\u2666FBA",
         "Number of interviews": 1
     }
 ]    
