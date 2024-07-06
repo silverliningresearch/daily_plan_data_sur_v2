2c258cbd1cd5b5253b44fe315052d2c9fce40c54
unknown <lb@silverliningresearch.com>
2024-07-06 10:51:42 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "06-07-2024 12:34:57"
+        "download_time": "06-07-2024 12:49:56"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -70494,6 +70494,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-07-06",
         "quota_id": "SN357-FIH",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "SN3783-LPA",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-06",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-07-2024 18:35:22"
+        "download_time": "06-07-2024 18:50:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16298,7 +16298,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-06",
         "quota_id": "ORCHID CAFETERIA\u2666TERMINAL 1\u2666Landside\u2666#B1-12A\u2666FBL\u2666Min25",
-        "Number of interviews": 6
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-07-06",
