81163f52971fb81cb800c4041b4f1f1bbe45c03c
unknown <lb@silverliningresearch.com>
2024-07-06 05:51:52 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "06-07-2024 07:35:07"
+        "download_time": "06-07-2024 07:50:20"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -7624,6 +7624,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-07-05",
         "quota_id": "RIX - Ryanair",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-07-06",
+        "quota_id": "HEL - Finnair",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-06",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-07-2024 13:35:16"
+        "download_time": "06-07-2024 13:50:30"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16278,7 +16278,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-06",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-06",
