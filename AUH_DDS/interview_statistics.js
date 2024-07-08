dfbe7c2970b45e221b13cc906c5e205c2c24e4bc
unknown <lb@silverliningresearch.com>
2024-07-08 02:09:36 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "08-07-2024 05:51:50"
+        "download_time": "08-07-2024 06:08:36"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -4646,7 +4646,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-07-08",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-07-2024 09:50:30"
+        "download_time": "08-07-2024 10:06:17"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16393,7 +16393,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-08",
         "quota_id": "CTE T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-72\u2666MC\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-08",
@@ -16402,8 +16402,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-08",
-        "quota_id": "UOB T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-71\u2666MC\u2666Min8",
+        "quota_id": "Changi Recommends (T1 Licensed Area A)\u2666TERMINAL 1\u2666Airside\u2666#02-COUNTER (near PAUL)\u2666CR\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-08",
+        "quota_id": "UOB T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-71\u2666MC\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-08",
