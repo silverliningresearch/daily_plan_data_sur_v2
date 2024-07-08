fabcfd1c9966e234ea30ab44c9b22f5e19e1f6b9
unknown <lb@silverliningresearch.com>
2024-07-08 18:36:52 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "08-07-2024 20:20:03"
+        "download_time": "08-07-2024 20:35:03"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -64499,6 +64499,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-07-08",
         "quota_id": "GVA-SN",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-07-08",
+        "quota_id": "HAM-SN",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-08",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "08-07-2024 22:21:09"
+        "download_time": "08-07-2024 22:36:07"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -4118,7 +4118,7 @@ let interview_statistics = `[
         "Flight": "EK801",
         "DestCode": "JED",
         "AirlineCode": "EK",
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-07-08",
