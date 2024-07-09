e14d9c688ad493d685e72971bb01962f25a9ed88
unknown <lb@silverliningresearch.com>
2024-07-09 00:22:00 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "09-07-2024 08:07:06"
+        "download_time": "09-07-2024 08:21:14"
     },
     {
         "InterviewDate": "01-07-2024",
@@ -905,6 +905,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_BKK",
         "interviewerID": "dididik",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "09-07-2024",
+        "quota_id": "T1_TR_BKI",
+        "interviewerID": "02",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "09-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-07-2024 08:06:16"
+        "download_time": "09-07-2024 08:20:30"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16489,6 +16489,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-09",
         "quota_id": "Burberry\u2666TERMINAL 3\u2666Airside\u2666#02-20\u2666LX\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-09",
+        "quota_id": "Furla\u2666TERMINAL 3\u2666Airside\u2666#02-14\u2666MP\u2666Min8",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-07-09",
