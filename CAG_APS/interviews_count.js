e89323410446956ab2ff64ef6b18bcf568ac29fb
unknown <lb@silverliningresearch.com>
2024-07-11 01:21:35 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "11-07-2024 09:06:01"
+        "download_time": "11-07-2024 09:20:58"
     },
     {
         "InterviewDate": "01-07-2024",
@@ -1211,6 +1211,12 @@ let interview_statistics  = `[
         "quota_id": "T1_JL_NRT",
         "interviewerID": "02",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "11-07-2024",
+        "quota_id": "T1_TR_SYD",
+        "interviewerID": "02",
+        "Number of interviews": 5
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "11-07-2024 09:05:18"
+        "download_time": "11-07-2024 09:20:18"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16708,7 +16708,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-11",
         "quota_id": "Noyah La Maison & Zero Degree (Premises A2)\u2666TERMINAL 4\u2666Airside\u2666#02-K26 (near Gate G17)\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-07-11",
