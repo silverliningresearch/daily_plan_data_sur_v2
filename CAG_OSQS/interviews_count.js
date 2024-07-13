8df222746806b0a1c618a895c6e444b5220a4872
unknown <lb@silverliningresearch.com>
2024-07-13 01:36:40 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-07-2024 09:20:37"
+        "download_time": "13-07-2024 09:35:23"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16949,6 +16949,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-13",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 4\u2666Airside\u2666#02-29\u2666CCD\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-13",
+        "quota_id": "The Cocoa Trees (Premises D)\u2666TERMINAL 4\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
