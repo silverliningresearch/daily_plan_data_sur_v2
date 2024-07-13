19d8fb5ce404e0e03b794241c684cf962f778b23
unknown <lb@silverliningresearch.com>
2024-07-13 01:06:37 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "13-07-2024 04:52:02"
+        "download_time": "13-07-2024 05:06:35"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-07-2024 08:50:24"
+        "download_time": "13-07-2024 09:05:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -16927,8 +16927,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-13",
-        "quota_id": "Charles & Keith\u2666TERMINAL 4\u2666Airside\u2666#02-30\u2666MP\u2666Min8",
+        "quota_id": "Bacha Coffee\u2666TERMINAL 4\u2666Airside\u2666#02-31\u2666CCD\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-13",
+        "quota_id": "Charles & Keith\u2666TERMINAL 4\u2666Airside\u2666#02-30\u2666MP\u2666Min8",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-07-13",
