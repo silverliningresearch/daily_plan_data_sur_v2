dabeda2e02964936f665f82dff80e7fabd978dae
unknown <lb@silverliningresearch.com>
2024-07-14 23:06:43 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "15-07-2024 02:51:37"
+        "download_time": "15-07-2024 03:06:40"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -5102,6 +5102,13 @@ let interview_statistics = `[
         "Survey": 7,
         "Location": 8888888,
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-07-15",
+        "quota_id": "1-1",
+        "Survey": 1,
+        "Location": 8888888,
+        "Number of interviews": 1
     }
 ]    
 
