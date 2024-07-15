30cba5d0bff99455c759f1fcd9614c9cbe615692
unknown <lb@silverliningresearch.com>
2024-07-15 02:51:35 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-07-2024 10:35:20"
+        "download_time": "15-07-2024 10:50:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17199,6 +17199,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-15",
         "quota_id": "Premises F - Baggage Storage by Smarte Carte\u2666TERMINAL 3\u2666Airside\u2666#02-68/69\u2666BS\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-15",
+        "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA\u2666Min25",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-15",
+        "quota_id": "UOB T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-K7\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "15-07-2024 04:36:38"
+        "download_time": "15-07-2024 04:51:23"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -7494,6 +7494,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-07-14",
         "quota_id": "TK-IST",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-07-15",
+        "quota_id": "FR-MLA",
+        "Number of interviews": 3
     }
 ]    
 
