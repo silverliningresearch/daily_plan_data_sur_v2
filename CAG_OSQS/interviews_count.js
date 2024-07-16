4e88ebce8ae0d45738bf643f00f783831453c9dc
unknown <lb@silverliningresearch.com>
2024-07-16 14:08:55 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-07-2024 21:51:00"
+        "download_time": "16-07-2024 22:07:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17368,12 +17368,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-16",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-05 (beside iSC counter near lifts)\u2666CR\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-07-16",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-180\u2666CR\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-07-16",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 2\u2666Landside\u2666#01-K15 (near taxi stand)\u2666CR\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-16",
@@ -17404,6 +17409,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-16",
         "quota_id": "Jamie's Deli\u2666TERMINAL 1\u2666Airside\u2666#02-40N\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-07-16",
+        "quota_id": "Jewel Changi Airport Attractions Ticketing\u2666TERMINAL 2\u2666Landside\u2666#01-K12\u2666LS\u2666Min8",
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-07-16",
