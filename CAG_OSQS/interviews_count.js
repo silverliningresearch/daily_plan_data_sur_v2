67011b0fc7ad12ebfb7b4c6f46675b23e6870fdf
unknown <lb@silverliningresearch.com>
2024-07-16 14:37:56 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-07-2024 22:20:58"
+        "download_time": "16-07-2024 22:36:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17383,7 +17383,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-16",
         "quota_id": "CHEERS\u2666TERMINAL 2\u2666Landside\u2666#02-24\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-07-16",
@@ -17418,7 +17418,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-16",
         "quota_id": "Jewel Changi Airport Attractions Ticketing\u2666TERMINAL 2\u2666Landside\u2666#01-K12\u2666LS\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-07-16",
