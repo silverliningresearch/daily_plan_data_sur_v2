8fa51173dd7f402f40d487ffef139f38872a787f
unknown <lb@silverliningresearch.com>
2024-07-17 04:53:04 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "17-07-2024 06:35:19"
+        "download_time": "17-07-2024 06:50:21"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -72693,7 +72693,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-07-17",
         "quota_id": "SN3175-FCO",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-17",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "17-07-2024 12:36:02"
+        "download_time": "17-07-2024 12:51:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17538,7 +17538,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-17",
         "quota_id": "Coffee Cart\u2666TERMINAL 3\u2666Airside\u2666#02-CC (outside A13)\u2666FBA\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-07-17",
@@ -17559,6 +17559,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-17",
         "quota_id": "LT T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-57\u2666LT\u2666Min15",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-07-17",
+        "quota_id": "Old Chang Kee\u2666TERMINAL 3\u2666Airside\u2666#02-K2\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-17",
