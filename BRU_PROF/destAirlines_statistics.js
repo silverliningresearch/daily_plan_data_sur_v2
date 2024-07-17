6604773b2291f66f3d555ac15e56fdb65b639b59
unknown <lb@silverliningresearch.com>
2024-07-17 07:38:06 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "17-07-2024 09:20:18"
+        "download_time": "17-07-2024 09:35:28"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -66217,6 +66217,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-17",
+        "quota_id": "LHR-SN",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-17",
         "quota_id": "LYS-SN",
         "Number of interviews": 6
     },
@@ -66228,7 +66233,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-07-17",
         "quota_id": "OSL-SK",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-07-17",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "17-07-2024 15:21:00"
+        "download_time": "17-07-2024 15:36:24"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17598,7 +17598,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-17",
         "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#02-24\u2666PC\u2666Min25",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-17",
