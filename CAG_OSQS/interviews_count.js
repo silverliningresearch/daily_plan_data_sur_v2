af7ee7bc0842a5f6395405fd8da42b4423a9d183
unknown <lb@silverliningresearch.com>
2024-07-17 03:53:05 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "17-07-2024 11:36:13"
+        "download_time": "17-07-2024 11:51:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17537,6 +17537,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-17",
+        "quota_id": "Coffee Cart\u2666TERMINAL 3\u2666Airside\u2666#02-CC (outside A13)\u2666FBA\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-17",
         "quota_id": "Hudsons Cafe\u2666TERMINAL 3\u2666Airside\u2666#02-60\u2666FBA\u2666Min8",
         "Number of interviews": 1
     },
@@ -17553,7 +17558,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-17",
         "quota_id": "LT T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-57\u2666LT\u2666Min15",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-07-17",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "17-07-2024 11:36:18"
+        "download_time": "17-07-2024 11:51:31"
     },
     {
         "InterviewDate": "2024-06-10",
@@ -4894,6 +4894,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-16",
         "quota_id": "WH Smith - Premises D (Air) (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#026-055-02A (#02-240)\u2666OW",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-07-17",
+        "quota_id": "Liquor & Tobacco (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-197\u2666LT",
+        "Number of interviews": 1
     }
 ]    
 
