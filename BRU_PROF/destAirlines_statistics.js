6fc5df3fdbe126ea34299b025ebee38bc302432f
unknown <lb@silverliningresearch.com>
2024-07-18 04:22:53 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "18-07-2024 06:05:52"
+        "download_time": "18-07-2024 06:20:24"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -66508,7 +66508,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-07-18",
         "quota_id": "PMI-TB",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "18-07-2024 12:07:00"
+        "download_time": "18-07-2024 12:21:12"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17637,6 +17637,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-18",
+        "quota_id": "HOKKAIDO BAKED CHEESE TART\u2666TERMINAL 3\u2666Landside\u2666#B2-11\u2666FBL\u2666Min8",
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-07-18",
         "quota_id": "JOLLIBEAN\u2666TERMINAL 3\u2666Landside\u2666#B2-13\u2666FBL\u2666Min8",
         "Number of interviews": 6
     },
@@ -17668,7 +17673,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-18",
         "quota_id": "myEureka\u2666TERMINAL 2\u2666Airside\u2666#02-181\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
