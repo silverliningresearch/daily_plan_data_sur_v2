57c4e704a2eaea2aabf3c2755b8f497b7c415b17
unknown <lb@silverliningresearch.com>
2024-07-20 23:38:05 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-07-2024 07:20:39"
+        "download_time": "21-07-2024 07:36:34"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17972,8 +17972,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-21",
-        "quota_id": "Smarte Carte Premises D (T2 Airside)\u2666TERMINAL 2\u2666Airside\u2666#02-242\u2666BS\u2666Min8",
+        "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-21",
+        "quota_id": "Smarte Carte Premises D (T2 Airside)\u2666TERMINAL 2\u2666Airside\u2666#02-242\u2666BS\u2666Min8",
+        "Number of interviews": 6
     }
 ]    
 
