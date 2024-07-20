472c5a7cd8954ac39e33d720d80be6438f7ece40
unknown <lb@silverliningresearch.com>
2024-07-20 02:37:04 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "20-07-2024 10:20:41"
+        "download_time": "20-07-2024 10:35:43"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -17859,6 +17859,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-20",
         "quota_id": "Changi Recommends\u2666TERMINAL 2\u2666Airside\u2666#02-K18\u2666CR\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-20",
+        "quota_id": "Ippudo Express\u2666TERMINAL 2\u2666Airside\u2666#02-K10\u2666FBA\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-20",
