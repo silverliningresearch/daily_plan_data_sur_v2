75f8d01b3532ac33ed32278f1d5f186764ad7439
unknown <lb@silverliningresearch.com>
2024-07-20 14:37:33 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "20-07-2024 18:21:58"
+        "download_time": "20-07-2024 18:37:31"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -5409,7 +5409,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-07-20",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "20-07-2024 16:21:45"
+        "download_time": "20-07-2024 16:37:17"
     },
     {
         "InterviewDate": "2024-01-03",
