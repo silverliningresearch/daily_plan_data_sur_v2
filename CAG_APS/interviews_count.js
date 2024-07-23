c52a5b3677dcee25acf37d229d2de321de829ae6
unknown <lb@silverliningresearch.com>
2024-07-23 00:08:57 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "23-07-2024 07:52:00"
+        "download_time": "23-07-2024 08:07:58"
     },
     {
         "InterviewDate": "01-07-2024",
@@ -1916,33 +1916,15 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "22-07-2024",
-        "quota_id": "T1_BA_LHA",
-        "interviewerID": "12",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "22-07-2024",
         "quota_id": "T1_BA_LHR",
         "interviewerID": "12",
-        "Number of interviews": 5
-    },
-    {
-        "InterviewDate": "22-07-2024",
-        "quota_id": "T1_BA_LHR ",
-        "interviewerID": "12",
-        "Number of interviews": 1
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "22-07-2024",
         "quota_id": "T1_QF_SYD",
         "interviewerID": "12",
-        "Number of interviews": 3
-    },
-    {
-        "InterviewDate": "22-07-2024",
-        "quota_id": "T1_QF_SYD ",
-        "interviewerID": "12",
-        "Number of interviews": 1
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "22-07-2024",
@@ -1954,25 +1936,13 @@ let interview_statistics  = `[
         "InterviewDate": "22-07-2024",
         "quota_id": "T1_TR_KCH",
         "interviewerID": "12",
-        "Number of interviews": 2
-    },
-    {
-        "InterviewDate": "22-07-2024",
-        "quota_id": "T1_TR_KCH ",
-        "interviewerID": "12",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "22-07-2024",
-        "quota_id": "T1_TR_TRG",
-        "interviewerID": "12",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "22-07-2024",
         "quota_id": "T1_TR_TRZ",
         "interviewerID": "12",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "22-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-07-2024 07:51:17"
+        "download_time": "23-07-2024 08:07:10"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18148,7 +18148,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-23",
         "quota_id": "CHEERS\u2666TERMINAL 4\u2666Landside\u2666#01-09\u2666LS\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
     }
 ]    
 
