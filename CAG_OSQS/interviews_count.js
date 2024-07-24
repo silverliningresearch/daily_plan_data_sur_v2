7166375e88b06bb4572b4e1707d7c83455bc8f65
unknown <lb@silverliningresearch.com>
2024-07-24 05:53:06 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-07-2024 13:36:49"
+        "download_time": "24-07-2024 13:51:23"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18362,6 +18362,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-24",
+        "quota_id": "Burberry\u2666TERMINAL 2\u2666Airside\u2666#02-214\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-24",
         "quota_id": "Bvlgari\u2666TERMINAL 2\u2666Airside\u2666#02-221\u2666LX\u2666Min8",
         "Number of interviews": 1
     },
@@ -18388,12 +18393,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-24",
         "quota_id": "Hermes\u2666TERMINAL 2\u2666Airside\u2666#02-211\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-24",
         "quota_id": "LT T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-68\u2666LT\u2666Min15",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-24",
@@ -18408,6 +18413,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-24",
         "quota_id": "Oakley\u2666TERMINAL 2\u2666Airside\u2666#02-244\u2666MP\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-24",
+        "quota_id": "Pazzion\u2666TERMINAL 2\u2666Airside\u2666#02-201\u2666MP\u2666Min8",
         "Number of interviews": 1
     },
     {
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "24-07-2024 07:38:05"
+        "download_time": "24-07-2024 07:52:39"
     },
     {
         "InterviewDate": "2024-01-03",
