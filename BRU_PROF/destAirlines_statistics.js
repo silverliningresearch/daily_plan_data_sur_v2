2f94fa95ffb1cd927c26e215df6abb895c1a693c
unknown <lb@silverliningresearch.com>
2024-07-25 15:53:25 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "25-07-2024 17:35:36"
+        "download_time": "25-07-2024 17:50:40"
     },
     {
         "InterviewDate": "2023-04-01",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "25-07-2024 17:37:55"
+        "download_time": "25-07-2024 17:53:00"
     },
     {
         "InterviewDate": "2024-01-03",
