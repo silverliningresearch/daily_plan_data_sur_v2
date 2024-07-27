daa209ac8e12ee0ec46e72037e73b0c0eb00363f
unknown <lb@silverliningresearch.com>
2024-07-27 02:36:57 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-07-2024 10:20:23"
+        "download_time": "27-07-2024 10:35:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18844,6 +18844,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-26",
         "quota_id": "The Green Party\u2666TERMINAL 3\u2666Landside\u2666#B2-35/36\u2666LS\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-07-27",
+        "quota_id": "Guardian (Premises B) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-29\u2666PH\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-27",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "27-07-2024 06:21:17"
+        "download_time": "27-07-2024 06:36:12"
     },
     {
         "InterviewDate": "2024-01-20",
