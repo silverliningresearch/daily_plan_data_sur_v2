0b9e32cc999f67ddea57d65a95644036c39cce5b
unknown <lb@silverliningresearch.com>
2024-07-27 07:06:54 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "27-07-2024 08:50:00"
+        "download_time": "27-07-2024 09:04:58"
     },
     {
         "InterviewerID": 1,
@@ -6283,7 +6283,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 13,
         "Interview_Date": "2024-07-27",
-        "completed_interviews": 3
+        "completed_interviews": 8
     },
     {
         "InterviewerID": 14,
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-07-2024 14:50:40"
+        "download_time": "27-07-2024 15:05:19"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18888,7 +18888,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-27",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-07-27",
@@ -18934,6 +18934,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-27",
         "quota_id": "Uniqlo\u2666TERMINAL 1\u2666Airside\u2666#02-23\u2666MP\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-07-27",
+        "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
