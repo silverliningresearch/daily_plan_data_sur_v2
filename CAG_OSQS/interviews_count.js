f00b123865f22c4b3f8eca7d7c4c7dab5bab1afe
unknown <lb@silverliningresearch.com>
2024-07-27 04:08:46 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-07-2024 11:50:32"
+        "download_time": "27-07-2024 12:05:53"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18853,7 +18853,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-27",
         "quota_id": "Hugo Boss\u2666TERMINAL 3\u2666Airside\u2666#02-23\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-27",
@@ -18863,7 +18863,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-27",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-27",
@@ -18873,7 +18873,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-27",
         "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-07-27",
