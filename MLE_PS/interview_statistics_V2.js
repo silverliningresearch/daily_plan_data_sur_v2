29cbec11303c124cced5bc7daa94343be0330d23
unknown <lb@silverliningresearch.com>
2024-07-27 02:08:26 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "27-07-2024 05:51:27"
+        "download_time": "27-07-2024 06:07:25"
     },
     {
         "InterviewDate": "2024-01-20",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "27-07-2024 03:52:03"
+        "download_time": "27-07-2024 04:08:13"
     },
     {
         "InterviewDate": "2024-01-28",
