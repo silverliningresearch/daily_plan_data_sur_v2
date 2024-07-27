b5196283deb68a6662379a3b8da14017436a1299
unknown <lb@silverliningresearch.com>
2024-07-27 04:21:57 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-07-2024 12:05:53"
+        "download_time": "27-07-2024 12:20:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18847,6 +18847,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-27",
+        "quota_id": "Changi Recommends (T3 Licensed Area C)\u2666TERMINAL 3\u2666Airside\u2666#02-COUNTER C (near Skytrain station to A15-21)\u2666CR\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-27",
         "quota_id": "Guardian (Premises B) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-29\u2666PH\u2666Min8",
         "Number of interviews": 8
     },
@@ -18862,6 +18867,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-27",
+        "quota_id": "LT T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-22\u2666LT\u2666Min15",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-27",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
         "Number of interviews": 6
     },
@@ -18874,6 +18884,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-27",
         "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-27",
+        "quota_id": "Perfumes & Cosmetics (T1 DTW #02-25)\u2666TERMINAL 1\u2666Airside\u2666#02-25\u2666PC\u2666Min15",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-27",
