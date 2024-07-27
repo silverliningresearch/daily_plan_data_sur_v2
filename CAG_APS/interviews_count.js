a83289a678412cef89c8e272e16db2e9a7243813
unknown <lb@silverliningresearch.com>
2024-07-27 00:21:59 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "27-07-2024 08:07:17"
+        "download_time": "27-07-2024 08:21:07"
     },
     {
         "InterviewDate": "01-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-07-2024 08:05:51"
+        "download_time": "27-07-2024 08:20:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18847,8 +18847,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-07-27",
+        "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-07-27",
         "quota_id": "TWG Tea Company (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-07-27",
