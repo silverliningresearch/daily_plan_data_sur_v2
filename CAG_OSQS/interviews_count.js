f295831d2650d217e9fa0ebd416d5ed77e17b4dd
unknown <lb@silverliningresearch.com>
2024-07-28 04:08:47 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "28-07-2024 11:50:38"
+        "download_time": "28-07-2024 12:06:48"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -18988,12 +18988,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-28",
         "quota_id": "Guardian (Premises B) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-16\u2666PH\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-07-28",
+        "quota_id": "Kaboom\u2666TERMINAL 1\u2666Airside\u2666#02-18N\u2666CT\u2666Min8",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-28",
         "quota_id": "LT T3 Arrival South\u2666TERMINAL 3\u2666Airside\u2666#01-19\u2666LT\u2666Min15",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-07-28",
@@ -19009,6 +19014,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-28",
         "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
         "Number of interviews": 16
+    },
+    {
+        "InterviewDate": "2024-07-28",
+        "quota_id": "T3B2 CSO Counter\u2666TERMINAL 3\u2666Landside\u2666NA\u2666PS\u2666Min25",
+        "Number of interviews": 2
     }
 ]    
 
