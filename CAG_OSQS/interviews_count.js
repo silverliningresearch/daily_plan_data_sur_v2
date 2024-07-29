7cd0a0c6de4dcf0863e9e55111f9c10d2e51e981
unknown <lb@silverliningresearch.com>
2024-07-29 00:22:26 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "29-07-2024 08:06:28"
+        "download_time": "29-07-2024 08:20:41"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19118,6 +19118,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-29",
         "quota_id": "LT T2 Duplex\u2666TERMINAL 2\u2666Airside\u2666#02-167 & #03-189\u2666LT\u2666Min25",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-29",
+        "quota_id": "Straits Food Village - Premises A\u2666TERMINAL 2\u2666Airside\u2666#03-179/180/181\u2666FBA\u2666Min25",
         "Number of interviews": 3
     }
 ]    
