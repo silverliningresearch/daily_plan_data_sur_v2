50318f7a42c2606b2db53a65b3194653a0d93c14
unknown <lb@silverliningresearch.com>
2024-07-29 02:52:30 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "29-07-2024 10:35:50"
+        "download_time": "29-07-2024 10:50:52"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19134,6 +19134,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-29",
         "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
         "Number of interviews": 13
+    },
+    {
+        "InterviewDate": "2024-07-29",
+        "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-11R\u2666FBA\u2666Min25",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-07-29",
