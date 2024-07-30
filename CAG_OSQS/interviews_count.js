b8c87a51afd02920cada2eba5f37fd442b8c6a9f
unknown <lb@silverliningresearch.com>
2024-07-30 01:52:25 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-07-2024 09:35:33"
+        "download_time": "30-07-2024 09:50:45"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19219,6 +19219,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-30",
         "quota_id": "Heavenly Wang\u2666TERMINAL 3\u2666Airside\u2666#02-K1\u2666FBA\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-30",
+        "quota_id": "Kaboom\u2666TERMINAL 3\u2666Airside\u2666#02-57A\u2666CT\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-30",
