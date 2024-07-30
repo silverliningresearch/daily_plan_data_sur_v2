990894ec636927414576409b244845bab00c7c27
unknown <lb@silverliningresearch.com>
2024-07-30 10:37:14 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "30-07-2024 12:20:08"
+        "download_time": "30-07-2024 12:35:07"
     },
     {
         "InterviewerID": 1,
@@ -4678,7 +4678,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 9,
         "Interview_Date": "2024-07-30",
-        "completed_interviews": 62
+        "completed_interviews": 65
     },
     {
         "InterviewerID": 10,
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "30-07-2024 12:20:08"
+        "download_time": "30-07-2024 12:35:06"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -75369,6 +75369,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-07-30",
         "quota_id": "TU955-TUN",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-07-30",
+        "quota_id": "UA619-EWR",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-30",
