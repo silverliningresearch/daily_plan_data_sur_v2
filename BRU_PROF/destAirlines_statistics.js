c7fd6f6762fdc6a3e5af228ef5f12c4c9b7c67a5
unknown <lb@silverliningresearch.com>
2024-07-30 02:23:38 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "30-07-2024 04:05:23"
+        "download_time": "30-07-2024 04:20:42"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-07-2024 10:06:11"
+        "download_time": "30-07-2024 10:21:23"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19199,6 +19199,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-30",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-30",
+        "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-30",
