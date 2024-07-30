f2532bf9ef096091f1f709b80165dbe2ec295ca0
unknown <lb@silverliningresearch.com>
2024-07-30 02:37:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-07-2024 10:21:23"
+        "download_time": "30-07-2024 10:35:31"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19203,7 +19203,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-30",
         "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-07-30",
@@ -19234,6 +19234,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-07-30",
         "quota_id": "Old Chang Kee\u2666TERMINAL 3\u2666Airside\u2666#02-K2\u2666FBA\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-30",
+        "quota_id": "Pontian Wanton Noodle\u2666TERMINAL 1\u2666Airside\u2666#02-K10N\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-07-30",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "30-07-2024 04:23:15"
+        "download_time": "30-07-2024 04:36:54"
     },
     {
         "InterviewDate": "2024-01-03",
