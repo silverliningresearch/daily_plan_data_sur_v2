c869b80644ab5133aa532b30b5c8bcc1f8a7e0b9
unknown <lb@silverliningresearch.com>
2024-07-31 01:37:30 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "31-07-2024 09:20:35"
+        "download_time": "31-07-2024 09:35:53"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19268,6 +19268,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-31",
         "quota_id": "Guardian (Premises A) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-66\u2666PH\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-07-31",
+        "quota_id": "TWG Tea (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
         "Number of interviews": 2
     }
 ]    
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "31-07-2024 03:21:59"
+        "download_time": "31-07-2024 03:37:15"
     },
     {
         "InterviewDate": "2024-05-01",
