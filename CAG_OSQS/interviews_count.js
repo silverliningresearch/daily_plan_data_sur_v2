796fc1d56f7188d1e3f61dfbd06cf4899f5299e0
unknown <lb@silverliningresearch.com>
2024-07-31 01:22:14 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "31-07-2024 09:05:40"
+        "download_time": "31-07-2024 09:20:35"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19263,6 +19263,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-07-30",
         "quota_id": "Tip Top Curry Puff\u2666TERMINAL 1\u2666Airside\u2666#02-K11N\u2666FBA\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-07-31",
+        "quota_id": "Guardian (Premises A) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-66\u2666PH\u2666Min8",
         "Number of interviews": 2
     }
 ]    
