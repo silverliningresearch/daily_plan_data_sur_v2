caa04f2486968fa6062397c80c029c4e77ea7474
unknown <lb@silverliningresearch.com>
2024-07-31 10:52:38 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "31-07-2024 12:35:40"
+        "download_time": "31-07-2024 12:50:32"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -8613,7 +8613,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-07-31",
         "quota_id": "PUY - easyJet Europe",
-        "Number of interviews": 1
+        "Number of interviews": 5
     }
 ]    
 
