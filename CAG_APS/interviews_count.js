3df7a101867a29097c0499a02b4c1a7bf2e03876
unknown <lb@silverliningresearch.com>
2024-08-01 10:23:54 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "01-08-2024 18:07:53"
+        "download_time": "01-08-2024 18:21:51"
     },
     {
         "InterviewDate": "01-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "01-08-2024 18:07:02"
+        "download_time": "01-08-2024 18:21:08"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19318,7 +19318,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-01",
         "quota_id": "Marc Jacobs\u2666TERMINAL 1\u2666Airside\u2666#02-58\u2666MP\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-08-01",
