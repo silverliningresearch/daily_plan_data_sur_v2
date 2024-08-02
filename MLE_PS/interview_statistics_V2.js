95e9efb44b2550d135e88b135a789a38a94a49d9
unknown <lb@silverliningresearch.com>
2024-08-02 20:22:20 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "03-08-2024 00:07:35"
+        "download_time": "03-08-2024 00:21:25"
     },
     {
         "InterviewDate": "2024-01-20",
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "02-08-2024 22:08:17"
+        "download_time": "02-08-2024 22:21:58"
     },
     {
         "InterviewDate": "2024-05-01",
