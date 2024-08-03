9c0149de8143aacb289f9b70d53de28eb5607428
unknown <lb@silverliningresearch.com>
2024-08-03 03:53:15 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "03-08-2024 07:37:31"
+        "download_time": "03-08-2024 07:53:10"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -5990,7 +5990,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-08-03",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-08-2024 11:35:50"
+        "download_time": "03-08-2024 11:51:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19417,6 +19417,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-08-03",
+        "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 4\u2666Landside\u2666#01-K3\u2666CR\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-08-03",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#02-K7\u2666CR\u2666Min8",
         "Number of interviews": 1
     },
@@ -19428,7 +19433,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-03",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-08-03",
