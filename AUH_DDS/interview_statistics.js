e236970fe926c3d433300980aa38ba63fe762f02
unknown <lb@silverliningresearch.com>
2024-08-05 03:37:27 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "05-08-2024 07:22:25"
+        "download_time": "05-08-2024 07:37:24"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -6144,7 +6144,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 2,
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "05-08-2024 11:20:47"
+        "download_time": "05-08-2024 11:35:49"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19553,7 +19553,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-05",
         "quota_id": "T3 iSC Departure counter\u2666TERMINAL 3\u2666Airside\u2666NA\u2666PS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 6
     }
 ]    
 
