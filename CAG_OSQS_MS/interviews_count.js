fe369d730425eacb07abafa72f654eba6dc12764
unknown <lb@silverliningresearch.com>
2024-08-05 23:37:46 +0000

    update new data

--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "06-08-2024 07:20:30"
+        "download_time": "06-08-2024 07:36:24"
     },
     {
         "InterviewDate": "2024-06-10",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "06-08-2024 03:21:17"
+        "download_time": "06-08-2024 03:37:07"
     },
     {
         "InterviewDate": "2024-01-20",
