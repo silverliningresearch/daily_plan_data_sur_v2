9c79fc196fde3dea5134f2112d0cf0aaeeb5cc18
unknown <lb@silverliningresearch.com>
2024-08-07 03:38:05 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "07-08-2024 05:20:39"
+        "download_time": "07-08-2024 05:35:42"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "07-08-2024 11:21:21"
+        "download_time": "07-08-2024 11:36:16"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19639,6 +19639,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-06",
         "quota_id": "Yakun Kaya Toast\u2666TERMINAL 2\u2666Airside\u2666#02-803\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-08-07",
+        "quota_id": "Decathlon\u2666TERMINAL 3\u2666Airside\u2666#Event Space EA(6)\u2666MP\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
