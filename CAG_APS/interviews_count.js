aa88a45382c27dd91423ffdec6692f0edadded34
unknown <lb@silverliningresearch.com>
2024-08-07 21:21:57 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "08-08-2024 05:06:14"
+        "download_time": "08-08-2024 05:21:13"
     },
     {
         "InterviewDate": "01-07-2024",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "08-08-2024 05:05:37"
+        "download_time": "08-08-2024 05:20:35"
     },
     {
         "InterviewDate": "2024-06-10",
