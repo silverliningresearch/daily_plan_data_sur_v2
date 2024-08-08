e52bbeae7f86f2fc4a4a2f5be4310fe621039c95
unknown <lb@silverliningresearch.com>
2024-08-08 00:52:59 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-08-2024 08:36:07"
+        "download_time": "08-08-2024 08:51:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19758,7 +19758,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-08",
         "quota_id": "Noyah La Maison & Zero Degree (Premises A2)\u2666TERMINAL 4\u2666Airside\u2666#02-K26 (near Gate G17)\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-08-08",
