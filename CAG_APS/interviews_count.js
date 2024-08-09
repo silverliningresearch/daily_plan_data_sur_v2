aeb76e44359eb83bc4be08300ea9fc786ba7f2f7
unknown <lb@silverliningresearch.com>
2024-08-09 23:52:49 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "10-08-2024 07:37:50"
+        "download_time": "10-08-2024 07:51:48"
     },
     {
         "InterviewDate": "01-07-2024",
@@ -1780,7 +1780,7 @@ let interview_statistics  = `[
         "InterviewDate": "10-08-2024",
         "quota_id": "T2_SQ_KUL",
         "interviewerID": "02",
-        "Number of interviews": 3
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "10-08-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "10-08-2024 07:36:46"
+        "download_time": "10-08-2024 07:51:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19973,7 +19973,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-10",
         "quota_id": "iStudio\u2666TERMINAL 2\u2666Airside\u2666#02-238\u2666EL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 5
     }
 ]    
 
