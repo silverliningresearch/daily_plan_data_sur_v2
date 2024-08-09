b9c6c3d2d13ead5bb5c29299c900a846f3bc8dca
unknown <lb@silverliningresearch.com>
2024-08-09 03:22:36 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "09-08-2024 07:07:40"
+        "download_time": "09-08-2024 07:22:32"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -6452,6 +6452,13 @@ let interview_statistics = `[
         "quota_id": "5-5",
         "Survey": 5,
         "Location": 2,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-09",
+        "quota_id": "6-6",
+        "Survey": 6,
+        "Location": 1,
         "Number of interviews": 1
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "09-08-2024 11:06:01"
+        "download_time": "09-08-2024 11:20:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -19862,7 +19862,22 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-08-09",
+        "quota_id": "Braun Buffel\u2666TERMINAL 2\u2666Airside\u2666#02-245\u2666MP\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-08-09",
         "quota_id": "Charles & Keith\u2666TERMINAL 2\u2666Airside\u2666#02-234 (#026-057)\u2666MP\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-09",
+        "quota_id": "Eu Yan Sang\u2666TERMINAL 2\u2666Airside\u2666#02-246\u2666PH\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-09",
+        "quota_id": "Oakley\u2666TERMINAL 2\u2666Airside\u2666#02-244\u2666MP\u2666Min8",
         "Number of interviews": 1
     },
     {
