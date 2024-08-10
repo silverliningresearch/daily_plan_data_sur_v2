b6303bf186f3d25e991f08cc1c93a2cf6fc666a7
unknown <lb@silverliningresearch.com>
2024-08-10 16:23:04 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "10-08-2024 18:06:09"
+        "download_time": "10-08-2024 18:20:30"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -70618,7 +70618,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-08-10",
         "quota_id": "HEL-AY",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-08-10",
@@ -70639,6 +70639,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-08-10",
         "quota_id": "IST-TK",
         "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2024-08-10",
+        "quota_id": "KGL-WB",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-08-10",
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "10-08-2024 18:06:09"
+        "download_time": "10-08-2024 18:20:30"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -77522,6 +77522,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-08-10",
+        "quota_id": "AY1546-HEL",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-08-10",
         "quota_id": "AZ149-LIN",
         "Number of interviews": 3
     },
@@ -77739,6 +77744,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-08-10",
         "quota_id": "VY8991-SVQ",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-08-10",
+        "quota_id": "WB701-KGL",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-08-10",
