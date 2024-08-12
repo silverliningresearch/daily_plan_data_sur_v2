3baa5326434e3ac8f0c176da4fcfe0ab9e676dd6
unknown <lb@silverliningresearch.com>
2024-08-12 00:23:04 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "12-08-2024 08:07:33"
+        "download_time": "12-08-2024 08:21:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -20318,7 +20318,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-12",
         "quota_id": "Premises F - Baggage Storage by Smarte Carte\u2666TERMINAL 3\u2666Airside\u2666#02-68/69\u2666BS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-08-12",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "12-08-2024 04:08:22"
+        "download_time": "12-08-2024 04:21:59"
     },
     {
         "InterviewDate": "2024-01-07",
