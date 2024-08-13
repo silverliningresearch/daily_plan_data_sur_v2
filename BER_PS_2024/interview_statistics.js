aef2bd2460e798201428e8a95003b74186da527a
unknown <lb@silverliningresearch.com>
2024-08-13 04:54:04 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "13-08-2024 06:36:02"
+        "download_time": "13-08-2024 06:51:51"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -9094,6 +9094,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-08-12",
         "quota_id": "ZRH - Eurowings",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-08-13",
+        "quota_id": "ZRH - Eurowings",
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-08-2024 12:36:22"
+        "download_time": "13-08-2024 12:52:12"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -20443,7 +20443,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-13",
         "quota_id": "Guardian (Premises A) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-66\u2666PH\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-08-13",
@@ -20463,7 +20463,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-13",
         "quota_id": "Louis Vuitton\u2666TERMINAL 1\u2666Airside\u2666#02-50\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-08-13",
@@ -20478,12 +20478,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-13",
         "quota_id": "Perfumes & Cosmetics (T1 DTW #02-43N)\u2666TERMINAL 1\u2666Airside\u2666#02-43N\u2666PC\u2666Min15",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-08-13",
         "quota_id": "Prosegur T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-K9\u2666MC\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-08-13",
