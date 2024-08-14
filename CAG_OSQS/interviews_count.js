d69b1ff9e49be55efd46e126ac31f31315c4136e
unknown <lb@silverliningresearch.com>
2024-08-14 00:38:30 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "14-08-2024 08:21:25"
+        "download_time": "14-08-2024 08:36:35"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -20579,6 +20579,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-14",
         "quota_id": "Prosegur T1 DT East\u2666TERMINAL 1\u2666Airside\u2666#02-72\u2666MC\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-14",
+        "quota_id": "Prosegur T1 DT West\u2666TERMINAL 1\u2666Airside\u2666#02-21\u2666MC\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-08-14",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "14-08-2024 02:22:59"
+        "download_time": "14-08-2024 02:38:13"
     },
     {
         "InterviewDate": "2024-01-28",
