2b83b7f411d811b1b07618651876d30e57a6dc76
unknown <lb@silverliningresearch.com>
2024-08-14 17:23:24 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "14-08-2024 19:06:09"
+        "download_time": "14-08-2024 19:21:09"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -9242,6 +9242,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-08-14",
+        "quota_id": "MAD - IBERIA Express",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-14",
         "quota_id": "MAN - Ryanair",
         "Number of interviews": 11
     },
@@ -9288,7 +9293,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-08-14",
         "quota_id": "WAW - LOT Polish Airlines",
-        "Number of interviews": 8
+        "Number of interviews": 9
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "14-08-2024 21:07:25"
+        "download_time": "14-08-2024 21:22:17"
     },
     {
         "InterviewDate": "2024-01-07",
