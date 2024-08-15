3cd6955286117c6547d5ee69fc125baa704c5ef4
unknown <lb@silverliningresearch.com>
2024-08-15 22:53:29 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "16-08-2024 00:36:16"
+        "download_time": "16-08-2024 00:51:14"
     },
     {
         "InterviewDate": "2024-01-03",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "16-08-2024 06:36:38"
+        "download_time": "16-08-2024 06:51:36"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -20819,6 +20819,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-15",
         "quota_id": "acme de la vie\u2666TERMINAL 4\u2666Airside\u2666#02-45\u2666MP\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-08-16",
+        "quota_id": "7-Eleven (T1)\u2666TERMINAL 1\u2666Airside\u2666#02-75\u2666CS\u2666Min8",
+        "Number of interviews": 4
     }
 ]    
 
