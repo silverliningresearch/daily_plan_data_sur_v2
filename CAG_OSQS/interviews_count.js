9440700c9848007c1cedc55256efe593777d2785
unknown <lb@silverliningresearch.com>
2024-08-15 00:23:29 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-08-2024 08:07:51"
+        "download_time": "15-08-2024 08:21:32"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -20679,6 +20679,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-14",
         "quota_id": "UOB T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-37\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-08-15",
+        "quota_id": "Garrett Popcorn Shops\u2666TERMINAL 4\u2666Airside\u2666#02-42\u2666CCD\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "15-08-2024 04:09:01"
+        "download_time": "15-08-2024 04:22:22"
     },
     {
         "InterviewDate": "2024-01-07",
