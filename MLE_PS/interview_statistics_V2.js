925f8e98b69146f7fc298158c9aceb24f6b0d402
unknown <lb@silverliningresearch.com>
2024-08-17 13:08:19 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "17-08-2024 16:52:33"
+        "download_time": "17-08-2024 17:07:20"
     },
     {
         "InterviewDate": "2024-01-20",
