ed0aea5cb5861694fbbabc92ac8476920c823fed
unknown <lb@silverliningresearch.com>
2024-08-18 21:53:52 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "19-08-2024 01:37:18"
+        "download_time": "19-08-2024 01:52:50"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -4814,7 +4814,7 @@ let interview_statistics = `[
         "Flight": "BS342",
         "DestCode": "DAC",
         "AirlineCode": "BS",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-08-19",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "BA-LHR",
         "InterviewerID": "7227736",
         "Number of interviews": 2,
-        "download_time": "19-08-2024 01:37:23"
+        "download_time": "19-08-2024 01:52:57"
     },
     {
         "InterviewDate": "2024-01-20",
