082b51ba7076942efc0efda1750fcc9ace4b92f7
unknown <lb@silverliningresearch.com>
2024-08-22 00:23:31 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "22-08-2024 02:06:59"
+        "download_time": "22-08-2024 02:20:55"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-08-2024 08:10:25"
+        "download_time": "22-08-2024 08:21:43"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -21444,6 +21444,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-22",
         "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 2\u2666Airside\u2666#03-175/176\u2666HL\u2666Min20",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-08-22",
+        "quota_id": "UOB T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209B\u2666MC\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
