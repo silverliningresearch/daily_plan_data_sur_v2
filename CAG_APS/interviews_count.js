179a47f874ebcca3bddd93e38d37f6db5816bd91
unknown <lb@silverliningresearch.com>
2024-08-23 02:23:20 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "23-08-2024 10:08:40"
+        "download_time": "23-08-2024 10:21:54"
     },
     {
         "InterviewDate": "01-07-2024",
@@ -3779,6 +3779,12 @@ let interview_statistics  = `[
         "quota_id": "T3_SQ_LAX",
         "interviewerID": "14",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "23-08-2024",
+        "quota_id": "T4_VJ_DAD",
+        "interviewerID": "03",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "23-08-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-08-2024 10:07:34"
+        "download_time": "23-08-2024 10:21:13"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -21543,7 +21543,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-23",
         "quota_id": "Heavenly Wang\u2666TERMINAL 3\u2666Airside\u2666#02-K1\u2666FBA\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
