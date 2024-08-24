663273e68490a64dbb3474a597cc4c27fbfa2154
unknown <lb@silverliningresearch.com>
2024-08-24 03:23:48 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "24-08-2024 05:06:20"
+        "download_time": "24-08-2024 05:20:34"
     },
     {
         "InterviewerID": 1,
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "24-08-2024 05:06:20"
+        "download_time": "24-08-2024 05:20:34"
     },
     {
         "InterviewDate": "2023-04-01",
