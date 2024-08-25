9796f8cd43195683fd32ac32fbfdb66cb66e48f9
unknown <lb@silverliningresearch.com>
2024-08-25 17:09:27 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "25-08-2024 18:50:59"
+        "download_time": "25-08-2024 19:06:01"
     },
     {
         "InterviewerID": 1,
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "25-08-2024 18:53:41"
+        "download_time": "25-08-2024 19:08:57"
     },
     {
         "InterviewDate": "2024-01-03",
