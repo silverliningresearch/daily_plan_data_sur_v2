effa89b05530f2aa3c2ead63144c4f437ae00eac
unknown <lb@silverliningresearch.com>
2024-08-26 00:41:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "26-08-2024 08:22:07"
+        "download_time": "26-08-2024 08:37:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -21973,12 +21973,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-26",
         "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 2\u2666Airside\u2666#03-175/176\u2666HL\u2666Min20",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2024-08-26",
         "quota_id": "Ambassador Transit Lounge\u2666TERMINAL 2\u2666Airside\u2666#036-123&123-A\u2666HL\u2666Min20",
-        "Number of interviews": 7
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-08-26",
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "26-08-2024 02:23:42"
+        "download_time": "26-08-2024 02:39:57"
     },
     {
         "InterviewDate": "2024-05-01",
