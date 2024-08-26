f49fe0bebf0d292829493444fb6f06aeb73589ed
unknown <lb@silverliningresearch.com>
2024-08-26 08:35:34 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -1,82 +1,10 @@
 let interview_statistics = `[
     {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "BA-LHR",
-        "InterviewerID": "7227736",
-        "Number of interviews": 2,
-        "download_time": "26-08-2024 12:22:20"
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "NO-MXP",
-        "InterviewerID": "7227736",
-        "Number of interviews": 7
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "OS-VIE",
-        "InterviewerID": "7227736",
-        "Number of interviews": 2
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "ZF-VKO",
-        "InterviewerID": "7227736",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "OS-VIE",
-        "InterviewerID": "7339101",
-        "Number of interviews": 3
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "NO-MXP",
-        "InterviewerID": "9444533",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "OS-VIE",
-        "InterviewerID": "9444533",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "NO-MXP",
-        "InterviewerID": "9900023",
-        "Number of interviews": 7
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "SU-SVO",
-        "InterviewerID": "9900023",
-        "Number of interviews": 1
-    },
-    {
-        "InterviewDate": "2024-01-20",
-        "quota_id": "WY-MCT",
-        "InterviewerID": "9900023",
-        "Number of interviews": 2
-    },
-    {
-        "InterviewDate": "2024-01-21",
-        "quota_id": "4Y-FRA",
-        "InterviewerID": "7339101",
-        "Number of interviews": 5
-    },
-    {
-        "InterviewDate": "2024-01-21",
-        "quota_id": "4Y-FRA",
-        "InterviewerID": "9444533",
-        "Number of interviews": 5
-    },
-    {
         "InterviewDate": "2024-08-26",
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
-        "Number of interviews": 1
+        "Number of interviews": 1,
+        "download_time": "26-08-2024 12:35:33"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -86,13 +14,31 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-08-26",
+        "quota_id": "6E-BOM",
+        "InterviewerID": "2",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-08-26",
         "quota_id": "6E-Indiamumbai",
         "InterviewerID": "3",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-08-26",
+        "quota_id": "UK-DEL",
+        "InterviewerID": "7715257",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-26",
         "quota_id": "6E-Goa",
+        "InterviewerID": "9198567",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-08-26",
+        "quota_id": "UK-DEL",
         "InterviewerID": "9198567",
         "Number of interviews": 1
     },
