dd652caf702160ad8c327ccbee334c81c8f7a885
unknown <lb@silverliningresearch.com>
2024-08-27 21:55:25 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "28-08-2024 01:39:15"
+        "download_time": "28-08-2024 01:55:22"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -7565,6 +7565,13 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-08-28",
+        "quota_id": "7-7",
+        "Survey": 7,
+        "Location": 8888888,
         "Number of interviews": 1
     }
 ]    
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "28-08-2024 05:38:04"
+        "download_time": "28-08-2024 05:54:06"
     },
     {
         "InterviewDate": "01-07-2024",
