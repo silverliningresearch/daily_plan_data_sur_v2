79a6cf14ea0205ec695ec4d08f712ebdfb5b3696
unknown <lb@silverliningresearch.com>
2024-08-27 00:39:29 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "27-08-2024 02:22:31"
+        "download_time": "27-08-2024 02:36:49"
     },
     {
         "InterviewDate": "2024-01-03",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-08-2024 08:23:08"
+        "download_time": "27-08-2024 08:37:19"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22108,7 +22108,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-27",
         "quota_id": "Gourmet Sarawak\u2666TERMINAL 3\u2666Airside\u2666#02-K5\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-08-27",
+        "quota_id": "IPPUDO EXPRESS\u2666TERMINAL 3\u2666Airside\u2666#02-K3\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-08-27",
