6afaa956936a409c5e8d4fde50424f3f8f37d91a
unknown <lb@silverliningresearch.com>
2024-08-27 07:28:57 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "27-08-2024 08:51:04"
+        "download_time": "27-08-2024 09:22:07"
     },
     {
         "InterviewerID": 1,
@@ -2198,7 +2198,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 3,
         "Interview_Date": "2024-08-27",
-        "completed_interviews": 30
+        "completed_interviews": 40
     },
     {
         "InterviewerID": 4,
@@ -4133,7 +4133,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 5,
         "Interview_Date": "2024-08-27",
-        "completed_interviews": 8
+        "completed_interviews": 15
     },
     {
         "InterviewerID": 6,
@@ -5013,7 +5013,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 9,
         "Interview_Date": "2024-08-27",
-        "completed_interviews": 21
+        "completed_interviews": 29
     },
     {
         "InterviewerID": 10,
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "27-08-2024 08:51:04"
+        "download_time": "27-08-2024 09:22:06"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -73337,6 +73337,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-08-27",
+        "quota_id": "EBB-SN",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-08-27",
         "quota_id": "FRA-LH",
         "Number of interviews": 12
     },
@@ -73352,18 +73357,28 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-08-27",
+        "quota_id": "JFK-SN",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-08-27",
+        "quota_id": "LHR-SN",
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-08-27",
         "quota_id": "MAD-SN",
         "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-08-27",
         "quota_id": "ORD-UA",
-        "Number of interviews": 7
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2024-08-27",
         "quota_id": "RIX-BT",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-08-27",
