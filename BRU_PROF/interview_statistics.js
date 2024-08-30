d4f4d24d5bd64022c976a13c2d39de5dcaafd00b
unknown <lb@silverliningresearch.com>
2024-08-30 01:38:53 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "30-08-2024 03:24:03"
+        "download_time": "30-08-2024 03:36:00"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-08-2024 09:25:21"
+        "download_time": "30-08-2024 09:36:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22443,7 +22443,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-30",
         "quota_id": "SIXT RENT A CAR\u2666TERMINAL 1\u2666Landside\u2666#01-K5\u2666LS\u2666Min5",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
