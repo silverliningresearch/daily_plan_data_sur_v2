9a025db2e03c5bb4083ad176116f931975e97605
unknown <lb@silverliningresearch.com>
2024-08-30 04:23:52 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "30-08-2024 06:06:01"
+        "download_time": "30-08-2024 06:21:17"
     },
     {
         "InterviewerID": 1,
@@ -6309,6 +6309,11 @@ let completed_by_interviewer = `[
         "InterviewerID": 11,
         "Interview_Date": "2024-08-28",
         "completed_interviews": 39
+    },
+    {
+        "InterviewerID": 11,
+        "Interview_Date": "2024-08-30",
+        "completed_interviews": 2
     },
     {
         "InterviewerID": 12,
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "30-08-2024 06:06:01"
+        "download_time": "30-08-2024 06:21:17"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -73944,6 +73944,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "TUN-TB",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-08-30",
+        "quota_id": "VIE-SN",
+        "Number of interviews": 2
     }
 ]    
 
