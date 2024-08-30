4c4526bfd308a9547ee25a4472b11cb205f83c03
unknown <lb@silverliningresearch.com>
2024-08-30 00:38:03 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "30-08-2024 08:26:31"
+        "download_time": "30-08-2024 08:37:02"
     },
     {
         "InterviewDate": "01-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "30-08-2024 08:25:07"
+        "download_time": "30-08-2024 08:36:20"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22433,7 +22433,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-08-30",
