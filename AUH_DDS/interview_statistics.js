2088239bf1150613504410534e4af9bdced2e18d
unknown <lb@silverliningresearch.com>
2024-09-01 11:09:57 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "01-09-2024 14:55:30"
+        "download_time": "01-09-2024 15:09:53"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -7670,14 +7670,14 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-09-01",
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-01",
@@ -7692,6 +7692,13 @@ let interview_statistics = `[
         "Survey": 3,
         "Location": 2,
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-01",
+        "quota_id": "5-5",
+        "Survey": 5,
+        "Location": 1,
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-09-01",
@@ -7712,7 +7719,7 @@ let interview_statistics = `[
         "quota_id": "6-6",
         "Survey": 6,
         "Location": 2,
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-09-01",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "01-09-2024 18:54:06"
+        "download_time": "01-09-2024 19:08:31"
     },
     {
         "InterviewDate": "01-07-2024",
