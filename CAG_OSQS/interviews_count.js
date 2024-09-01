3197ead642406264f386e8478ecfc9e06b0a7f59
unknown <lb@silverliningresearch.com>
2024-09-01 08:54:54 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "01-09-2024 16:38:10"
+        "download_time": "01-09-2024 16:52:49"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22498,7 +22498,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-01",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 1\u2666Landside\u2666#01-K9 (beside Starhub)\u2666CR\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-01",
@@ -22548,7 +22548,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-01",
         "quota_id": "STARHUB\u2666TERMINAL 1\u2666Landside\u2666#01-K8\u2666CR\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-01",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "01-09-2024 16:38:15"
+        "download_time": "01-09-2024 16:52:56"
     },
     {
         "InterviewDate": "2024-06-10",
