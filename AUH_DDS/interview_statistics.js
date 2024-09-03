aaf9f3f344559706680b88458df2e681858e8075
unknown <lb@silverliningresearch.com>
2024-09-03 04:56:23 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "03-09-2024 08:39:41"
+        "download_time": "03-09-2024 08:56:19"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -7811,6 +7811,13 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-09-03",
+        "quota_id": "3-3",
+        "Survey": 3,
+        "Location": 1,
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-03",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "03-09-2024 12:37:41"
+        "download_time": "03-09-2024 12:54:21"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22683,7 +22683,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-03",
         "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-03",
@@ -22698,7 +22698,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-03",
         "quota_id": "GHS3\u2666TERMINAL 3\u2666Airside\u2666GHS3 (A1-8)\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-03",
