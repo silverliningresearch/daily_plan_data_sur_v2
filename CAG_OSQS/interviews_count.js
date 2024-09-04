2d1341666d57b12af7732a595ed4a470930ceb36
unknown <lb@silverliningresearch.com>
2024-09-04 00:42:16 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "04-09-2024 08:23:09"
+        "download_time": "04-09-2024 08:38:39"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -22752,8 +22752,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-09-04",
+        "quota_id": "Giordano\u2666TERMINAL 1\u2666Airside\u2666#02-19N\u2666MP\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-04",
         "quota_id": "Kaboom\u2666TERMINAL 1\u2666Airside\u2666#02-18N\u2666CT\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "04-09-2024 02:24:50"
+        "download_time": "04-09-2024 02:41:32"
     },
     {
         "InterviewDate": "2024-01-28",
@@ -1618,7 +1618,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-09-04",
         "quota_id": "XC-AYT",
-        "Number of interviews": 1
+        "Number of interviews": 4
     }
 ]    
 
