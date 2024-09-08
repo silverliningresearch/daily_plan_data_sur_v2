0ebe8c1ece35d43ab88f8ec725c537ed5031dc89
unknown <lb@silverliningresearch.com>
2024-09-08 03:25:35 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_FOC",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "08-09-2024 11:08:45"
+        "download_time": "08-09-2024 11:24:08"
     },
     {
         "InterviewDate": "01-07-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "08-09-2024 11:08:02"
+        "download_time": "08-09-2024 11:23:25"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -23113,7 +23113,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-08",
         "quota_id": "The Green Party\u2666TERMINAL 3\u2666Landside\u2666#B2-35/36\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
