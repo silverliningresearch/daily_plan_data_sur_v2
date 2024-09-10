ee47c3f6289c9d595f9d5f30bdcd2a40ca3d0113
unknown <lb@silverliningresearch.com>
2024-09-10 21:57:37 +0000

    update new data

--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "10-09-2024 23:40:31"
+        "download_time": "10-09-2024 23:57:36"
     },
     {
         "InterviewDate": "2024-09-03",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "10-09-2024 23:40:03"
+        "download_time": "10-09-2024 23:57:08"
     },
     {
         "InterviewDate": "2024-01-28",
