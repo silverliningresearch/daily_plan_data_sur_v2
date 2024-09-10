ed17e115ea99ca67a534654a26d930f29a29baa5
unknown <lb@silverliningresearch.com>
2024-09-10 00:25:26 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "10-09-2024 08:10:18"
+        "download_time": "10-09-2024 08:23:12"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -23374,6 +23374,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-09-09",
         "quota_id": "UOB T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-K7\u2666MC\u2666Min8",
         "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-10",
+        "quota_id": "Jamie's Deli\u2666TERMINAL 1\u2666Airside\u2666#02-40N\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-09-10",
--- VIE_media/interview_statistics.js
+++ VIE_media/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-04-23",
         "quota_id": "LHR",
         "Number of interviews": 1,
-        "download_time": "10-09-2024 02:13:39"
+        "download_time": "10-09-2024 02:24:49"
     },
     {
         "InterviewDate": "2024-05-01",
