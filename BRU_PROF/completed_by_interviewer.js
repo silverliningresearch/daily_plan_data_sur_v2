3a29a9da781846d1f6cc07213104aad6838376f9
unknown <lb@silverliningresearch.com>
2024-09-11 21:25:31 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "11-09-2024 23:06:36"
+        "download_time": "11-09-2024 23:21:28"
     },
     {
         "InterviewerID": 1,
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "29 \u2013 Werner",
         "Interview_Date": "2024-09-04",
         "completed_interviews": 19,
-        "download_time": "11-09-2024 23:12:13"
+        "download_time": "11-09-2024 23:25:30"
     },
     {
         "InterviewerID": "29 \u2013 Werner",
