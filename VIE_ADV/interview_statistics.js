134589d90470e7510147ac60e38035600afe2c41
unknown <lb@silverliningresearch.com>
2024-09-14 09:37:19 +0000

    update new data

--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "14-09-2024 11:22:09"
+        "download_time": "14-09-2024 11:37:01"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -9873,6 +9873,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-09-14",
         "quota_id": "FR-PFO",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-09-14",
+        "quota_id": "FR-RIX",
         "Number of interviews": 4
     },
     {
