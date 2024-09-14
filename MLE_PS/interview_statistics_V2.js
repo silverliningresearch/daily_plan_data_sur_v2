abb3ddffcda16028038e134229cbcdad2d87522b
unknown <lb@silverliningresearch.com>
2024-09-14 22:22:38 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "15-09-2024 02:09:20"
+        "download_time": "15-09-2024 02:22:05"
     },
     {
         "InterviewDate": "2024-08-26",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "15-09-2024 00:10:12"
+        "download_time": "15-09-2024 00:22:37"
     },
     {
         "InterviewerID": "29 \u2013 Werner",
