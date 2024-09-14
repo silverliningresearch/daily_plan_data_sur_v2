996ecbe3f304bba7b0b1a246a1b354d42516bb7d
unknown <lb@silverliningresearch.com>
2024-09-14 19:37:28 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "14-09-2024 23:21:52"
+        "download_time": "14-09-2024 23:36:54"
     },
     {
         "InterviewDate": "2024-08-26",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "14-09-2024 21:22:24"
+        "download_time": "14-09-2024 21:37:27"
     },
     {
         "InterviewerID": "29 \u2013 Werner",
