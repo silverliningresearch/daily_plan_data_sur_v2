30cbfbef7f6c7f1a1325f5b83847454dc135b400
unknown <lb@silverliningresearch.com>
2024-09-18 19:39:01 +0000

    update new data

--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "18-09-2024 21:22:50"
+        "download_time": "18-09-2024 21:38:59"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "18-09-2024 21:22:26"
+        "download_time": "18-09-2024 21:38:02"
     },
     {
         "InterviewDate": "2024-01-03",
