b1da5e66582b3746db4c0eddb6b6c804bfb0343b
unknown <lb@silverliningresearch.com>
2024-09-20 20:38:24 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "21-09-2024 00:23:06"
+        "download_time": "21-09-2024 00:37:39"
     },
     {
         "InterviewDate": "2024-08-26",
--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "20-09-2024 22:23:44"
+        "download_time": "20-09-2024 22:38:23"
     },
     {
         "InterviewDate": "2024-09-03",
