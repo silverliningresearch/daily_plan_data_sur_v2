d61332789fe678d067c98c716bfcb003a326033d
unknown <lb@silverliningresearch.com>
2024-09-20 19:38:18 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "20-09-2024 21:20:22"
+        "download_time": "20-09-2024 21:35:21"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-06-10",
         "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD",
         "Number of interviews": 1,
-        "download_time": "21-09-2024 03:21:26"
+        "download_time": "21-09-2024 03:36:43"
     },
     {
         "InterviewDate": "2024-06-10",
