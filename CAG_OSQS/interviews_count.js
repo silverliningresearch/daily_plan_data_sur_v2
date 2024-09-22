b9cdf0ba28596dc7bdec2d5d8ad757bc50c33ca8
unknown <lb@silverliningresearch.com>
2024-09-22 01:08:08 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "22-09-2024 08:52:16"
+        "download_time": "22-09-2024 09:06:24"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25208,7 +25208,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-22",
         "quota_id": "Travelex T4 DT North\u2666TERMINAL 4\u2666Airside\u2666#02-37\u2666MC\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     }
 ]    
 
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "22-09-2024 02:54:01"
+        "download_time": "22-09-2024 03:07:44"
     },
     {
         "InterviewDate": "2024-01-28",
