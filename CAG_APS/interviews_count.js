2b89d9f72dddd5b50fa20864d26f9fb670406de2
unknown <lb@silverliningresearch.com>
2024-09-23 11:23:03 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_KR_PNH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "23-09-2024 19:07:26"
+        "download_time": "23-09-2024 19:21:57"
     },
     {
         "InterviewDate": "01-09-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "23-09-2024 19:06:45"
+        "download_time": "23-09-2024 19:21:12"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25268,7 +25268,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-23",
         "quota_id": "Burberry\u2666TERMINAL 2\u2666Airside\u2666#02-214\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-09-23",
