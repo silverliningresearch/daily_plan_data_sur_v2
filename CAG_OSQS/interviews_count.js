d098649b8ccf3b2f185248bb56f538f75b18e31f
unknown <lb@silverliningresearch.com>
2024-09-24 06:37:28 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-09-2024 14:21:34"
+        "download_time": "24-09-2024 14:35:56"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25392,6 +25392,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-09-24",
+        "quota_id": "Bottega Veneta\u2666TERMINAL 3\u2666Airside\u2666#02-19\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-24",
         "quota_id": "Bvlgari\u2666TERMINAL 2\u2666Airside\u2666#02-221\u2666LX\u2666Min8",
         "Number of interviews": 2
     },
@@ -25423,7 +25428,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "MA MUM TO GO\u2666TERMINAL 1\u2666Landside\u2666#01-K23\u2666FBL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-09-24",
@@ -25468,7 +25473,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "Tod's\u2666TERMINAL 2\u2666Airside\u2666#02-225-01\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-24",
@@ -25478,7 +25483,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "Zegna\u2666TERMINAL 2\u2666Airside\u2666#02-217\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "24-09-2024 10:23:20"
+        "download_time": "24-09-2024 10:36:42"
     },
     {
         "InterviewDate": "2024-01-07",
