d47433cfdd842e71983952e95f9f355fe670f1af
unknown <lb@silverliningresearch.com>
2024-09-24 12:39:04 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_KR_PNH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "24-09-2024 20:24:09"
+        "download_time": "24-09-2024 20:37:25"
     },
     {
         "InterviewDate": "01-09-2024",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "24-09-2024 16:24:50"
+        "download_time": "24-09-2024 16:37:40"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -3368,6 +3368,12 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-09-24",
+        "quota_id": "EY-AUH",
+        "InterviewerID": "7715257",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-24",
         "quota_id": "5W-AUH",
         "InterviewerID": "9198567",
         "Number of interviews": 1
@@ -3382,7 +3388,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-24",
         "quota_id": "EK-DXB",
         "InterviewerID": "9846200",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-24",
@@ -3406,7 +3412,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-24",
         "quota_id": "EY-AUH",
         "InterviewerID": "9900023",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
