21fb4a8927aaf90d7e4932c68c8d80bbaa66e2f2
unknown <lb@silverliningresearch.com>
2024-09-24 05:08:41 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "24-09-2024 08:52:12"
+        "download_time": "24-09-2024 09:08:32"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -9189,7 +9189,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-24",
@@ -9210,6 +9210,13 @@ let interview_statistics = `[
         "quota_id": "5-5",
         "Survey": 5,
         "Location": 1,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-24",
+        "quota_id": "5-5",
+        "Survey": 5,
+        "Location": 2,
         "Number of interviews": 1
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-09-2024 12:50:53"
+        "download_time": "24-09-2024 13:06:56"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25433,7 +25433,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "Sony\u2666TERMINAL 3\u2666Airside\u2666#02-62\u2666EL\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-09-24",
@@ -25443,7 +25443,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 2\u2666Airside\u2666#02-216\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-24",
