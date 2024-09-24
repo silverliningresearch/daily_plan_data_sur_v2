68245ac4d7d6f9d8a940aae32b4a7e57b1a02522
unknown <lb@silverliningresearch.com>
2024-09-24 03:07:30 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "24-09-2024 10:51:53"
+        "download_time": "24-09-2024 11:06:03"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25382,6 +25382,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-09-24",
+        "quota_id": "Bacha Coffee (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-52\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-24",
         "quota_id": "Discover Singapore (Premises E) (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-63\u2666SV\u2666Min8",
         "Number of interviews": 8
     },
@@ -25398,7 +25403,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "Victoria's Secret\u2666TERMINAL 3\u2666Airside\u2666#02-56\u2666PC\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     }
 ]    
 
