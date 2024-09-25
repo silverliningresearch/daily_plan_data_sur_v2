496b90fad26f825a614a9a548ec0d1b26e54ea51
unknown <lb@silverliningresearch.com>
2024-09-25 10:12:37 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "25-09-2024 17:51:26"
+        "download_time": "25-09-2024 18:08:44"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25592,6 +25592,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-09-25",
+        "quota_id": "Maison Christian Dior/ Diptyque/ Byredo\u2666TERMINAL 2\u2666Airside\u2666#026-100-03\u2666PC\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-25",
         "quota_id": "Mr. Bucket Chocolaterie\u2666TERMINAL 3\u2666Airside\u2666#02-K14\u2666CCD\u2666Min8",
         "Number of interviews": 8
     },
@@ -25658,6 +25663,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-25",
         "quota_id": "TWG Tea (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-K12\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-25",
+        "quota_id": "Tumi\u2666TERMINAL 2\u2666Airside\u2666#026-100\u2666MP\u2666Min8",
         "Number of interviews": 1
     }
 ]    
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "25-09-2024 13:52:22"
+        "download_time": "25-09-2024 14:10:49"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -3477,6 +3477,12 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-09-25",
         "quota_id": "UK-DEL",
+        "InterviewerID": "9198567",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-25",
+        "quota_id": "UL-ACJ",
         "InterviewerID": "9198567",
         "Number of interviews": 1
     }
