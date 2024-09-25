fee548270b425ce014ab644fda49d1cd5478fede
unknown <lb@silverliningresearch.com>
2024-09-25 03:52:25 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "25-09-2024 05:35:08"
+        "download_time": "25-09-2024 05:50:12"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -77283,6 +77283,11 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-09-24",
         "quota_id": "ZAG-OU",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-25",
+        "quota_id": "AGP-TB",
         "Number of interviews": 3
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "25-09-2024 11:35:53"
+        "download_time": "25-09-2024 11:50:58"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25547,6 +25547,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-09-25",
+        "quota_id": "Irvins Salted Egg\u2666TERMINAL 1\u2666Airside\u2666#02-K07A\u2666CCD\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-25",
         "quota_id": "LT T1 DT Central\u2666TERMINAL 1\u2666Airside\u2666#02-53,02-46,02-46A\u2666LT\u2666Min25",
         "Number of interviews": 1
     },
@@ -25574,6 +25579,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-09-25",
         "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-09-25",
+        "quota_id": "Roger Vivier\u2666TERMINAL 1\u2666Airside\u2666#02-13AF\u2666LX\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-09-25",
