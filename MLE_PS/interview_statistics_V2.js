2829846a885009f8c91d36a5ee09bd50ceb25701
unknown <lb@silverliningresearch.com>
2024-09-27 14:12:10 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "27-09-2024 17:55:15"
+        "download_time": "27-09-2024 18:10:41"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -3670,6 +3670,12 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-27",
         "quota_id": "MU-PVG",
         "InterviewerID": "7715257",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-09-27",
+        "quota_id": "QR-DOH",
+        "InterviewerID": "7715257",
         "Number of interviews": 1
     },
     {
@@ -3734,9 +3740,21 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-09-27",
+        "quota_id": "Ul-BKK",
+        "InterviewerID": "9198567",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-27",
         "quota_id": "MU-PVG",
         "InterviewerID": "9846200",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-09-27",
+        "quota_id": "QR-DOH",
+        "InterviewerID": "9846200",
+        "Number of interviews": 1
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "27-09-2024 15:55:35"
+        "download_time": "27-09-2024 16:11:13"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -10413,6 +10413,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-09-27",
         "quota_id": "TO-ORY",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-09-27",
+        "quota_id": "W4-BCN",
         "Number of interviews": 4
     }
 ]    
