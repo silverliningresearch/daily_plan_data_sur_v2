f7c41a3aa61de40432969c2a9fcf625d99d34649
unknown <lb@silverliningresearch.com>
2024-09-27 10:12:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "27-09-2024 17:53:15"
+        "download_time": "27-09-2024 18:08:33"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25853,7 +25853,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-27",
         "quota_id": "A Noodle Story\u2666TERMINAL 1\u2666Airside\u2666#02-K19\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-09-27",
@@ -25893,7 +25893,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-27",
         "quota_id": "Lixin Teochew Fishball Noodles\u2666TERMINAL 1\u2666Airside\u2666#02-K17\u2666FBA\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-09-27",
@@ -25938,7 +25938,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-27",
         "quota_id": "Victoria's Secret\u2666TERMINAL 1\u2666Airside\u2666#02-27\u2666PC\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-09-27",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-22",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "27-09-2024 13:56:35"
+        "download_time": "27-09-2024 14:12:12"
     },
     {
         "InterviewDate": "2024-09-25",
