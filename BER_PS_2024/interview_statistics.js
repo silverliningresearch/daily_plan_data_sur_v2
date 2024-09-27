5f7e4e00d218ab7b094b3f637db770aef60a60ec
unknown <lb@silverliningresearch.com>
2024-09-27 16:38:37 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "27-09-2024 18:23:40"
+        "download_time": "27-09-2024 18:36:19"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -11914,6 +11914,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-27",
         "quota_id": "BRU - Brussels Airlines",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-09-27",
+        "quota_id": "IST - Turkish Airlines",
+        "Number of interviews": 1
     }
 ]    
 
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "27-09-2024 20:27:10"
+        "download_time": "27-09-2024 20:37:48"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -3766,7 +3766,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-27",
         "quota_id": "EK-DXB",
         "InterviewerID": "9846200",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-09-27",
