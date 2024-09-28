5c97e22f928af45d60f5c0d561ce4066c0283577
unknown <lb@silverliningresearch.com>
2024-09-28 03:11:02 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-22",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "28-09-2024 06:54:29"
+        "download_time": "28-09-2024 07:11:01"
     },
     {
         "InterviewDate": "2024-09-25",
--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "28-09-2024 04:54:26"
+        "download_time": "28-09-2024 05:10:59"
     },
     {
         "InterviewDate": "2024-09-03",
