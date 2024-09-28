25cc134147bc33c598397d35f25db48bea5cd1b4
unknown <lb@silverliningresearch.com>
2024-09-28 23:39:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "29-09-2024 07:21:45"
+        "download_time": "29-09-2024 07:37:28"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25978,12 +25978,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-09-29",
         "quota_id": "Hainan Story Coffee & Toast\u2666TERMINAL 1\u2666Airside\u2666#02-K18\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-09-29",
         "quota_id": "Luckin Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     }
 ]    
 
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "29-09-2024 01:23:37"
+        "download_time": "29-09-2024 01:39:40"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
