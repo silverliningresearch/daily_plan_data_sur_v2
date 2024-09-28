cb5f43c7a77e9198c152d384c57913f23a47926d
unknown <lb@silverliningresearch.com>
2024-09-28 23:08:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "29-09-2024 06:53:35"
+        "download_time": "29-09-2024 07:06:54"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -25974,6 +25974,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-09-28",
         "quota_id": "Pret A Manger\u2666TERMINAL 3\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-29",
+        "quota_id": "Hainan Story Coffee & Toast\u2666TERMINAL 1\u2666Airside\u2666#02-K18\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
