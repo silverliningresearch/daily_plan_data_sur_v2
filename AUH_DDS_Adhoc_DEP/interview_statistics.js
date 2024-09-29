235c62564015f7dffc070e40f35b12119a6cda6b
unknown <lb@silverliningresearch.com>
2024-09-29 11:19:01 +0000

    update new data

---
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -0,0 +1,10 @@
+let interview_statistics = `[
+    {
+        "InterviewDate": "2024-09-29",
+        "quota_id": "6E1494-TRZ",
+        "Number of interviews": 1,
+        "download_time": "29-09-2024 15:19:00"
+    }
+]    
+
+ `;
