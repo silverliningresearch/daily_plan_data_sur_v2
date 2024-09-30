4d5d3c03194d9198e720077ec34a4010b6c79c07
unknown <lb@silverliningresearch.com>
2024-09-30 01:24:21 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "6E-BOM",
         "InterviewerID": "+918876250212",
         "Number of interviews": 1,
-        "download_time": "30-09-2024 05:08:05"
+        "download_time": "30-09-2024 05:23:33"
     },
     {
         "InterviewDate": "2024-08-26",
@@ -4108,7 +4108,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "GF-CMB",
         "InterviewerID": "7715257",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-09-30",
@@ -4120,7 +4120,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "GF-CMB",
         "InterviewerID": "9846200",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
