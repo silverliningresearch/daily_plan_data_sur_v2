ef5d9db32385d8f68987add095ace0d4ff71dfb5
unknown <lb@silverliningresearch.com>
2024-09-30 12:54:36 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-22",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "30-09-2024 16:39:19"
+        "download_time": "30-09-2024 16:54:35"
     },
     {
         "InterviewDate": "2024-09-25",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "30-09-2024 14:39:16"
+        "download_time": "30-09-2024 14:54:33"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
@@ -43,7 +43,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": "29 \u2013 Werner",
         "Interview_Date": "2024-09-30",
-        "completed_interviews": 1
+        "completed_interviews": 2
     },
     {
         "InterviewerID": "34 \u2013 Maja",
