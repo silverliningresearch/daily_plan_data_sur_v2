f7cf9aad04438b081fd09b5304dd399189b30e66
unknown <lb@silverliningresearch.com>
2024-10-01 23:38:39 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-10-2024 07:22:11"
+        "download_time": "02-10-2024 07:36:44"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26109,6 +26109,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "Wee Nam Kee\u2666TERMINAL 2\u2666Airside\u2666#02-K9\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-10-02",
+        "quota_id": "Luckin Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "02-10-2024 01:23:33"
+        "download_time": "02-10-2024 01:38:09"
     },
     {
         "InterviewDate": "2024-01-03",
