bdf77c52db3e43aeb8ea7403993850580948b98c
unknown <lb@silverliningresearch.com>
2024-10-01 04:54:13 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "01-10-2024 12:36:45"
+        "download_time": "01-10-2024 12:52:07"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26028,7 +26028,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-01",
         "quota_id": "IPPUDO EXPRESS\u2666TERMINAL 3\u2666Airside\u2666#02-K3\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-10-01",
@@ -26049,6 +26049,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 3\u2666Airside\u2666#02-K4\u2666FBA\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-01",
+        "quota_id": "The Green Market\u2666TERMINAL 2\u2666Airside\u2666#036-083\u2666FBA\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-10-01",
