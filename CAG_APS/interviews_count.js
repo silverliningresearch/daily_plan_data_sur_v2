f9d9d48c7c65a6af44fd5abc19b8852600c21b15
unknown <lb@silverliningresearch.com>
2024-10-02 01:08:41 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_KR_PNH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "02-10-2024 08:52:43"
+        "download_time": "02-10-2024 09:07:28"
     },
     {
         "InterviewDate": "01-09-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-10-2024 08:51:59"
+        "download_time": "02-10-2024 09:06:47"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26114,6 +26114,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-10-02",
         "quota_id": "Luckin Coffee\u2666TERMINAL 1\u2666Airside\u2666#02-59\u2666FBA\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-10-02",
+        "quota_id": "Pontian Wanton Noodle\u2666TERMINAL 1\u2666Airside\u2666#02-K10N\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-10-02",
