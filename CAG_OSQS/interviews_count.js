1519e5730f3561b985fdb4c435df7d7460d18080
unknown <lb@silverliningresearch.com>
2024-10-02 11:53:53 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "02-10-2024 19:36:52"
+        "download_time": "02-10-2024 19:51:54"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26168,7 +26168,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-02",
         "quota_id": "Hermes\u2666TERMINAL 3\u2666Airside\u2666#02-05E\u2666LX\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-10-02",
@@ -26223,7 +26223,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-02",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-10-02",
@@ -26274,6 +26274,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-10-02",
         "quota_id": "Uniqlo\u2666TERMINAL 1\u2666Airside\u2666#02-23\u2666MP\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-10-02",
+        "quota_id": "WH Smith - Premises H (Air) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-07\u2666OW\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-10-02",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-22",
         "quota_id": "F3-AHB",
         "Number of interviews": 1,
-        "download_time": "02-10-2024 15:38:53"
+        "download_time": "02-10-2024 15:53:53"
     },
     {
         "InterviewDate": "2024-09-25",
