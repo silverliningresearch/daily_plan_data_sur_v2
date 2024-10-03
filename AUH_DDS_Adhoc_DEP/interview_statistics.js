e0f4031bae718a4818d73625eb88fb08fd199cb8
unknown <lb@silverliningresearch.com>
2024-10-03 08:36:23 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -1,9 +1,109 @@
 let interview_statistics = `[
     {
         "InterviewDate": "2024-09-29",
-        "quota_id": "6E1494-TRZ",
+        "quota_id": "inf-TRZ",
         "Number of interviews": 1,
-        "download_time": "29-09-2024 15:19:00"
+        "download_time": "03-10-2024 12:36:23"
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY286-AMD",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "3L015-BAH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY647-BAH",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY406-BKK",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY238-BLR",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY248-CCJ",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "AF639-CDG",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY254-COK",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY210-DEL",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY323-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY327-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "QR1045-DOH",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY233-ISB",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "TK869-IST",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY424-MNL",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY843-SVO",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY450-SYD",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "3L690-TBS",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY593-TLV",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-03",
+        "quota_id": "EY021-YYZ",
+        "Number of interviews": 2
     }
 ]    
 
