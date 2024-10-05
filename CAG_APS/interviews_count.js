edba8a4feeb52d79b2fc1a323d40c32fa8717db9
unknown <lb@silverliningresearch.com>
2024-10-05 04:52:46 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_KR_PNH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "05-10-2024 12:36:44"
+        "download_time": "05-10-2024 12:51:46"
     },
     {
         "InterviewDate": "01-09-2024",
@@ -844,7 +844,7 @@ let interview_statistics  = `[
         "InterviewDate": "05-10-2024",
         "quota_id": "T3_SQ_HKG",
         "interviewerID": "02",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "05-10-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "05-10-2024 12:36:04"
+        "download_time": "05-10-2024 12:51:06"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26677,6 +26677,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-10-05",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K11\u2666CR\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-05",
         "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#02-K7\u2666CR\u2666Min8",
         "Number of interviews": 3
     },
@@ -26688,7 +26693,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-05",
         "quota_id": "Prosegur T4 DCI Central\u2666TERMINAL 4\u2666Landside\u2666#02-01\u2666MC\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-10-05",
@@ -26698,7 +26703,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-05",
         "quota_id": "Travelex T4 DCI Central\u2666TERMINAL 4\u2666Landside\u2666#02-02\u2666MC\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
