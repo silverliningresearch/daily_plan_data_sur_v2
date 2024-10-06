7756c3cc13adccf5a0b1f35d28b90fd8a5395503
unknown <lb@silverliningresearch.com>
2024-10-06 07:23:59 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-03",
         "quota_id": "EY286-AMD",
         "Number of interviews": 1,
-        "download_time": "06-10-2024 11:07:49"
+        "download_time": "06-10-2024 11:23:55"
     },
     {
         "InterviewDate": "2024-10-03",
@@ -752,6 +752,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-10-06",
+        "quota_id": "EY311-JED",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-06",
         "quota_id": "BA072-LHR",
         "Number of interviews": 4
     },
@@ -779,6 +784,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-06",
         "quota_id": "EY843-SVO",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-06",
+        "quota_id": "3L690-TBS",
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "06-10-2024 15:06:09"
+        "download_time": "06-10-2024 15:21:05"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -26808,7 +26808,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-06",
         "quota_id": "Chopstix & Rice and Taiwan Culture\u2666TERMINAL 2\u2666Landside\u2666#03-11/12\u2666FBL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-10-06",
@@ -26818,7 +26818,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-06",
         "quota_id": "Jones The Grocer\u2666TERMINAL 2\u2666Landside\u2666#02-01 & #03-01\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-10-06",
