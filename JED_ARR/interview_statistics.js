5f5427c78618089bcbd3fb1799eb842f3ed73772
unknown <lb@silverliningresearch.com>
2024-10-08 14:24:53 +0000

    update new data

---
+++ JED_ARR/interview_statistics.js
@@ -0,0 +1,20 @@
+let interview_statistics = `[
+    {
+        "InterviewDate": "2024-10-07",
+        "quota_id": "TK-IST",
+        "Number of interviews": 1,
+        "download_time": "08-10-2024 18:24:52"
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "quota_id": "W6-BUD",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "quota_id": "SV-DXB",
+        "Number of interviews": 1
+    }
+]    
+
+ `;
