e77968141efcf556f9835b2adf5be3edd6fbbdf9
unknown <lb@silverliningresearch.com>
2024-10-13 09:40:24 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "13-10-2024 11:20:58"
+        "download_time": "13-10-2024 11:36:02"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -12488,7 +12488,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-13",
         "quota_id": "PSA - Ryanair",
-        "Number of interviews": 5
+        "Number of interviews": 6
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "13-10-2024 17:21:28"
+        "download_time": "13-10-2024 17:37:02"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -27878,12 +27878,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-13",
         "quota_id": "Chef Wei\u2666TERMINAL 2\u2666Airside\u2666#02-K6\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-10-13",
         "quota_id": "Dunkin\u2019 Donuts\u2666TERMINAL 2\u2666Airside\u2666#02-K14\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-10-13",
@@ -27898,7 +27898,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-13",
         "quota_id": "GOPIZZA\u2666TERMINAL 2\u2666Airside\u2666#02-K15\u2666FBA\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-10-13",
