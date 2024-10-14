297f7deb2e62281011e9dacb116699d1751a0ad3
unknown <lb@silverliningresearch.com>
2024-10-14 23:54:45 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_KR_PNH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "15-10-2024 07:37:20"
+        "download_time": "15-10-2024 07:52:52"
     },
     {
         "InterviewDate": "01-09-2024",
@@ -2177,6 +2177,12 @@ let interview_statistics  = `[
         "quota_id": "T4_HU_HAK",
         "interviewerID": "04",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "15-10-2024",
+        "quota_id": "T4_AK_KUL",
+        "interviewerID": "04",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "15-10-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "15-10-2024 07:36:40"
+        "download_time": "15-10-2024 07:52:04"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -28088,7 +28088,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-10-15",
         "quota_id": "Giordano\u2666TERMINAL 2\u2666Airside\u2666#026-096 (#02-160)\u2666MP\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 8
     }
 ]    
 
