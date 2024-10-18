c00df7499f20387fb99ca6ac7b1913adc6fc93d6
unknown <lb@silverliningresearch.com>
2024-10-18 03:53:07 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-03",
         "quota_id": "EY286-AMD",
         "Number of interviews": 1,
-        "download_time": "18-10-2024 07:37:32"
+        "download_time": "18-10-2024 07:52:56"
     },
     {
         "InterviewDate": "2024-10-03",
@@ -2953,7 +2953,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-18",
         "quota_id": "EY653-KWI",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-10-18",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "18-10-2024 07:37:06"
+        "download_time": "18-10-2024 07:52:27"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -1063,6 +1063,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-18",
         "quota_id": "SV-CAI",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-18",
+        "quota_id": "UJ-CAI",
         "Number of interviews": 2
     },
     {
