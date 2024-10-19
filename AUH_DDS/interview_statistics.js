dadbbba8911c1a13f55595f4d2513c8cfff5d958
unknown <lb@silverliningresearch.com>
2024-10-19 14:54:28 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "19-10-2024 18:37:00"
+        "download_time": "19-10-2024 18:53:02"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -10715,7 +10715,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 5
+        "Number of interviews": 7
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "19-10-2024 18:37:14"
+        "download_time": "19-10-2024 18:53:50"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -1193,7 +1193,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-19",
         "quota_id": "SV-GIZ",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-10-19",
@@ -1208,7 +1208,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-19",
         "quota_id": "WY-MCT",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-10-19",
