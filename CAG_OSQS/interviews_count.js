05e60060f6a5d51b2aa263e17257f782ac60bd1b
unknown <lb@silverliningresearch.com>
2024-10-21 05:52:34 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-01-31",
         "quota_id": "KOPITIAM\u2666TERMINAL 3\u2666Landside\u2666#B2-03\u2666FBL\u2666Min25",
         "Number of interviews": 5,
-        "download_time": "21-10-2024 13:35:27"
+        "download_time": "21-10-2024 13:50:59"
     },
     {
         "InterviewDate": "2024-01-31",
@@ -28769,6 +28769,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-10-21",
         "quota_id": "Irvin's Salted Egg\u2666TERMINAL 2\u2666Airside\u2666#02-209D\u2666CCD\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-10-21",
+        "quota_id": "Lacoste\u2666TERMINAL 4\u2666Airside\u2666#02-49\u2666MP\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-21",
+        "quota_id": "Lego\u2666TERMINAL 4\u2666Airside\u2666#02-48\u2666CT\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-10-21",
