58de300642afaae2487017574060d33fa79e27c5
unknown <lb@silverliningresearch.com>
2024-10-22 21:23:01 +0000

    update new data

--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "22-10-2024 23:07:02"
+        "download_time": "22-10-2024 23:22:00"
     },
     {
         "InterviewDate": "2024-09-03",
