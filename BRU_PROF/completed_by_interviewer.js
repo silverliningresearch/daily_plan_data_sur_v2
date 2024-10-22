4665857e2fb5a35ecdd4a157d26033a63edd6106
unknown <lb@silverliningresearch.com>
2024-10-22 19:53:07 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "22-10-2024 21:35:27"
+        "download_time": "22-10-2024 21:50:22"
     },
     {
         "InterviewerID": 1,
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "22-10-2024 23:37:13"
+        "download_time": "22-10-2024 23:52:12"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -1407,6 +1407,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-10-22",
+        "quota_id": "SV-BOM",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-22",
         "quota_id": "XY-CAI",
         "Number of interviews": 1
     },
@@ -1432,6 +1437,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-10-22",
+        "quota_id": "SV-HYD",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-10-22",
         "quota_id": "SV-ISB",
         "Number of interviews": 2
     },
@@ -1453,7 +1463,12 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-10-22",
         "quota_id": "SV-RUH",
-        "Number of interviews": 16
+        "Number of interviews": 18
+    },
+    {
+        "InterviewDate": "2024-10-22",
+        "quota_id": "XY-RUH",
+        "Number of interviews": 1
     }
 ]    
 
