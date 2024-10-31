1f1eea92d8f6ed8aa457ab6bdd6534f8c9ccc6f8
unknown <lb@silverliningresearch.com>
2024-10-31 10:27:55 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-03",
         "quota_id": "EY-AMD",
         "Number of interviews": 1,
-        "download_time": "31-10-2024 14:15:42"
+        "download_time": "31-10-2024 14:26:51"
     },
     {
         "InterviewDate": "2024-10-03",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_HGH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "31-10-2024 18:11:44"
+        "download_time": "31-10-2024 18:24:01"
     },
     {
         "InterviewDate": "01-10-2024",
