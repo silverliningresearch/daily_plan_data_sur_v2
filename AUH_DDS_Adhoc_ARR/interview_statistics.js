482a7104be531e668ff06e53418b9986f8e7650e
unknown <lb@silverliningresearch.com>
2024-11-05 22:51:59 +0000

    update new data

--- AUH_DDS_Adhoc_ARR/interview_statistics.js
+++ AUH_DDS_Adhoc_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "32-ADJ",
         "Number of interviews": 1,
-        "download_time": "06-11-2024 02:37:37"
+        "download_time": "06-11-2024 02:51:57"
     },
     {
         "InterviewDate": "2024-09-30",
@@ -3408,7 +3408,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-11-06",
         "quota_id": "EY-MEL",
-        "Number of interviews": 1
+        "Number of interviews": 6
     }
 ]    
 
--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-03",
         "quota_id": "EY-AMD",
         "Number of interviews": 1,
-        "download_time": "06-11-2024 02:37:24"
+        "download_time": "06-11-2024 02:51:45"
     },
     {
         "InterviewDate": "2024-10-03",
