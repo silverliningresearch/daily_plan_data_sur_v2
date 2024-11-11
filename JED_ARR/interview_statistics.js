fd61ff72e274196df4cb197455530ba53e0739d4
unknown <lb@silverliningresearch.com>
2024-11-11 21:22:32 +0000

    update new data

--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "12-11-2024 00:07:42"
+        "download_time": "12-11-2024 00:22:07"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -2663,6 +2663,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-11-12",
         "quota_id": "F3-CAI",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-12",
+        "quota_id": "SV-RUH",
         "Number of interviews": 1
     }
 ]    
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "12-11-2024 00:07:31"
+        "download_time": "12-11-2024 00:21:58"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -2404,6 +2404,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-11-11",
         "quota_id": "SV-RUH",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-11-12",
+        "quota_id": "SV-ELQ",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-11-12",
