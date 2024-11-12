a3efb56f2739ce2f9f9889240bd1c1a5e2ef6616
unknown <lb@silverliningresearch.com>
2024-11-12 00:27:47 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_MF_HGH",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "12-11-2024 08:08:12"
+        "download_time": "12-11-2024 08:24:21"
     },
     {
         "InterviewDate": "01-10-2024",
@@ -1754,6 +1754,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "12-11-2024",
+        "quota_id": "T3_SQ_BLR",
+        "interviewerID": "-",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "12-11-2024",
+        "quota_id": "T3_SQ_BLR",
+        "interviewerID": "04",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "12-11-2024",
         "quota_id": "T1_CZ_CAN",
         "interviewerID": "22",
         "Number of interviews": 1
@@ -1762,7 +1774,7 @@ let interview_statistics  = `[
         "InterviewDate": "12-11-2024",
         "quota_id": "T1_MF_XMN",
         "interviewerID": "22",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "13-10-2024",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "12-11-2024 03:10:44"
+        "download_time": "12-11-2024 03:26:33"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -2677,8 +2677,13 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-11-12",
+        "quota_id": "XY-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-12",
         "quota_id": "SV-RUH",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-11-12",
