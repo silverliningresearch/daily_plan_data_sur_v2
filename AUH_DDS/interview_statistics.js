f1d18020f4727eeaba85661259e7610f047f8fd7
unknown <lb@silverliningresearch.com>
2024-11-19 22:53:10 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "20-11-2024 02:39:43"
+        "download_time": "20-11-2024 02:51:56"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -12618,6 +12618,13 @@ let interview_statistics = `[
         "InterviewDate": "2024-11-20",
         "quota_id": "3-3",
         "Survey": 3,
+        "Location": 2,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-20",
+        "quota_id": "5-5",
+        "Survey": 5,
         "Location": 2,
         "Number of interviews": 1
     },
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "20-11-2024 06:37:32"
+        "download_time": "20-11-2024 06:50:56"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -9758,6 +9758,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-11-19",
         "quota_id": "WH Smith - Premises C (Land) (T1 Landside)\u2666TERMINAL 1\u2666Landside\u2666#02-84\u2666OW\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-20",
+        "quota_id": "LACOSTE\u2666TERMINAL 1\u2666Airside\u2666#02-30F\u2666MP\u2666Min8",
         "Number of interviews": 1
     }
 ]    
