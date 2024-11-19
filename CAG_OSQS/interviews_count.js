1a3ac195c5c1c57d200e94bcd4ff6fcd77c8c06a
unknown <lb@silverliningresearch.com>
2024-11-19 23:42:02 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "20-11-2024 07:22:19"
+        "download_time": "20-11-2024 07:37:18"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -9788,7 +9788,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-11-20",
         "quota_id": "WH Smith - Premises B (Air) (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-41C\u2666OW\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     }
 ]    
 
--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "20-11-2024 00:24:27"
+        "download_time": "20-11-2024 00:40:10"
     },
     {
         "InterviewDate": "2024-09-03",
