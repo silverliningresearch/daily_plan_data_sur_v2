b7523fc3da5d29ac8045c0a7f1051526fd606ca1
unknown <lb@silverliningresearch.com>
2024-11-22 03:13:20 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TG_BKK",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "22-11-2024 10:51:11"
+        "download_time": "22-11-2024 11:08:29"
     },
     {
         "InterviewDate": "01-11-2024",
@@ -1792,7 +1792,7 @@ let interview_statistics  = `[
         "InterviewDate": "22-11-2024",
         "quota_id": "T4_AK_BKI",
         "interviewerID": "04",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "22-11-2024",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "22-11-2024 05:52:22"
+        "download_time": "22-11-2024 06:11:04"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -3024,6 +3024,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-11-22",
         "quota_id": "XY-GIZ",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-22",
+        "quota_id": "SV-RUH",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-11-22",
