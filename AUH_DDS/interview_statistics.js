f89da4ea61d3f9ad96f658a125f211674acc1b63
unknown <lb@silverliningresearch.com>
2024-11-24 06:26:47 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "24-11-2024 10:08:19"
+        "download_time": "24-11-2024 10:24:51"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -12864,7 +12864,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-11-24",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "24-11-2024 14:07:14"
+        "download_time": "24-11-2024 14:22:32"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -10158,12 +10158,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-11-24",
         "quota_id": "Aroma Truffle\u2666TERMINAL 1\u2666Airside\u2666#02-K19B\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-11-24",
         "quota_id": "Breo\u2666TERMINAL 1\u2666Airside\u2666#02-K46A\u2666EL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-11-24",
@@ -10188,12 +10188,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-11-24",
         "quota_id": "The Cocoa Trees (T1 DTE Kiosk)\u2666TERMINAL 1\u2666Airside\u2666#02-K46\u2666CCD\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-11-24",
         "quota_id": "WH Smith (T1 DTE)\u2666TERMINAL 1\u2666Airside\u2666#02-61\u2666OW\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-11-24",
