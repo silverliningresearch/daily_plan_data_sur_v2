69729dc0c5db1121739fb5dbfba359fbf70f7423
unknown <lb@silverliningresearch.com>
2024-11-25 21:28:10 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "26-11-2024 01:09:28"
+        "download_time": "26-11-2024 01:26:22"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -13004,7 +13004,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 2,
-        "Number of interviews": 5
+        "Number of interviews": 7
     }
 ]    
 
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "25-11-2024 22:07:15"
+        "download_time": "25-11-2024 22:23:09"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -14834,6 +14834,16 @@ let interview_statistics = `[
         "InterviewDate": "2024-11-25",
         "quota_id": "CGN - Eurowings",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-11-25",
+        "quota_id": "DUB - Ryanair",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-25",
+        "quota_id": "DXB - Condor",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-11-25",
