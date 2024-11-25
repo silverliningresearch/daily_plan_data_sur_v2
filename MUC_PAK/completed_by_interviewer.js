f927914665b10b37c6d01781843c099e261c46d7
unknown <lb@silverliningresearch.com>
2024-11-25 23:10:42 +0000

    update new data

--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "25-11-2024 23:52:54"
+        "download_time": "26-11-2024 00:09:49"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 5,
-        "download_time": "25-11-2024 23:52:54"
+        "download_time": "26-11-2024 00:09:49"
     },
     {
         "InterviewDate": "2024-09-03",
