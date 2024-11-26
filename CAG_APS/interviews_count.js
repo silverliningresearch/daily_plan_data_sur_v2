a973d94fd5fcfa0bc9c8be0ff783e87432ea2caa
unknown <lb@silverliningresearch.com>
2024-11-26 01:11:58 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TG_BKK",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "26-11-2024 08:51:56"
+        "download_time": "26-11-2024 09:08:39"
     },
     {
         "InterviewDate": "01-11-2024",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "26-11-2024 08:51:19"
+        "download_time": "26-11-2024 09:07:57"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -10374,6 +10374,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-11-26",
         "quota_id": "LT T4 DT South (walkthrough)\u2666TERMINAL 4\u2666Airside\u2666#02-25\u2666LT\u2666Min25",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "Metapod\u2666TERMINAL 4\u2666Airside\u2666#02-51\u2666EL\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-11-26",
