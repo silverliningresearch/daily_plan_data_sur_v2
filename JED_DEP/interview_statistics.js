31e02a2c7dd2c2519b377d9b308537b6e282d1c4
unknown <lb@silverliningresearch.com>
2024-11-27 14:12:45 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "27-11-2024 16:53:14"
+        "download_time": "27-11-2024 17:11:31"
     },
     {
         "InterviewDate": "2024-10-01",
