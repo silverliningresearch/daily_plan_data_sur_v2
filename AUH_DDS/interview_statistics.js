524daa9d8285bd03e2783345fa30dfd48ca282da
unknown <lb@silverliningresearch.com>
2024-11-30 00:39:31 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "30-11-2024 04:25:52"
+        "download_time": "30-11-2024 04:38:28"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "30-11-2024 08:24:29"
+        "download_time": "30-11-2024 08:37:15"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -11028,7 +11028,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-11-30",
         "quota_id": "KOPITIAM\u2666TERMINAL 4\u2666Landside\u2666#02-205\u2666FBL\u2666Min25",
-        "Number of interviews": 12
+        "Number of interviews": 15
     }
 ]    
 
