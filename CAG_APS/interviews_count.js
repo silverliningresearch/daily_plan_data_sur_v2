832f69845bcff4178840b3a45b9f74b2029ebf64
unknown <lb@silverliningresearch.com>
2024-12-01 00:29:19 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_TG_BKK",
         "interviewerID": "02",
         "Number of interviews": 8,
-        "download_time": "01-12-2024 08:11:47"
+        "download_time": "01-12-2024 08:26:01"
     },
     {
         "InterviewDate": "01-11-2024",
@@ -40,6 +40,12 @@ let interview_statistics  = `[
         "InterviewDate": "01-11-2024",
         "quota_id": "T1_TR_MDC",
         "interviewerID": "999",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "01-12-2024",
+        "quota_id": "T1_TR_CNX",
+        "interviewerID": "02",
         "Number of interviews": 1
     },
     {
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "01-12-2024 08:11:01"
+        "download_time": "01-12-2024 08:25:17"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -11123,12 +11123,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-01",
         "quota_id": "Bvlgari\u2666TERMINAL 2\u2666Airside\u2666#02-221\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-12-01",
         "quota_id": "Ferragamo\u2666TERMINAL 2\u2666Airside\u2666#02-222 (#026-078E)\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-12-01",
