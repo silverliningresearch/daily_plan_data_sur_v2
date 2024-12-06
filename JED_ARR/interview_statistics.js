6179bbf2ece85f7c2416dd61f9735e51d0164b83
unknown <lb@silverliningresearch.com>
2024-12-06 01:41:55 +0000

    update new data

--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "06-12-2024 04:27:30"
+        "download_time": "06-12-2024 04:41:10"
     },
     {
         "InterviewDate": "2024-10-08",
