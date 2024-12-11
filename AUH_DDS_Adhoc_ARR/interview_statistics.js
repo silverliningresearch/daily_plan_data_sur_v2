341e55e6f14254deb4683b125a3edcf8116485d0
unknown <lb@silverliningresearch.com>
2024-12-11 14:31:41 +0000

    update new data

--- AUH_DDS_Adhoc_ARR/interview_statistics.js
+++ AUH_DDS_Adhoc_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "32-ADJ",
         "Number of interviews": 1,
-        "download_time": "11-12-2024 18:15:45"
+        "download_time": "11-12-2024 18:30:43"
     },
     {
         "InterviewDate": "2024-09-30",
--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "11-12-2024 15:08:44"
+        "download_time": "11-12-2024 15:24:29"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -15753,7 +15753,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-12-11",
         "quota_id": "BGY - Ryanair",
-        "Number of interviews": 4
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2024-12-11",
