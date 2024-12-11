d7ffcf7f83e41394e0174d6f66f9aca83ca3d3c5
unknown <lb@silverliningresearch.com>
2024-12-11 02:54:44 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "11-12-2024 10:41:18"
+        "download_time": "11-12-2024 10:52:23"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -11989,6 +11989,11 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-10",
         "quota_id": "iStudio\u2666TERMINAL 2\u2666Airside\u2666#02-238\u2666EL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-12-11",
+        "quota_id": "7-Eleven (T2 Level 2)\u2666TERMINAL 2\u2666Airside\u2666#02-404\u2666CS\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-12-11",
