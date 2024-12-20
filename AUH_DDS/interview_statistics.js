df60d66a79f9bf38afa7cae4794222eac4ce0507
unknown <lb@silverliningresearch.com>
2024-12-20 11:29:49 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "20-12-2024 15:11:46"
+        "download_time": "20-12-2024 15:28:39"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -14355,7 +14355,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2024-12-20",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "20-12-2024 12:07:06"
+        "download_time": "20-12-2024 12:23:02"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -88803,7 +88803,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-12-20",
         "quota_id": "KEF-FI",
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-12-20",
