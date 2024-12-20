b1216307ba0a7d9c54d560a78c6b7f2c9b780e6a
unknown <lb@silverliningresearch.com>
2024-12-20 13:32:36 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "20-12-2024 14:08:32"
+        "download_time": "20-12-2024 14:23:55"
     },
     {
         "InterviewDate": "2024-01-03",
