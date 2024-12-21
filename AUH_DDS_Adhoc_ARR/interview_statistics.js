c93751e879f9b0f4dca452d48325d41bd70b8558
unknown <lb@silverliningresearch.com>
2024-12-21 02:44:15 +0000

    update new data

--- AUH_DDS_Adhoc_ARR/interview_statistics.js
+++ AUH_DDS_Adhoc_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "32-ADJ",
         "Number of interviews": 1,
-        "download_time": "21-12-2024 06:32:47"
+        "download_time": "21-12-2024 06:44:12"
     },
     {
         "InterviewDate": "2024-09-30",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "21-12-2024 03:29:15"
+        "download_time": "21-12-2024 03:41:31"
     },
     {
         "InterviewDate": "2024-01-03",
