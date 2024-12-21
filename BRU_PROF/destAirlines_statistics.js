689f670b238fbeb9cebec2d3da5229b682a9d59b
unknown <lb@silverliningresearch.com>
2024-12-21 06:57:47 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "21-12-2024 07:21:54"
+        "download_time": "21-12-2024 07:52:58"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -88904,6 +88904,11 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2024-12-21",
         "quota_id": "LIS-TP",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-21",
+        "quota_id": "YUL-AC",
+        "Number of interviews": 7
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "21-12-2024 14:25:00"
+        "download_time": "21-12-2024 14:55:11"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -13503,7 +13503,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-21",
         "quota_id": "LT T3 Duplex\u2666TERMINAL 3\u2666Airside\u2666#02-37/39E & #03-07/08\u2666LT\u2666Min25",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-12-21",
@@ -13513,7 +13513,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-21",
         "quota_id": "MCM\u2666TERMINAL 3\u2666Airside\u2666#02-25\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-12-21",
@@ -13528,6 +13528,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-21",
         "quota_id": "Michael Kors\u2666TERMINAL 3\u2666Airside\u2666#02-22\u2666LX\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-21",
+        "quota_id": "Perfumes & Cosmetics (T3 DTC)\u2666TERMINAL 3\u2666Airside\u2666#02-35E & 03-06B (Central)\u2666PC\u2666Min25",
         "Number of interviews": 1
     },
     {
