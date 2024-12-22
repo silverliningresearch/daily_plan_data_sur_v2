58639da275981387c075bfbff46e6d0be31e4349
unknown <lb@silverliningresearch.com>
2024-12-22 20:56:44 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "23-12-2024 00:41:12"
+        "download_time": "23-12-2024 00:55:26"
     },
     {
         "InterviewDate": "2024-04-13",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "22-12-2024 21:41:51"
+        "download_time": "22-12-2024 21:55:36"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
