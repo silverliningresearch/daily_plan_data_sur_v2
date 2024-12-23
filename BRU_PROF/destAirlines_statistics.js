8cfc994472fd690d2599a022d0a13e3709c2cf4d
unknown <lb@silverliningresearch.com>
2024-12-23 10:00:02 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "23-12-2024 10:23:55"
+        "download_time": "23-12-2024 10:51:21"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -89092,6 +89092,11 @@ let destAirlines_statistics  = `[
     },
     {
         "InterviewDate": "2024-12-23",
+        "quota_id": "BLQ-SN",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-12-23",
         "quota_id": "EBB-SN",
         "Number of interviews": 3
     },
@@ -89133,6 +89138,11 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-12-23",
         "quota_id": "MUC-LH",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-12-23",
+        "quota_id": "NSI-SN",
         "Number of interviews": 2
     },
     {
--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "23-12-2024 10:23:54"
+        "download_time": "23-12-2024 10:51:20"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -97742,6 +97742,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2024-12-23",
+        "quota_id": "SN3125-BLQ",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-12-23",
         "quota_id": "SN3175-FCO",
         "Number of interviews": 4
     },
@@ -97749,6 +97754,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-12-23",
         "quota_id": "SN3597-MRS",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-12-23",
+        "quota_id": "SN369-NSI",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-12-23",
