eba8bd6421505f7982d5c601135f1b9ba4b4d4d0
unknown <lb@silverliningresearch.com>
2024-12-24 04:58:55 +0000

    update new data

--- AUH_DDS_Adhoc_ARR/interview_statistics.js
+++ AUH_DDS_Adhoc_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-09-30",
         "quota_id": "32-ADJ",
         "Number of interviews": 1,
-        "download_time": "24-12-2024 08:46:08"
+        "download_time": "24-12-2024 08:58:54"
     },
     {
         "InterviewDate": "2024-09-30",
