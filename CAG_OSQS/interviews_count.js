104121e03ae25ad94e75991236622142a52c12b6
unknown <lb@silverliningresearch.com>
2024-12-24 07:48:34 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "24-12-2024 15:25:17"
+        "download_time": "24-12-2024 15:44:42"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -13773,7 +13773,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-24",
         "quota_id": "HOKKAIDO BAKED CHEESE TART\u2666TERMINAL 3\u2666Landside\u2666#B2-11\u2666FBL\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2024-12-24",
@@ -13808,7 +13808,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-24",
         "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min15",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-12-24",
@@ -13838,7 +13838,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-24",
         "quota_id": "Victoria's Secret\u2666TERMINAL 3\u2666Airside\u2666#02-56\u2666PC\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2024-12-24",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "24-12-2024 10:30:57"
+        "download_time": "24-12-2024 10:47:51"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -5108,6 +5108,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-12-23",
         "quota_id": "XY-TAS",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-12-24",
+        "quota_id": "F3-DMM",
         "Number of interviews": 2
     }
 ]    
