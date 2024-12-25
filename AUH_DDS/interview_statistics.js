bd0e4533538ca2db3e38ced26c9256fd3ceecd6d
unknown <lb@silverliningresearch.com>
2024-12-25 00:11:42 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "25-12-2024 03:55:48"
+        "download_time": "25-12-2024 04:10:16"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -14656,7 +14656,7 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2024-12-25",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "25-12-2024 03:54:02"
+        "download_time": "25-12-2024 04:09:10"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -7717,6 +7717,14 @@ let interview_statistics = `[
         "quota_id": "EK582",
         "Flight": "EK582",
         "DestCode": "DAC",
+        "AirlineCode": "EK",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-25",
+        "quota_id": "EK658",
+        "Flight": "EK658",
+        "DestCode": "MLE",
         "AirlineCode": "EK",
         "Number of interviews": 1
     },
