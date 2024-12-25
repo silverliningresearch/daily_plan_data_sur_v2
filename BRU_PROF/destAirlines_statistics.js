cc1fdacdcae126e59e6aea10fab19882e909222d
unknown <lb@silverliningresearch.com>
2024-12-25 13:31:03 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "25-12-2024 14:06:22"
+        "download_time": "25-12-2024 14:22:09"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -89293,6 +89293,11 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2024-12-25",
         "quota_id": "FAO-HV",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-12-25",
+        "quota_id": "FCO-SN",
         "Number of interviews": 3
     },
     {
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "25-12-2024 16:10:45"
+        "download_time": "25-12-2024 16:29:30"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -4283,7 +4283,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-12-25",
         "quota_id": "SV-CAI",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-12-25",
