0bdbe95de6296673a14f812f54e7f34a8da9ef74
unknown <lb@silverliningresearch.com>
2024-12-25 15:29:39 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "25-12-2024 18:10:39"
+        "download_time": "25-12-2024 18:28:14"
     },
     {
         "InterviewDate": "2024-10-01",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "25-12-2024 16:09:28"
+        "download_time": "25-12-2024 16:25:35"
     },
     {
         "InterviewDate": "2024-01-03",
