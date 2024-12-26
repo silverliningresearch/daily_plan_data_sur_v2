964c41b2e2b94d1803ab38621e84c67fcefe4277
unknown <lb@silverliningresearch.com>
2024-12-26 23:54:11 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "27-12-2024 07:37:38"
+        "download_time": "27-12-2024 07:51:39"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14049,6 +14049,16 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-26",
         "quota_id": "Victoria's Secret\u2666TERMINAL 1\u2666Airside\u2666#02-27\u2666PC\u2666Min8",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-12-27",
+        "quota_id": "ANJAPPAR AND KAILASH PARBAT\u2666TERMINAL 1\u2666Landside\u2666#03-20\u2666FBL\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-12-27",
+        "quota_id": "BURGER KING\u2666TERMINAL 1\u2666Landside\u2666#03-23\u2666FBL\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-12-27",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "27-12-2024 00:39:12"
+        "download_time": "27-12-2024 00:52:32"
     },
     {
         "InterviewDate": "2024-01-03",
