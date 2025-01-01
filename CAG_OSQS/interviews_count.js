df0a386890e7e0e247659bc7c50dbe1d86e0df60
unknown <lb@silverliningresearch.com>
2025-01-01 05:17:51 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "01-01-2025 12:54:23"
+        "download_time": "01-01-2025 13:10:34"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14453,7 +14453,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-01",
         "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 2\u2666Airside\u2666#02-800\u2666FBA\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-01-01",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2024-09-14",
         "completed_interviews": 17,
-        "download_time": "01-01-2025 05:57:08"
+        "download_time": "01-01-2025 06:14:42"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
