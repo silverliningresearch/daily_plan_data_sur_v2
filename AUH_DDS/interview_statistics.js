c9bfb361814baf7b3cf22f52f4913bcb38c5ec55
unknown <lb@silverliningresearch.com>
2025-01-01 04:31:59 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "01-01-2025 07:57:41"
+        "download_time": "01-01-2025 08:28:51"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -14888,6 +14888,13 @@ let interview_statistics = `[
         "Survey": 6,
         "Location": 2,
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-01-01",
+        "quota_id": "1-1",
+        "Survey": 1,
+        "Location": 8888888,
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-01-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "01-01-2025 11:54:42"
+        "download_time": "01-01-2025 12:25:49"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14459,6 +14459,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-01-01",
         "quota_id": "The Green Market\u2666TERMINAL 2\u2666Airside\u2666#036-083\u2666FBA\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-01-01",
+        "quota_id": "Yakun Kaya Toast\u2666TERMINAL 2\u2666Airside\u2666#02-803\u2666FBA\u2666Min8",
+        "Number of interviews": 4
     }
 ]    
 
